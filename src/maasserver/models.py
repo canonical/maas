2d60843970ed55837e71947c6d93d5061713c251
Raphael Badin <raphael.badin@canonical.com>
2012-01-17 17:17:52 +0100

    Basic model.

--- src/maasserver/models.py
+++ src/maasserver/models.py
@@ -1,6 +1,31 @@
 import datetime
+import re
+from uuid import uuid1
+
 from django.db import models
 from django.contrib import admin
+from django.core.validators import RegexValidator
+
+from maasserver.fields import MACAddressField
+
+
+class CommonInfo(models.Model):
+    created = models.DateField(editable=False)
+    updated = models.DateTimeField(editable=False)
+
+    class Meta:
+        abstract = True
+
+    def save(self):
+        if not self.id:
+            self.created = datetime.date.today()
+        self.updated = datetime.datetime.today()
+        super(CommonInfo, self).save()
+
+
+def generate_node_system_id():
+    return 'node-' + str(uuid1())
+
 
 NODE_STATUS_CHOICES = (
     (u'NEW', u'New'),
@@ -11,17 +36,35 @@ NODE_STATUS_CHOICES = (
 )
 
 
-class Node(models.Model):
-    name = models.CharField(max_length=30)
+class Node(CommonInfo):
+    system_id = models.CharField(
+        max_length=41, unique=True, editable=False,
+        default=generate_node_system_id)
+    hostname = models.CharField(max_length=255)
     status = models.CharField(max_length=10, choices=NODE_STATUS_CHOICES)
-    created = models.DateField(editable=False)
-    updated = models.DateTimeField(editable=False)
 
-    def save(self):
-        if not self.id:
-            self.created = datetime.date.today()
-        self.updated = datetime.datetime.today()
-        super(Node, self).save()
+    def __unicode__(self):
+        return self.system_id
 
 
+mac_re = re.compile(r'^([0-9a-fA-F]{2}(:|$)){6}$')
+
+
+validate_mac_address = RegexValidator(
+    regex = mac_re,
+    message = u'Enter a valid MAC address (e.g. AA:BB:CC:DD:EE:FF).')
+
+
+class MACAddress(CommonInfo):
+    mac_address = models.CharField(
+        max_length=17, validators=[validate_mac_address])
+    node = models.ForeignKey(Node)
+
+    def __unicode__(self):
+        return self.mac_address
+
+
+# Register the models in the admin site.
 admin.site.register(Node)
+admin.site.register(MACAddress)
+
