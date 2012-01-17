2b7b3c8267d628093be744999bcaafffc983dd18
Julian Edwards <julian.edwards@canonical.com>
2012-01-17 18:10:18 +0000

    Add hashbang and copyright to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Distutils installer for maas."""
+
 import os
 from setuptools import setup, find_packages
 
