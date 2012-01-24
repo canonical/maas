9c21bfb4f78c844a27d2ed4416c09671fa522b12
Gavin Panella <gavin.panella@canonical.com>
2012-01-24 12:32:21 +0000

    [r=bigjools] Script to generate and upload current development documentation.

---
+++ templates/script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+from __future__ import (
+    print_function,
+    unicode_literals,
+    )
+
+"""..."""
+
+__metaclass__ = type
+
+import argparse
+
+
+# See http://docs.python.org/release/2.7/library/argparse.html.
+argument_parser = argparse.ArgumentParser(description=__doc__)
+
+
+if __name__ == "__main__":
+    args = argument_parser.parse_args()
+    print(args)
---
+++ templates/script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+# Exit immediately if a command exits with a non-zero status.
+set -o errexit
+# Treat unset variables as an error when substituting.
+set -o nounset
