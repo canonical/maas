b6d7fed14ec1838d37f518c30205ae12d7fe10be
Raphael Badin <raphael.badin@canonical.com>
2012-02-10 11:50:35 +0000

    [r=jtv][bug=][author=rvb] Update HACKING.txt.

--- src/maasserver/api.py
+++ src/maasserver/api.py
@@ -382,7 +382,7 @@ def generate_api_doc():
         generate_doc(NodeMacHandler),
         )
 
-    messages = ['MaaS API\n========\n\n']
+    messages = []
     for doc in docs:
         for method in doc.get_methods():
             messages.append(
