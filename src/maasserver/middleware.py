76d0bfb4b5b86dbb87d13bb409bd6ff50acaa588
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-02-16 11:50:33 +0000

    [r=allenap][bug=][author=jtv] Lint.

--- src/maasserver/middleware.py
+++ src/maasserver/middleware.py
@@ -112,11 +112,12 @@ class APIErrorsMiddleware:
             # "Internal Server Error" response.
             return None
 
+
 class ConsoleExceptionMiddleware:
     def process_exception(self, request, exception):
         import traceback
         import sys
         exc_info = sys.exc_info()
-        print("######################## Exception #############################")
+        print(" Exception ".center(79, "#"))
         print(''.join(traceback.format_exception(*exc_info)))
-        print("################################################################")
+        print("#" * 79)
