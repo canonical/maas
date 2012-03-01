94b6f5e450860366b82833d2839269484e3bdd78
Raphael Badin <raphael.badin@canonical.com>
2012-03-01 17:00:42 +0000

    [r=allenap][bug=][author=rvb] Clean up development.py.

--- src/maas/settings.py
+++ src/maas/settings.py
@@ -160,6 +160,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'maasserver.middleware.AccessMiddleware',
