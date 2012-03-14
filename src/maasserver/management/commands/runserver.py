870fa6d0489aa1fefc616adb57a3abe4ee9a7701
Raphael Badin <raphael.badin@canonical.com>
2012-03-14 14:50:39 +0000

    [r=allenap][bug=][author=rvb] Multithreading dev server.

--- src/maasserver/management/commands/runserver.py
+++ src/maasserver/management/commands/runserver.py
@@ -11,8 +11,13 @@ from __future__ import (
 __metaclass__ = type
 __all__ = ['Command']
 
+from optparse import make_option
+from SocketServer import ThreadingMixIn
+
 from django.conf import settings
 from django.core.management.commands.runserver import BaseRunserverCommand
+from django.core.servers import basehttp
+from django.core.servers.basehttp import WSGIServer
 import oops
 from oops_datedir_repo import DateDirRepo
 from oops_wsgi import (
@@ -45,6 +50,23 @@ def render_error(report):
 
 class Command(BaseRunserverCommand):
     """Customized "runserver" command that wraps the WSGI handler."""
+    option_list = BaseRunserverCommand.option_list + (
+        make_option('--threading', action='store_true',
+            dest='use_threading', default=False,
+            help='Use threading for web server.'),
+        )
+
+    def run(self, *args, **options):
+        threading = options.get('use_threading', False)
+        if threading:
+            # This is a simple backport from Django's future
+            # version to support threading.
+            class ThreadedWSGIServer(ThreadingMixIn, WSGIServer):
+                pass
+            # Monkey patch basehttp.WSGIServer.
+            setattr(basehttp, 'WSGIServer', ThreadedWSGIServer)
+
+        return super(Command, self).run(*args, **options)
 
     def get_handler(self, *args, **kwargs):
         """Overridable from `BaseRunserverCommand`: Obtain a WSGI handler."""
