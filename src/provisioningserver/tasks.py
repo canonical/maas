bbd175d2a0f6fb1aa38e7b7c97f8caa933d60ebe
Julian Edwards <julian.edwards@canonical.com>
2012-05-21 00:40:44 +0000

    [r=rvb][bug=][author=julian-edwards] Add a celeryconfig and a stub tasks.py for the provisioning server.

---
+++ src/provisioningserver/tasks.py
@@ -0,0 +1,13 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Provisioning server tasks that are run in Celery workers."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
