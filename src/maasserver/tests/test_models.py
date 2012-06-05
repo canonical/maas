9fb2487a49a87abaf5a526c2c5862950729168cd
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-06-05 00:40:48 +0000

    [r=julian-edwards][bug=][author=jtv] Migrate UserProfile tests into their own module.

--- src/maasserver/tests/test_models.py
+++ src/maasserver/tests/test_models.py
@@ -18,7 +18,6 @@ from datetime import (
     )
 
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.core.exceptions import (
     PermissionDenied,
     ValidationError,
@@ -30,22 +29,16 @@ from maasserver.enum import (
     NODE_STATUS_CHOICES,
     NODE_STATUS_CHOICES_DICT,
     )
-from maasserver.exceptions import (
-    CannotDeleteUserException,
-    NodeStateViolation,
-    )
+from maasserver.exceptions import NodeStateViolation
 from maasserver.models import (
     Config,
     create_auth_token,
-    GENERIC_CONSUMER,
     get_auth_tokens,
     get_db_state,
     MACAddress,
     Node,
     NODE_TRANSITIONS,
     now,
-    SYSTEM_USERS,
-    UserProfile,
     )
 from maasserver.provisioning import get_provisioning_api_proxy
 from maasserver.testing import reload_object
@@ -66,10 +59,8 @@ from metadataserver.models import (
     NodeUserData,
     )
 from piston.models import (
-    Consumer,
     KEY_SIZE,
     SECRET_SIZE,
-    Token,
     )
 from provisioningserver.enum import POWER_TYPE
 from provisioningserver.power.poweraction import PowerAction
@@ -947,83 +938,3 @@ class AuthTokensTest(TestCase):
         token.is_approved = False
         token.save()
         self.assertNotIn(token, get_auth_tokens(user))
-
-
-# Scheduled for model migration on 2012-06-05
-class UserProfileTest(TestCase):
-
-    def test_profile_creation(self):
-        # A profile is created each time a user is created.
-        user = factory.make_user()
-        self.assertIsInstance(user.get_profile(), UserProfile)
-        self.assertEqual(user, user.get_profile().user)
-
-    def test_consumer_creation(self):
-        # A generic consumer is created each time a user is created.
-        user = factory.make_user()
-        consumers = Consumer.objects.filter(user=user, name=GENERIC_CONSUMER)
-        self.assertEqual([user], [consumer.user for consumer in consumers])
-
-    def test_token_creation(self):
-        # A token is created each time a user is created.
-        user = factory.make_user()
-        [token] = get_auth_tokens(user)
-        self.assertEqual(user, token.user)
-
-    def test_create_authorisation_token(self):
-        # UserProfile.create_authorisation_token calls create_auth_token.
-        user = factory.make_user()
-        profile = user.get_profile()
-        consumer, token = profile.create_authorisation_token()
-        self.assertEqual(user, token.user)
-        self.assertEqual(user, consumer.user)
-
-    def test_get_authorisation_tokens(self):
-        # UserProfile.get_authorisation_tokens calls get_auth_tokens.
-        user = factory.make_user()
-        consumer, token = user.get_profile().create_authorisation_token()
-        self.assertIn(token, user.get_profile().get_authorisation_tokens())
-
-    def test_delete(self):
-        # Deleting a profile also deletes the related user.
-        profile = factory.make_user().get_profile()
-        profile_id = profile.id
-        user_id = profile.user.id
-        self.assertTrue(User.objects.filter(id=user_id).exists())
-        self.assertTrue(
-            UserProfile.objects.filter(id=profile_id).exists())
-        profile.delete()
-        self.assertFalse(User.objects.filter(id=user_id).exists())
-        self.assertFalse(
-            UserProfile.objects.filter(id=profile_id).exists())
-
-    def test_delete_consumers_tokens(self):
-        # Deleting a profile deletes the related tokens and consumers.
-        profile = factory.make_user().get_profile()
-        token_ids = []
-        consumer_ids = []
-        for i in range(3):
-            token, consumer = profile.create_authorisation_token()
-            token_ids.append(token.id)
-            consumer_ids.append(consumer.id)
-        profile.delete()
-        self.assertFalse(Consumer.objects.filter(id__in=consumer_ids).exists())
-        self.assertFalse(Token.objects.filter(id__in=token_ids).exists())
-
-    def test_delete_attached_nodes(self):
-        # Cannot delete a user with nodes attached to it.
-        profile = factory.make_user().get_profile()
-        factory.make_node(owner=profile.user)
-        self.assertRaises(CannotDeleteUserException, profile.delete)
-
-    def test_manager_all_users(self):
-        users = set(factory.make_user() for i in range(3))
-        all_users = set(UserProfile.objects.all_users())
-        self.assertEqual(users, all_users)
-
-    def test_manager_all_users_no_system_user(self):
-        for i in range(3):
-            factory.make_user()
-        usernames = set(
-            user.username for user in UserProfile.objects.all_users())
-        self.assertTrue(set(SYSTEM_USERS).isdisjoint(usernames))
