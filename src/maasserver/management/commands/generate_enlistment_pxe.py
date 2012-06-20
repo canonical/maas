74040f2d49f17e6280a16c7e0c8c848ab0423ec7
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-06-20 02:50:50 +0000

    [r=julian-edwards][bug=][author=jtv] New django command: generate_enlistment_pxe.

---
+++ src/maasserver/management/commands/generate_enlistment_pxe.py
@@ -0,0 +1,68 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Django command: generate a PXE configuration file for node enlistment.
+
+Produces the "default" PXE configuration that we provide to nodes that
+MAAS is not yet aware of.  A node that netboots using this configuration
+will then register itself with the MAAS.
+"""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = [
+    'Command',
+    ]
+
+
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from provisioningserver.pxe.pxeconfig import PXEConfig
+
+
+class Command(BaseCommand):
+    """Print out enlistment PXE config."""
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--arch', dest='arch', default=None,
+            help="Main system architecture to generate config for."),
+        make_option(
+            '--subarch', dest='arch', default='generic',
+            help="Sub-architecture of the main architecture."),
+        make_option(
+            '--release', dest='release', default=None,
+            help="Ubuntu release to run when enlisting nodes."),
+        )
+
+    def handle(self, arch=None, subarch='generic', release=None, **kwargs):
+        image_path = '/maas/%s/%s/%s/install' % (arch, subarch, release)
+        # TODO: This needs to go somewhere more appropriate, and
+        # probably contain more appropriate options.
+        kernel_opts = ' '.join([
+            # Default kernel options (similar to those used by Cobbler):
+            'initrd=%s' % '/'.join([image_path, 'initrd.gz']),
+            'ksdevice=bootif',
+            'lang=  text ',
+            'hostname=%s-%s' % (release, arch),
+            'domain=local.lan',
+            'suite=%s' % release,
+
+            # MAAS-specific options:
+            'priority=critical',
+            'local=en_US',
+            'netcfg/choose_interface=auto',
+            ])
+        template_args = {
+            'menutitle': "Enlisting with MAAS",
+            # Enlistment uses the same kernel as installation.
+            'kernelimage': '/'.join([image_path, 'linux']),
+            'append': kernel_opts,
+        }
+        PXEConfig(arch, subarch).write_config(**template_args)
---
+++ src/maasserver/tests/test_commands_generate_enlistment_pxe.py
@@ -0,0 +1,41 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for the generate-enlistment-pxe command."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
+
+import os.path
+
+from django.core.management import call_command
+from maasserver.enum import ARCHITECTURE_CHOICES
+from maasserver.testing.factory import factory
+from maasserver.testing.testcase import TestCase
+from provisioningserver.pxe.pxeconfig import PXEConfig
+
+
+class TestGenerateEnlistmentPXE(TestCase):
+
+    def test_generates_default_pxe_config(self):
+        arch = factory.getRandomChoice(ARCHITECTURE_CHOICES)
+        release = 'precise'
+        tftpdir = self.make_dir()
+        self.patch(PXEConfig, 'target_basedir', tftpdir)
+        call_command('generate_enlistment_pxe', arch=arch, release=release)
+        # This produces a "default" PXE config file in the right place.
+        # It refers to the kernel and initrd for the requested
+        # architecture and release.
+        result_path = os.path.join(
+            tftpdir, arch, 'generic', 'pxelinux.cfg', 'default')
+        with open(result_path) as result_file:
+            contents = result_file.read()
+        self.assertIn(
+            '/'.join(['/maas', arch, 'generic', release, 'install', 'linux']),
+            contents)
