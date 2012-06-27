2b52ee60385220247f50dec3c508845597ff4c34
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-06-27 03:50:53 +0000

    [r=julian-edwards][bug=][author=jtv] Model migration: NodeUserData tests.

--- src/metadataserver/tests/test_models.py
+++ src/metadataserver/tests/test_models.py
@@ -19,7 +19,6 @@ from maastesting.djangotestcase import DjangoTestCase
 from metadataserver.models import (
     NodeCommissionResult,
     NodeKey,
-    NodeUserData,
     )
 
 
@@ -70,72 +69,6 @@ class TestNodeKeyManager(DjangoTestCase):
             key,
             NodeKey.objects.get_token_for_node(
                 NodeKey.objects.get_node_for_key(key)).key)
-
-
-# Scheduled for model migration on 2012-06-28
-class TestNodeUserDataManager(DjangoTestCase):
-    """Test NodeUserDataManager."""
-
-    def setUp(self):
-        super(TestNodeUserDataManager, self).setUp()
-
-    def test_set_user_data_creates_new_nodeuserdata_if_needed(self):
-        node = factory.make_node()
-        data = b'foo'
-        NodeUserData.objects.set_user_data(node, data)
-        self.assertEqual(data, NodeUserData.objects.get(node=node).data)
-
-    def test_set_user_data_overwrites_existing_userdata(self):
-        node = factory.make_node()
-        data = b'bar'
-        NodeUserData.objects.set_user_data(node, b'old data')
-        NodeUserData.objects.set_user_data(node, data)
-        self.assertEqual(data, NodeUserData.objects.get(node=node).data)
-
-    def test_set_user_data_leaves_data_for_other_nodes_alone(self):
-        node = factory.make_node()
-        NodeUserData.objects.set_user_data(node, b'intact')
-        NodeUserData.objects.set_user_data(factory.make_node(), b'unrelated')
-        self.assertEqual(b'intact', NodeUserData.objects.get(node=node).data)
-
-    def test_set_user_data_to_None_removes_user_data(self):
-        node = factory.make_node()
-        NodeUserData.objects.set_user_data(node, b'original')
-        NodeUserData.objects.set_user_data(node, None)
-        self.assertItemsEqual([], NodeUserData.objects.filter(node=node))
-
-    def test_set_user_data_to_None_when_none_exists_does_nothing(self):
-        node = factory.make_node()
-        NodeUserData.objects.set_user_data(node, None)
-        self.assertItemsEqual([], NodeUserData.objects.filter(node=node))
-
-    def test_get_user_data_retrieves_data(self):
-        node = factory.make_node()
-        data = b'splat'
-        NodeUserData.objects.set_user_data(node, data)
-        self.assertEqual(data, NodeUserData.objects.get_user_data(node))
-
-    def test_get_user_data_raises_DoesNotExist_if_not_found(self):
-        node = factory.make_node()
-        self.assertRaises(
-            NodeUserData.DoesNotExist,
-            NodeUserData.objects.get_user_data, node)
-
-    def test_get_user_data_ignores_other_nodes(self):
-        node = factory.make_node()
-        data = b'bzzz'
-        NodeUserData.objects.set_user_data(node, data)
-        NodeUserData.objects.set_user_data(factory.make_node(), b'unrelated')
-        self.assertEqual(data, NodeUserData.objects.get_user_data(node))
-
-    def test_has_user_data_returns_False_if_node_has_no_user_data(self):
-        self.assertFalse(
-            NodeUserData.objects.has_user_data(factory.make_node()))
-
-    def test_has_user_data_returns_True_if_node_has_user_data(self):
-        node = factory.make_node()
-        NodeUserData.objects.set_user_data(node, b"This node has user data.")
-        self.assertTrue(NodeUserData.objects.has_user_data(node))
 
 
 class TestNodeCommissionResult(DjangoTestCase):
