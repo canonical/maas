37a96cb871f4efd2a78e100510a8d1ea7359f8ab
Raphael Badin <raphael.badin@canonical.com>
2012-07-31 16:20:53 +0000

    [r=allenap][bug=1031365][author=rvb] This branch fixes the metadata API urls.

--- src/metadataserver/urls.py
+++ src/metadataserver/urls.py
@@ -49,16 +49,16 @@ version_index_by_mac_handler = Resource(VersionIndexHandler)
 node_patterns = patterns(
     '',
     url(
-        r'(?P<version>[^/]+)/meta-data/(?P<item>.*)$',
+        r'^(?P<version>[^/]+)/meta-data/(?P<item>.*)$',
         meta_data_handler,
         name='metadata-meta-data'),
     url(
-        r'(?P<version>[^/]+)/user-data$', user_data_handler,
+        r'^(?P<version>[^/]+)/user-data$', user_data_handler,
         name='metadata-user-data'),
     url(
-        r'(?P<version>[^/]+)/', version_index_handler,
+        r'^(?P<version>[^/]+)/', version_index_handler,
         name='metadata-version'),
-    url(r'', index_handler, name='metadata'),
+    url(r'^', index_handler, name='metadata'),
     )
 
 # Anonymous random metadata access, keyed by system ID.  These serve requests
@@ -69,11 +69,11 @@ by_id_patterns = patterns(
     # XXX: rvb 2012-06-20 bug=1015559:  This method is accessible
     # without authentication.  This is a security threat.
     url(
-        r'(?P<version>[^/]+)/by-id/(?P<system_id>[\w\-]+)/$',
+        r'^(?P<version>[^/]+)/by-id/(?P<system_id>[\w\-]+)/$',
         meta_data_anon_handler,
         name='metadata-node-by-id'),
     url(
-        r'(?P<version>[^/]+)/enlist-preseed/$',
+        r'^(?P<version>[^/]+)/enlist-preseed/$',
         meta_data_anon_handler,
         name='metadata-enlist-preseed'),
     )
@@ -84,15 +84,15 @@ by_id_patterns = patterns(
 by_mac_patterns = patterns(
     '',
     url(
-        r'(?P<version>[^/]+)/by-mac/(?P<mac>[^/]+)/meta-data/(?P<item>.*)$',
+        r'^(?P<version>[^/]+)/by-mac/(?P<mac>[^/]+)/meta-data/(?P<item>.*)$',
         meta_data_by_mac_handler,
         name='metadata-meta-data-by-mac'),
     url(
-        r'(?P<version>[^/]+)/by-mac/(?P<mac>[^/]+)/user-data$',
+        r'^(?P<version>[^/]+)/by-mac/(?P<mac>[^/]+)/user-data$',
         user_data_by_mac_handler,
         name='metadata-user-data-by-mac'),
     url(
-        r'(?P<version>[^/]+)/by-mac/(?P<mac>[^/]+)/',
+        r'^(?P<version>[^/]+)/by-mac/(?P<mac>[^/]+)/',
         version_index_by_mac_handler,
         name='metadata-version-by-mac'),
     )
