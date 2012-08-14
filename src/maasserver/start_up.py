a63540e32e60d98eda334126f8e81897dd0c9e93
Raphael Badin <raphael.badin@canonical.com>
2012-08-14 16:00:57 +0000

    [r=allenap][bug=][author=rvb] Use file-based lock in start_up method.

--- src/maasserver/start_up.py
+++ src/maasserver/start_up.py
@@ -15,9 +15,17 @@ __all__ = [
     ]
 
 
+from lockfile import FileLock
 from maasserver.dns import write_full_dns_config
 from maasserver.maasavahi import setup_maas_avahi_service
 from maasserver.models import NodeGroup
+
+# Lock file used to prevent concurrent runs of the start_up() method.
+LOCK_FILE_NAME = '/run/lock/' + __name__
+
+
+# Timeout used to grab the filed-based lock used by the start_up() method.
+LOCK_TIMEOUT = 60
 
 
 def start_up():
@@ -30,8 +38,20 @@ def start_up():
     This method is called when the MAAS application starts up.
     In production, it's called from the WSGI script so this shouldn't block
     at any costs.  It should simply call very simple methods or Celery tasks.
-    """
 
+    The method will be executed multiple times if multiple processes are used
+    but this method uses file-based locking to ensure that the methods it calls
+    internally are not ran concurrently.
+    """
+    lock = FileLock(LOCK_FILE_NAME)
+    lock.acquire(timeout=LOCK_TIMEOUT)
+    try:
+        inner_start_up()
+    finally:
+        lock.release()
+
+
+def inner_start_up():
     # Publish the MAAS server existence over Avahi.
     setup_maas_avahi_service()
 
--- src/maasserver/tests/test_start_up.py
+++ src/maasserver/tests/test_start_up.py
@@ -12,6 +12,15 @@ from __future__ import (
 __metaclass__ = type
 __all__ = []
 
+from multiprocessing import (
+    Process,
+    Value,
+    )
+
+from lockfile import (
+    FileLock,
+    LockTimeout,
+    )
 from maasserver import start_up
 from maasserver.models.nodegroup import NodeGroup
 from maastesting.fakemethod import FakeMethod
@@ -20,6 +29,10 @@ from maastesting.testcase import TestCase
 
 class TestStartUp(TestCase):
     """Testing for the method `start_up`."""
+
+    def setUp(self):
+        super(TestStartUp, self).setUp()
+        self.patch(start_up, 'LOCK_FILE_NAME', self.make_file())
 
     def test_start_up_calls_setup_maas_avahi_service(self):
         recorder = FakeMethod()
@@ -40,3 +53,31 @@ class TestStartUp(TestCase):
     def test_start_up_creates_master_nodegroup(self):
         start_up.start_up()
         self.assertEqual(1, NodeGroup.objects.all().count())
+
+    def test_start_up_runs_in_exclusion(self):
+        called = Value('b', False)
+
+        def check_lock():
+            called.value = True
+            lock = FileLock(start_up.LOCK_FILE_NAME)
+            self.assertRaises(LockTimeout, lock.acquire, timeout=0.1)
+
+        def check_lock_in_subprocess():
+             proc = Process(target=check_lock)
+             proc.start()
+             proc.join()
+
+        self.patch(start_up, 'inner_start_up', check_lock_in_subprocess)
+        start_up.start_up()
+        self.assertTrue(called.value)
+
+    def test_start_up_respects_timeout_to_acquire_lock(self):
+        recorder = FakeMethod()
+        self.patch(start_up, 'inner_start_up', recorder)
+        # Use a timeout more suitable for automated testing.
+        self.patch(start_up, 'LOCK_TIMEOUT', 0.1)
+        # Manually create a lock.
+        self.make_file(FileLock(start_up.LOCK_FILE_NAME).lock_file)
+
+        self.assertRaises(LockTimeout, start_up.start_up)
+        self.assertEqual(0, recorder.call_count)
