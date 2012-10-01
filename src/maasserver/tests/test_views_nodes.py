23d64ea79fd76a015199fd9357dbaea34060d9a9
John Arbash Meinel <john@arbash-meinel.com>
2012-10-01 07:04:18 +0000

    [r=julian-edwards][bug=][author=jameinel] Display Architecture, CPU count, and Memory on the Node view page.

--- src/maasserver/tests/test_views_nodes.py
+++ src/maasserver/tests/test_views_nodes.py
@@ -99,12 +99,18 @@ class NodeViewsTest(LoggedInTestCase):
     def test_view_node_displays_node_info(self):
         # The node page features the basic information about the node.
         node = factory.make_node(owner=self.logged_in_user)
+        node.cpu_count = 123
+        node.memory = 512
+        node.save()
         node_link = reverse('node-view', args=[node.system_id])
         response = self.client.get(node_link)
         doc = fromstring(response.content)
         content_text = doc.cssselect('#content')[0].text_content()
         self.assertIn(node.hostname, content_text)
         self.assertIn(node.display_status(), content_text)
+        self.assertIn(node.architecture, content_text)
+        self.assertIn('%d MB' % (node.memory,), content_text)
+        self.assertIn('%d' % (node.cpu_count,), content_text)
         self.assertIn(self.logged_in_user.username, content_text)
 
     def test_view_node_displays_node_info_no_owner(self):
