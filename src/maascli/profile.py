80f94fa789dbfc21e0ee6d16aa50ef0b6626f942
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-10-08 15:36:31 +0000

    [r=allenap][bug=][author=jtv] Small maascli module for getting profiles.

---
+++ src/maascli/profile.py
@@ -0,0 +1,66 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Profile-related functionality."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = [
+    'get_profile',
+    'select_profile',
+    ]
+
+from itertools import islice
+
+
+class InvalidProfile(Exception):
+    """Unknown profile specified."""
+
+
+def get_profile(profiles, profile_name):
+    """Look up the named profile in `profiles`.
+
+    :param profiles: The result of `ProfileConfig.open()`.
+    :param profile_name: The profile requested by the user.
+    :return: The `ProfileConfig` option for the requested profile.
+    :raise InvalidProfile: Requested profile was not found.
+    """
+    if profile_name not in profiles:
+        raise InvalidProfile("'%s' is not an active profile." % profile_name)
+    return profiles[profile_name]
+
+
+def name_default_profile(profiles):
+    """Return name of the default profile, or raise `NoDefaultProfile`.
+
+    :param profiles: The result of `ProfileConfig.open()`.
+    :return: The name of the default profile, or None if there is no
+        reasonable default.
+    """
+    profiles_sample = list(islice(profiles, 2))
+    if len(profiles_sample) == 1:
+        # There's exactly one profile.  That makes a sensible default.
+        return profiles_sample[0]
+
+    return None
+
+
+def select_profile(profiles, profile_name=None):
+    """Return name for the applicable profile: the given name, or the default.
+
+    :param profiles: The result of `ProfileConfig.open()`.
+    :param profile_name: The profile requested by the user, if any.  This may
+        be `None`, in which case `select_profile` will look for a sensible
+        default to use.
+    :return: Name of the applicable profile, or `None` if no profile was
+        explicitly requested and no sensible default presents itself.
+    """
+    if profile_name is None:
+        return name_default_profile(profiles)
+    else:
+        return profile_name
---
+++ src/maascli/tests/test_profile.py
@@ -0,0 +1,59 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for `maascli.profile`."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+__metaclass__ = type
+__all__ = []
+
+from maascli.profile import (
+    get_profile,
+    InvalidProfile,
+    name_default_profile,
+    select_profile,
+    )
+from maascli.testing.config import make_configs
+from maastesting.factory import factory
+from maastesting.testcase import TestCase
+
+
+class TestProfile(TestCase):
+
+    def test_get_profile_finds_profile(self):
+        profiles = make_configs()
+        [name] = profiles.keys()
+        self.assertEqual(profiles[name], get_profile(profiles, name))
+
+    def test_get_profile_raises_if_not_found(self):
+        profiles = make_configs()
+        self.assertRaises(
+            InvalidProfile,
+            get_profile, profiles, factory.make_name('nonexistent-profile'))
+
+    def test_name_default_profile_picks_single_profile(self):
+        profiles = make_configs(1)
+        [name] = profiles.keys()
+        self.assertEqual(name, name_default_profile(profiles))
+
+    def test_name_default_profile_returns_None_if_no_profile_found(self):
+        self.assertIsNone(name_default_profile(make_configs(0)))
+
+    def test_name_default_profile_returns_None_if_multiple_profiles(self):
+        profiles = make_configs(2)
+        self.assertIsNone(name_default_profile(profiles))
+
+    def test_select_profile_returns_named_profile(self):
+        profiles = make_configs(3)
+        profile_name = profiles.keys()[1]
+        self.assertEqual(profile_name, select_profile(profiles, profile_name))
+
+    def test_select_profile_selects_default_if_no_profile_named(self):
+        profiles = make_configs(1)
+        [name] = profiles.keys()
+        self.assertEqual(name, select_profile(profiles))
