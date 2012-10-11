7520dcac48b61829499955c5b9b77b408ef8726f
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2012-10-11 12:45:51 +0000

    [r=rvb][bug=1064227][author=jtv] Change the DHCP leases parser: an IP/MAC mapping remains current as long as there is an unexpired lease entry for it, or a host declaration without a subsequent rubout declaration.

--- src/provisioningserver/dhcp/leases_parser.py
+++ src/provisioningserver/dhcp/leases_parser.py
@@ -47,29 +47,70 @@ set_statement = (
     Suppress('=') +
     QuotedString('"'))
 
+# For our purposes, leases and host declarations are similar enough that
+# we can parse them as the same construct with different names.
+lease_or_host = oneOf(['lease', 'host'], caseless=True)
+
 hardware = CaselessKeyword("hardware") + hardware_type("type") + mac("mac")
 ends = CaselessKeyword("ends") + expiry("expiry")
-other_statement = (
-    oneOf(
-        ['starts', 'tstp', 'atsfp', 'tsfp', 'cltt', 'uid', 'binding', 'next',
-         'client-hostname', 'abandoned', 'option', 'ddns-text',
-         'ddns-fwd-name', 'ddns-client-fqdn', 'ddns-rev-name',
-         'vendor-class-identifier', 'bootp', 'reserved', 'rewind'],
-        caseless=True) + args
-    )
-lone_statement = (
-    oneOf(['abandoned', 'bootp', 'reserved'], caseless=True))
+deleted = CaselessKeyword("deleted")
+
+lone_statement_names = [
+    'abandoned',
+    'bootp',
+    'deleted',
+    'dynamic',
+    'reserved',
+    ]
+lone_statement = oneOf(lone_statement_names, caseless=True)
+
+other_statement_names = [
+    'atsfp',
+    'binding',
+    'bootp',
+    'client-hostname',
+    'cltt',
+    'ddns-client-fqdn',
+    'ddns-fwd-name',
+    'ddns-rev-name',
+    'ddns-text',
+    'fixed-address',
+    'next',
+    'option',
+    'reserved',
+    'rewind',
+    'starts',
+    'tstp',
+    'tsfp',
+    'uid',
+    'vendor-class-identifier',
+    ]
+other_statement = oneOf(other_statement_names, caseless=True) + args
 
 lease_statement = (
-    hardware | ends | set_statement | lone_statement | other_statement
+    hardware | deleted | ends | set_statement | lone_statement |
+    other_statement
     ) + Suppress(';')
 lease_parser = (
-    CaselessKeyword("lease") + ip("ip") +
+    lease_or_host("lease_or_host") + ip("ip") +
     Suppress('{') +
     Dict(ZeroOrMore(Group(lease_statement))) +
     Suppress('}')
     )
 lease_parser.ignore('#' + restOfLine)
+
+
+def is_lease(entry):
+    """Is `entry` a lease declaration?"""
+    entry_type = entry.lease_or_host.lower()
+    assert entry_type in {'host', 'lease'}, (
+        "Unknown entry type (not a host or lease): %s" % entry_type)
+    return entry_type == 'lease'
+
+
+def is_host(entry):
+    """Is `entry` a host declaration?"""
+    return not is_lease(entry)
 
 
 def get_expiry_date(lease):
@@ -79,6 +120,7 @@ def get_expiry_date(lease):
     :return: A UTC-based timestamp representing the lease's moment of
         expiry, or None if the lease has no expiry date.
     """
+    assert is_lease(lease)
     ends = getattr(lease, 'ends', None)
     if ends is None or len(ends) == 0 or ends.lower() == 'never':
         return None
@@ -93,11 +135,64 @@ def has_expired(lease, now):
     :param now: The current UTC-based timestamp to check expiry against.
     :return: Whether the lease has expired.
     """
+    assert is_lease(lease)
     expiry_date = get_expiry_date(lease)
     if expiry_date is None:
         return False
     else:
         return expiry_date < now
+
+
+def gather_leases(hosts_and_leases):
+    """Find current leases among `hosts_and_leases`."""
+    now = datetime.utcnow()
+    # If multiple leases for the same address are valid at the same
+    # time, for whatever reason, the dict will contain the one that was
+    # last appended to the leases file.
+    return {
+        lease.ip: lease.hardware.mac
+        for lease in filter(is_lease, hosts_and_leases)
+            if not has_expired(lease, now)}
+
+
+def get_host_mac(host):
+    """Get the MAC address from a host declaration.  A rubout has none."""
+    assert is_host(host)
+    if 'deleted' in host:
+        return None
+    hardware = getattr(host, 'hardware', None)
+    if hardware in (None, '', b''):
+        return None
+    else:
+        return hardware.mac
+
+
+def gather_hosts(hosts_and_leases):
+    """Find current host declarations among `hosts_and_leases`."""
+    # Get MAC address mappings for host entries.  A newer entry
+    # overwrites an older one for the same IP address.  A rubout entry
+    # will have no MAC address.
+    host_maps = {
+        host.ip: get_host_mac(host)
+        for host in filter(is_host, hosts_and_leases)}
+    # Now filter out mappings where the last entry was a rubout.
+    return {
+        ip: mac
+        for ip, mac in host_maps.items()
+            if mac is not None}
+
+
+def combine_entries(entries):
+    """Combine the hosts and leases declarations in a parsed leases file.
+
+    :param entries: Parsed host/leases entries from a leases file.
+    :return: A dict mapping leased IP addresses to the respective MAC
+        addresses that currently own them (regardless of whether they
+        were found in a lease or in a host declaration).
+    """
+    leases = gather_leases(entries)
+    leases.update(gather_hosts(entries))
+    return leases
 
 
 def parse_leases(leases_contents):
@@ -107,12 +202,5 @@ def parse_leases(leases_contents):
     :return: A dict mapping each currently leased IP address to the MAC
         address that it is associated with.
     """
-    now = datetime.utcnow()
-    leases = lease_parser.searchString(leases_contents)
-    # If multiple leases for the same address are valid at the same
-    # time, for whatever reason, the dict will contain the one that was
-    # last appended to the leases file.
-    return {
-        lease.ip: lease.hardware.mac
-        for lease in leases
-            if not has_expired(lease, now)}
+    entries = lease_parser.searchString(leases_contents)
+    return combine_entries(entries)
--- src/provisioningserver/dhcp/tests/test_leases_parser.py
+++ src/provisioningserver/dhcp/tests/test_leases_parser.py
@@ -1,7 +1,7 @@
 # Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""..."""
+"""Tests for the DHCP leases parser."""
 
 from __future__ import (
     absolute_import,
@@ -19,21 +19,43 @@ from textwrap import dedent
 from maastesting.factory import factory
 from maastesting.testcase import TestCase
 from provisioningserver.dhcp.leases_parser import (
+    combine_entries,
+    gather_hosts,
+    gather_leases,
     get_expiry_date,
+    get_host_mac,
     has_expired,
+    is_host,
+    is_lease,
+    lease_parser,
     parse_leases,
     )
 
 
 class TestLeasesParser(TestCase):
 
-    def fake_parsed_lease(self, ip=None, mac=None, ends=None):
+    def fake_parsed_lease(self, ip=None, mac=None, ends=None,
+                          entry_type='lease'):
         """Fake a lease as produced by the parser."""
         if ip is None:
             ip = factory.getRandomIPAddress()
         if mac is None:
             mac = factory.getRandomMACAddress()
-        return namedtuple('lease', ['ip', 'mac', 'ends'])(ip, mac, ends)
+        Hardware = namedtuple('Hardware', ['mac'])
+        Lease = namedtuple(
+            'Lease', ['lease_or_host', 'ip', 'hardware', 'ends'])
+        return Lease(entry_type, ip, Hardware(mac), ends)
+
+    def fake_parsed_host(self, ip=None, mac=None):
+        """Fake a host declaration as produced by the parser."""
+        return self.fake_parsed_lease(ip=ip, mac=mac, entry_type='host')
+
+    def fake_parsed_rubout(self, ip=None):
+        """Fake a "rubout" host declaration."""
+        if ip is None:
+            ip = factory.getRandomIPAddress()
+        Rubout = namedtuple('Rubout', ['lease_or_host', 'ip'])
+        return Rubout('host', ip)
 
     def test_get_expiry_date_parses_expiry_date(self):
         lease = self.fake_parsed_lease(ends='0 2011/01/02 03:04:05')
@@ -64,6 +86,129 @@ class TestLeasesParser(TestCase):
         earlier = '1 2001/01/01 00:00:00'
         self.assertTrue(
             has_expired(self.fake_parsed_lease(ends=earlier), now))
+
+    def test_gather_leases_finds_current_leases(self):
+        lease = self.fake_parsed_lease()
+        self.assertEqual(
+            {lease.ip: lease.hardware.mac},
+            gather_leases([lease]))
+
+    def test_gather_leases_ignores_expired_leases(self):
+        earlier = '1 2001/01/01 00:00:00'
+        lease = self.fake_parsed_lease(ends=earlier)
+        self.assertEqual({}, gather_leases([lease]))
+
+    def test_gather_leases_combines_expired_and_current_leases(self):
+        earlier = '1 2001/01/01 00:00:00'
+        ip = factory.getRandomIPAddress()
+        old_owner = factory.getRandomMACAddress()
+        new_owner = factory.getRandomMACAddress()
+        leases = [
+            self.fake_parsed_lease(ip=ip, mac=old_owner, ends=earlier),
+            self.fake_parsed_lease(ip=ip, mac=new_owner),
+            ]
+        self.assertEqual({ip: new_owner}, gather_leases(leases))
+
+    def test_gather_leases_ignores_ordering(self):
+        earlier = '1 2001/01/01 00:00:00'
+        ip = factory.getRandomIPAddress()
+        old_owner = factory.getRandomMACAddress()
+        new_owner = factory.getRandomMACAddress()
+        leases = [
+            self.fake_parsed_lease(ip=ip, mac=new_owner),
+            self.fake_parsed_lease(ip=ip, mac=old_owner, ends=earlier),
+            ]
+        self.assertEqual({ip: new_owner}, gather_leases(leases))
+
+    def test_gather_leases_ignores_host_declarations(self):
+        self.assertEqual({}, gather_leases([self.fake_parsed_host()]))
+
+    def test_gather_hosts_finds_hosts(self):
+        host = self.fake_parsed_host()
+        self.assertEqual({host.ip: host.hardware.mac}, gather_hosts([host]))
+
+    def test_gather_hosts_ignores_unaccompanied_rubouts(self):
+        self.assertEqual({}, gather_hosts([self.fake_parsed_rubout()]))
+
+    def test_gather_hosts_ignores_rubbed_out_entries(self):
+        ip = factory.getRandomIPAddress()
+        hosts = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_rubout(ip=ip),
+            ]
+        self.assertEqual({}, gather_hosts(hosts))
+
+    def test_gather_hosts_follows_reassigned_host(self):
+        ip = factory.getRandomIPAddress()
+        new_owner = factory.getRandomMACAddress()
+        hosts = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_rubout(ip=ip),
+            self.fake_parsed_host(ip=ip, mac=new_owner),
+            ]
+        self.assertEqual({ip: new_owner}, gather_hosts(hosts))
+
+    def test_is_lease_and_is_host_recognize_lease(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        [parsed_lease] = lease_parser.searchString(dedent("""\
+            lease %(ip)s {
+                hardware ethernet %(mac)s;
+            }
+            """ % params))
+        self.assertEqual(
+            (True, False),
+            (is_lease(parsed_lease), is_host(parsed_lease)))
+
+    def test_is_lease_and_is_host_recognize_host(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        [parsed_host] = lease_parser.searchString(dedent("""\
+            host %(ip)s {
+                hardware ethernet %(mac)s;
+            }
+            """ % params))
+        self.assertEqual(
+            (False, True),
+            (is_lease(parsed_host), is_host(parsed_host)))
+
+    def test_get_host_mac_returns_None_for_host(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        [parsed_host] = lease_parser.searchString(dedent("""\
+            host %(ip)s {
+                hardware ethernet %(mac)s;
+            }
+            """ % params))
+        self.assertEqual(params['mac'], get_host_mac(parsed_host))
+
+    def test_get_host_mac_returns_None_for_rubout(self):
+        ip = factory.getRandomIPAddress()
+        [parsed_host] = lease_parser.searchString(dedent("""\
+            host %s {
+                deleted;
+            }
+            """ % ip))
+        self.assertIsNone(get_host_mac(parsed_host))
+
+    def test_get_host_mac_returns_None_for_rubout_even_with_mac(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        [parsed_host] = lease_parser.searchString(dedent("""\
+            host %(ip)s {
+                deleted;
+                hardware ethernet %(mac)s;
+            }
+            """ % params))
+        self.assertIsNone(get_host_mac(parsed_host))
 
     def test_parse_leases_copes_with_empty_file(self):
         self.assertEqual({}, parse_leases(""))
@@ -101,6 +246,28 @@ class TestLeasesParser(TestCase):
             }
             """ % params))
         self.assertEqual({params['ip']: params['mac']}, leases)
+
+    def test_parse_leases_parses_host(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        leases = parse_leases(dedent("""\
+            host %(ip)s {
+                dynamic;
+                hardware ethernet %(mac)s;
+                fixed-address %(ip)s;
+            }
+            """ % params))
+        self.assertEqual({params['ip']: params['mac']}, leases)
+
+    def test_parse_leases_parses_host_rubout(self):
+        leases = parse_leases(dedent("""\
+            host %s {
+                deleted;
+            }
+            """ % factory.getRandomIPAddress()))
+        self.assertEqual({}, leases)
 
     def test_parse_leases_ignores_incomplete_lease_at_end(self):
         params = {
@@ -185,3 +352,103 @@ class TestLeasesParser(TestCase):
             }
             """ % params))
         self.assertEqual({params['ip']: params['new_owner']}, leases)
+
+    def test_parse_leases_recognizes_host_deleted_statement_as_rubout(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        leases = parse_leases(dedent("""\
+            host %(ip)s {
+                dynamic;
+                hardware ethernet %(mac)s;
+                fixed-address %(ip)s;
+                deleted;
+            }
+            """ % params))
+        self.assertEqual({}, leases)
+
+    def test_host_declaration_is_like_an_unexpired_lease(self):
+        params = {
+            'ip': factory.getRandomIPAddress(),
+            'mac': factory.getRandomMACAddress(),
+        }
+        leases = parse_leases(dedent("""\
+            host %(ip)s {
+                hardware ethernet %(mac)s;
+                fixed-address %(ip)s;
+            }
+            """ % params))
+        self.assertEqual({params['ip']: params['mac']}, leases)
+
+    def test_combine_entries_accepts_host_followed_by_expired_lease(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        earlier = '1 2001/01/01 00:00:00'
+        entries = [
+            self.fake_parsed_host(ip=ip, mac=mac),
+            self.fake_parsed_lease(ip=ip, ends=earlier),
+            ]
+        self.assertEqual({ip: mac}, combine_entries(entries))
+
+    def test_combine_entries_accepts_expired_lease_followed_by_host(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        earlier = '1 2001/01/01 00:00:00'
+        entries = [
+            self.fake_parsed_lease(ip=ip, ends=earlier),
+            self.fake_parsed_host(ip=ip, mac=mac),
+            ]
+        self.assertEqual({ip: mac}, combine_entries(entries))
+
+    def test_combine_entries_accepts_rubout_followed_by_current_lease(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        entries = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_rubout(ip=ip),
+            self.fake_parsed_lease(ip=ip, mac=mac),
+            ]
+        self.assertEqual({ip: mac}, combine_entries(entries))
+
+    def test_combine_entries_ignores_rubout_followed_by_expired_lease(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        earlier = '1 2001/01/01 00:00:00'
+        entries = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_rubout(ip=ip),
+            self.fake_parsed_lease(ip=ip, mac=mac, ends=earlier),
+            ]
+        self.assertEqual({}, combine_entries(entries))
+
+    def test_combine_entries_ignores_expired_lease_followed_by_rubout(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        earlier = '1 2001/01/01 00:00:00'
+        entries = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_lease(ip=ip, mac=mac, ends=earlier),
+            self.fake_parsed_rubout(ip=ip),
+            ]
+        self.assertEqual({}, combine_entries(entries))
+
+    def test_combine_entries_accepts_valid_lease_followed_by_rubout(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        entries = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_lease(ip=ip, mac=mac),
+            self.fake_parsed_rubout(ip=ip),
+            ]
+        self.assertEqual({ip: mac}, combine_entries(entries))
+
+    def test_combine_entries_accepts_reassigned_host(self):
+        ip = factory.getRandomIPAddress()
+        mac = factory.getRandomMACAddress()
+        entries = [
+            self.fake_parsed_host(ip=ip),
+            self.fake_parsed_rubout(ip=ip),
+            self.fake_parsed_host(ip=ip, mac=mac),
+            ]
+        self.assertEqual({ip: mac}, combine_entries(entries))
