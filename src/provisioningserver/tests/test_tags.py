283bb8cbd1bef8e3c06575327b3c9a5b0e623c5e
Jelmer Vernooij <jelmer@samba.org>
2012-10-11 13:25:43 +0000

    [r=jameinel][bug=][author=jelmer] Fix the process_node_tags task to use the right max_retries constant.

--- src/provisioningserver/tests/test_tags.py
+++ src/provisioningserver/tests/test_tags.py
@@ -132,10 +132,8 @@ class TestTagUpdating(PservTestCase):
         self.patch(MAASClient, 'get')
         self.patch(MAASClient, 'post')
         tag_name = factory.make_name('tag')
-        try:
-            tags.process_node_tags(tag_name, '//node')
-        except tags.MissingCredentials:
-            pass
+        self.assertRaises(tags.MissingCredentials,
+            tags.process_node_tags, tag_name, '//node')
         self.assertFalse(MAASClient.get.called)
         self.assertFalse(MAASClient.post.called)
 
