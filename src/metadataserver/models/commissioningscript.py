0ca9aecf825ef4115190d1f61acfc4309e122b44
Jeroen Vermeulen <jtv@canonical.com>
2012-11-30 03:25:05 +0000

    [r=julian-edwards][bug=][author=jtv] Model method: create tar archive of all commissioning scripts.

--- src/metadataserver/models/commissioningscript.py
+++ src/metadataserver/models/commissioningscript.py
@@ -15,13 +15,41 @@ __all__ = [
     'CommissioningScript',
     ]
 
+from io import BytesIO
+import os.path
+import tarfile
 
 from django.db.models import (
     CharField,
+    Manager,
     Model,
     )
 from metadataserver import DefaultMeta
 from metadataserver.fields import BinaryField
+
+# Path prefix for commissioning scripts.  Commissioning scripts will be
+# extracted into this directory.
+ARCHIVE_PREFIX = "commissioning.d"
+
+
+class CommissioningScriptManager(Manager):
+    """Utility for the collection of `CommissioningScript`s."""
+
+    def get_archive(self):
+        """Produce a tar archive of all commissioning scripts.
+
+        Each of the scripts will be in the `ARCHIVE_PREFIX` directory.
+        """
+        binary = BytesIO()
+        tarball = tarfile.open(mode='w', fileobj=binary)
+        for script in self.all().order_by('name'):
+            path = os.path.join(ARCHIVE_PREFIX, script.name)
+            tarinfo = tarfile.TarInfo(name=path)
+            tarinfo.size = len(script.content)
+            tarball.addfile(tarinfo, BytesIO(script.content))
+        tarball.close()
+        binary.seek(0)
+        return binary.read()
 
 
 class CommissioningScript(Model):
@@ -33,6 +61,8 @@ class CommissioningScript(Model):
 
     class Meta(DefaultMeta):
         """Needed for South to recognize this model."""
+
+    objects = CommissioningScriptManager()
 
     name = CharField(max_length=255, null=False, editable=False, unique=True)
     content = BinaryField(null=False)
--- src/metadataserver/tests/test_commissioningscript.py
+++ src/metadataserver/tests/test_commissioningscript.py
@@ -12,13 +12,22 @@ from __future__ import (
 __metaclass__ = type
 __all__ = []
 
+from io import BytesIO
+import os.path
 from random import randint
+import tarfile
 
 from maasserver.testing.factory import factory
 from maasserver.testing.testcase import TestCase
 from maastesting.utils import sample_binary_data
 from metadataserver.fields import Bin
 from metadataserver.models import CommissioningScript
+from metadataserver.models.commissioningscript import ARCHIVE_PREFIX
+
+
+def open_tarfile(content):
+    """Open tar file from raw binary data."""
+    return tarfile.open(fileobj=BytesIO(content))
 
 
 def make_script_name(base_name=None, number=None):
@@ -29,6 +38,36 @@ def make_script_name(base_name=None, number=None):
         number = randint(0, 99)
     return factory.make_name(
         '%0.2d-%s' % (number, factory.make_name(base_name)))
+
+
+class TestCommissioningScriptManager(TestCase):
+
+    def test_get_archive_wraps_scripts_in_tar(self):
+        script = factory.make_commissioning_script()
+        archive = open_tarfile(CommissioningScript.objects.get_archive())
+        archived_script = archive.next()
+        self.assertTrue(archived_script.isfile())
+        self.assertEqual(
+            os.path.join(ARCHIVE_PREFIX, script.name),
+            archived_script.name)
+        self.assertEqual(
+            script.content,
+            archive.extractfile(archived_script).read())
+
+    def test_get_archive_wraps_all_scripts(self):
+        scripts = {factory.make_commissioning_script() for counter in range(3)}
+        archive = open_tarfile(CommissioningScript.objects.get_archive())
+        self.assertItemsEqual(
+            {os.path.join(ARCHIVE_PREFIX, script.name) for script in scripts},
+            archive.getnames())
+
+    def test_get_archive_supports_binary_scripts(self):
+        script = factory.make_commissioning_script(content=sample_binary_data)
+        archive = open_tarfile(CommissioningScript.objects.get_archive())
+        archived_script = archive.next()
+        self.assertEqual(
+            script.content,
+            archive.extractfile(archived_script).read())
 
 
 class TestCommissioningScript(TestCase):
