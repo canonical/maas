a0d9aeca1d1635a133ba36f9c52f9fc53fccad35
Andres Rodriguez <andreserl@ubuntu.com>
2013-01-15 03:06:12 +0000

    [r=andreserl][bug=][author=andreserl] Fix minor formatting issue.

--- src/maasserver/power_parameters.py
+++ src/maasserver/power_parameters.py
@@ -34,9 +34,9 @@ from django import forms
 from maasserver.config_forms import DictCharField
 from maasserver.fields import MACAddressFormField
 from provisioningserver.enum import (
-    POWER_TYPE,
+    IPMI_DRIVER,
     IPMI_DRIVER_CHOICES,
-    IPMI_DRIVER
+    POWER_TYPE,
     )
 
 
