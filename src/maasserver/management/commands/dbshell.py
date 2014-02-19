5e6b1cf496f2d595858f276232d5d563dc3f2acb
jtv at canonical.com <jtv@canonical.com>
2014-02-19 11:05:55 +0000

    [r=allenap][bug=1058126][author=jtv] Make ‘dbshell’ management command work for installed MAAS systems.

--- src/maasserver/management/commands/dbshell.py
+++ src/maasserver/management/commands/dbshell.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2014 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: start a database shell.
@@ -17,15 +17,53 @@ str = None
 __metaclass__ = type
 __all__ = ['Command']
 
+from optparse import make_option
+import subprocess
+
+from django.core.management.base import (
+    BaseCommand,
+    CommandError,
+    )
 from django.core.management.commands import dbshell
-from maasserver.testing.database import MAASClusterFixture
 
 
 class Command(dbshell.Command):
     """Customized "dbshell" command."""
 
+    help = "Interactive psql shell for the MAAS database."
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--database', default=None, help="Database to connect to."),
+        make_option(
+            '--installed', '-i', action='store_true', default=False,
+            help=(
+                "Connect to global, system-installed database.  "
+                "Default is to start, and connect to, database in a "
+                "development branch.")),
+        )
+
     def handle(self, **options):
-        # Don't call up to Django's dbshell, because that ends up exec'ing the
-        # shell, preventing this from clearing down the fixture.
-        with MAASClusterFixture(options.get('database')) as cluster:
-            cluster.shell(cluster.dbname)
+        if options.get('installed'):
+            # Access the global system-installed MAAS database.
+            database = options.get('database')
+            if database is None:
+                database = 'maasdb'
+            try:
+                subprocess.check_call(
+                    ['sudo', '-u', 'postgres', 'psql', database])
+            except subprocess.CalledProcessError:
+                # If psql fails to run, it will print a message to stderr.
+                # Capturing that can get a little involved; psql might think
+                # it was running noninteractively.  So just produce a standard
+                # error message, and rely on the stderr output coming from
+                # psql itself.
+                raise CommandError("psql failed.")
+        else:
+            # Don't call up to Django's dbshell, because that ends up exec'ing
+            # the shell, preventing this from clearing down the fixture.
+            # Import fixture here, because installed systems won't have it.
+            from maasserver.testing import database
+            cluster = database.MAASClusterFixture(options.get('database'))
+            with cluster:
+                cluster.shell(cluster.dbname)
---
+++ src/maasserver/tests/test_commands_dbshell.py
@@ -0,0 +1,79 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for the `dbshell` management command."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = []
+
+import subprocess
+
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from maasserver.testing import database as database_module
+from maasserver.testing.factory import factory
+from maasserver.testing.testcase import MAASServerTestCase
+import mock
+
+
+class TestDBShell(MAASServerTestCase):
+
+    def test_runs_local_cluster_by_default(self):
+        dbname = factory.make_name('db')
+        cluster = self.patch(database_module, 'MAASClusterFixture')
+        cluster.return_value.dbname = dbname
+        call_command('dbshell')
+        self.assertEqual(
+            [
+                mock.call(None),
+                mock.call().__enter__(),
+                mock.call().shell(dbname),
+                mock.call().__exit__(None, None, None),
+            ],
+            cluster.mock_calls)
+
+    def test_local_run_obeys_database_option_if_given(self):
+        dbname = factory.make_name('db')
+        cluster = self.patch(database_module, 'MAASClusterFixture')
+        cluster.return_value.dbname = dbname
+        call_command('dbshell', database=dbname)
+        self.assertEqual(
+            [
+                mock.call(dbname),
+                mock.call().__enter__(),
+                mock.call().shell(dbname),
+                mock.call().__exit__(None, None, None),
+            ],
+            cluster.mock_calls)
+
+    def test_installed_option_connects_to_installed_cluster(self):
+        check_call = self.patch(subprocess, 'check_call')
+        call_command('dbshell', installed=True, database=None)
+        self.assertEqual(
+            [mock.call(['sudo', '-u', 'postgres', 'psql', 'maasdb'])],
+            check_call.mock_calls)
+
+    def test_installed_run_obeys_database_option_if_given(self):
+        dbname = factory.make_name('db')
+        check_call = self.patch(subprocess, 'check_call')
+        call_command('dbshell', installed=True, database=dbname)
+        self.assertEqual(
+            [mock.call(['sudo', '-u', 'postgres', 'psql', dbname])],
+            check_call.mock_calls)
+
+    def test_installed_run_raises_errors_as_CommandError(self):
+        self.patch(
+            subprocess, 'check_call',
+            mock.MagicMock(side_effect=subprocess.CalledProcessError(
+                99, ['command', 'line'])))
+        error = self.assertRaises(
+            CommandError, call_command, 'dbshell', installed=True)
+        self.assertEquals("psql failed.", unicode(error))
