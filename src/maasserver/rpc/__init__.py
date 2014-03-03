076f60dddcb651b3ab1eed08ea51fb4a8384de21
Gavin Panella <gavin.panella@canonical.com>
2014-03-03 04:59:36 +0000

    [r=rvb][bug=][author=allenap] Convenient top-level API for making RPCs to clusters.

---
+++ src/maasserver/rpc/__init__.py
@@ -0,0 +1,44 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Region Controller RPC."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = [
+    "getClientFor",
+    "getAllClients",
+]
+
+from maasserver import eventloop
+from provisioningserver.rpc import errors
+from provisioningserver.utils import asynchronous
+
+
+@asynchronous
+def getClientFor(uuid):
+    """Get a client with which to make RPCs to the specified cluster."""
+    try:
+        service = eventloop.services.getServiceNamed("rpc")
+    except KeyError:
+        raise errors.NoConnectionsAvailable()
+    else:
+        return service.getClientFor(uuid)
+
+
+@asynchronous
+def getAllClients():
+    """Get all recorded clients ready to make RPCs to clusters."""
+    try:
+        service = eventloop.services.getServiceNamed("rpc")
+    except KeyError:
+        return []
+    else:
+        return service.getAllClients()
---
+++ src/maasserver/rpc/tests/test_module.py
@@ -0,0 +1,58 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for the top-level region RPC API."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = []
+
+from crochet import wait_for_reactor
+from maasserver import (
+    eventloop,
+    rpc,
+    )
+from maastesting.matchers import MockCalledOnceWith
+from maastesting.testcase import MAASTestCase
+from mock import sentinel
+from provisioningserver.rpc import errors
+from testtools.matchers import (
+    Equals,
+    Is,
+    )
+
+
+class TestFunctions(MAASTestCase):
+
+    @wait_for_reactor
+    def test_getClientFor_service_not_running(self):
+        self.assertRaises(
+            errors.NoConnectionsAvailable,
+            rpc.getClientFor, sentinel.uuid)
+
+    @wait_for_reactor
+    def test_getClientFor(self):
+        getServiceNamed = self.patch(eventloop.services, "getServiceNamed")
+        getClientFor = getServiceNamed.return_value.getClientFor
+        getClientFor.return_value = sentinel.client
+        self.assertThat(getClientFor(sentinel.uuid), Is(sentinel.client))
+        self.assertThat(getClientFor, MockCalledOnceWith(sentinel.uuid))
+
+    @wait_for_reactor
+    def test_getAllClients_service_not_running(self):
+        self.assertThat(rpc.getAllClients(), Equals([]))
+
+    @wait_for_reactor
+    def test_getAllClients(self):
+        getServiceNamed = self.patch(eventloop.services, "getServiceNamed")
+        getAllClients = getServiceNamed.return_value.getAllClients
+        getAllClients.return_value = sentinel.clients
+        self.assertThat(getAllClients(), Is(sentinel.clients))
+        self.assertThat(getAllClients, MockCalledOnceWith())
