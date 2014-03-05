0bbd629f639f0c10b579c468cedcd44f237bc664
Graham Binns <graham.binns@canonical.com>
2014-03-05 01:35:53 +0000

    [r=gmb][bug=][author=gmb] Add an async module to src/maasserver/utils. This introduces utilities that we can use when working with region/cluster RPCs.

---
+++ src/maasserver/utils/async.py
@@ -0,0 +1,96 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Utilities for working with asynchronous operations."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = [
+    "gather",
+]
+
+from itertools import count
+from Queue import Queue
+
+from crochet import wait_for_reactor
+from twisted.internet import reactor
+from twisted.internet.defer import maybeDeferred
+from twisted.python import log
+
+
+@wait_for_reactor
+def gather(calls, timeout=10.0):
+    """Issue calls into the reactor, passing results back to another thread.
+
+    :param calls: An iterable of no-argument callables to be called in
+        the reactor thread. Each will be called via `maybeDeferred`.
+
+    :param timeout: The number of seconds before further results are
+        ignored. Outstanding results (i.e. `Deferred`s) will be
+        cancelled.
+
+    :return: An iterator of results. A result might be a failure,
+        i.e. an instance of :class:`twisted.python.failure.Failure`, or
+        a valid result; it's up to the caller to check.
+    """
+
+    # Prepare of a list of Deferreds that we're going to wait for.
+    deferreds = [maybeDeferred(call) for call in calls]
+
+    # We'll use this queue (thread-safe) to pass results back.
+    queue = Queue()
+
+    # A sentinel to mark the end of the results.
+    done = object()
+
+    # This function will get called if not all results are in before
+    # `timeout` seconds have passed. It puts `done` into the queue to
+    # indicate the end of results, and cancels all outstanding deferred
+    # calls.
+    def cancel():
+        queue.put(done)
+        for deferred in deferreds:
+            try:
+                deferred.cancel()
+            except:
+                log.err()
+
+    if timeout is None:
+        canceller = None
+    else:
+        canceller = reactor.callLater(timeout, cancel)
+
+    countdown = count(len(deferreds), -1)
+
+    # Callback to report the result back to the queue. If it's the last
+    # result to be reported, `done` is put into the queue, and the
+    # delayed call to `cancel` is itself cancelled.
+    def report(result):
+        queue.put(result)
+        if next(countdown) == 1:
+            queue.put(done)
+            if canceller is not None:
+                if canceller.active():
+                    canceller.cancel()
+
+    for deferred in deferreds:
+        deferred.addBoth(report)
+
+    # If there are no calls then there will be no results, so we put
+    # `done` into the queue, and cancel the nascent delayed call to
+    # `cancel`, if it exists.
+    if len(deferreds) == 0:
+        queue.put(done)
+        if canceller is not None:
+            canceller.cancel()
+
+    # Return an iterator to the invoking thread that will stop at the
+    # first sign of the `done` sentinel.
+    return iter(queue.get, done)
---
+++ src/maasserver/utils/tests/test_async.py
@@ -0,0 +1,91 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for asynchronous utilities."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = []
+
+from functools import partial
+from time import time
+
+from maasserver import eventloop
+from maasserver.utils import async
+from maastesting.testcase import MAASTestCase
+from mock import sentinel
+from testtools.matchers import (
+    Contains,
+    Equals,
+    HasLength,
+    Is,
+    IsInstance,
+    LessThan,
+    )
+from twisted.internet import reactor
+from twisted.internet.task import deferLater
+from twisted.python.failure import Failure
+
+
+eventloop.start()
+
+
+class TestGather(MAASTestCase):
+
+    def test_gather_nothing(self):
+        time_before = time()
+        results = list(async.gather([], timeout=10))
+        time_after = time()
+        self.assertThat(results, Equals([]))
+        # gather() should return well within 9 seconds; this shows
+        # that the call is not timing out.
+        self.assertThat(time_after - time_before, LessThan(9))
+
+
+class TestGatherScenarios(MAASTestCase):
+
+    scenarios = (
+        ("synchronous", {
+            # Return the call as-is.
+            "wrap": lambda call: call,
+        }),
+        ("asynchronous", {
+            # Defer the call to a later reactor iteration.
+            "wrap": lambda call: partial(deferLater, reactor, 0, call),
+        }),
+    )
+
+    def test_gather_from_calls_without_errors(self):
+        values = [
+            self.getUniqueInteger(),
+            self.getUniqueString(),
+        ]
+        calls = [
+            self.wrap(lambda v=value: v)
+            for value in values
+        ]
+        results = list(async.gather(calls))
+
+        self.assertItemsEqual(values, results)
+
+    def test_gather_from_calls_with_errors(self):
+        calls = [
+            (lambda: sentinel.okay),
+            (lambda: 1 / 0),  # ZeroDivisionError
+        ]
+        calls = [self.wrap(call) for call in calls]
+        results = list(async.gather(calls))
+
+        self.assertThat(results, Contains(sentinel.okay))
+        results.remove(sentinel.okay)
+        self.assertThat(results, HasLength(1))
+        failure = results[0]
+        self.assertThat(failure, IsInstance(Failure))
+        self.assertThat(failure.type, Is(ZeroDivisionError))
