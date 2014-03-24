b62a16638ec26d89f515d7a41ea59051f94eb904
Gavin Panella <gavin.panella@canonical.com>
2014-03-24 10:52:18 +0000

    [r=julian-edwards][bug=][author=allenap] Convenience module for using PostgreSQL's advisory locks.

---
+++ src/maasserver/utils/dblocks.py
@@ -0,0 +1,79 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Region-wide advisory locking."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = [
+    "startup",
+]
+
+from contextlib import closing
+from operator import itemgetter
+
+from django.db import connection
+
+# The fixed classid used for all MAAS locks. See `DatabaseLock` for the
+# rationale, and an explanation of this number's origin.
+classid = 20120116
+
+
+class DatabaseLock(tuple):
+    """An advisory lock held in the database.
+
+    Implemented using PostgreSQL's advisory locking functions.
+
+    PostgreSQL's advisory lock functions are all available with a choice
+    of two call signatures: (64-bit integer) or (32-bit integer, 32-bit
+    integer). Here we use the two-argument form.
+
+    The first argument is fixed at 20120116. This makes it easy to
+    identify locks belonging to the MAAS application in PostgreSQL's
+    ``pg_locks`` table. For example::
+
+      SELECT objid FROM pg_locks WHERE classid = 20120116;
+
+    returns the second part of the lock key for all locks associated
+    with the MAAS application.
+
+    Fwiw, 20120116 is the date on which source history for MAAS began.
+    It has no special significance to PostgreSQL, as far as I am aware.
+
+    """
+
+    __slots__ = ()
+
+    classid = property(itemgetter(0))
+    objid = property(itemgetter(1))
+
+    def __new__(cls, objid):
+        return super(cls, DatabaseLock).__new__(cls, (classid, objid))
+
+    def __enter__(self):
+        with closing(connection.cursor()) as cursor:
+            cursor.execute("SELECT pg_advisory_lock(%s, %s)", self)
+
+    def __exit__(self, *exc_info):
+        with closing(connection.cursor()) as cursor:
+            cursor.execute("SELECT pg_advisory_unlock(%s, %s)", self)
+
+    def __repr__(self):
+        return b"<%s classid=%d objid=%d>" % (
+            self.__class__.__name__, self[0], self[1])
+
+    def is_locked(self):
+        stmt = "SELECT 1 FROM pg_locks WHERE classid = %s AND objid = %s"
+        with closing(connection.cursor()) as cursor:
+            cursor.execute(stmt, self)
+            return len(cursor.fetchall()) >= 1
+
+
+startup = DatabaseLock(1)
---
+++ src/maasserver/utils/tests/test_dblocks.py
@@ -0,0 +1,71 @@
+# Copyright 2014 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for `maasserver.utils.dblocks`."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = []
+
+from contextlib import closing
+
+from django.db import connection
+from maasserver.utils import dblocks
+from maastesting.testcase import MAASTestCase
+
+
+def get_locks():
+    """Return the set of locks held."""
+    stmt = "SELECT objid FROM pg_locks WHERE classid = %s"
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(stmt, [dblocks.classid])
+        return {result[0] for result in cursor.fetchall()}
+
+
+class TestDatabaseLock(MAASTestCase):
+
+    def test_create_lock(self):
+        objid = self.getUniqueInteger()
+        lock = dblocks.DatabaseLock(objid)
+        self.assertEqual(lock, (dblocks.classid, objid))
+
+    def test_properties(self):
+        lock = dblocks.DatabaseLock(self.getUniqueInteger())
+        self.assertEqual(lock, (lock.classid, lock.objid))
+
+    def test_lock_actually_locked(self):
+        objid = self.getUniqueInteger()
+        lock = dblocks.DatabaseLock(objid)
+
+        locks_held_before = get_locks()
+        with lock:
+            locks_held = get_locks()
+        locks_held_after = get_locks()
+
+        locks_obtained = locks_held - locks_held_before
+        self.assertEqual({objid}, locks_obtained)
+
+        locks_released = locks_held - locks_held_after
+        self.assertEqual({objid}, locks_released)
+
+    def test_is_locked(self):
+        objid = self.getUniqueInteger()
+        lock = dblocks.DatabaseLock(objid)
+
+        self.assertFalse(lock.is_locked())
+        with lock:
+            self.assertTrue(lock.is_locked())
+        self.assertFalse(lock.is_locked())
+
+    def test_repr(self):
+        lock = dblocks.DatabaseLock(self.getUniqueInteger())
+        self.assertEqual(
+            "<DatabaseLock classid=%d objid=%d>" % lock,
+            repr(lock))
