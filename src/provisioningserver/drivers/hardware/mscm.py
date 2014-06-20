93f4de2b72e572198116e4d19b927691c09c28f7
Newell Jensen <newell.jensen@canonical.com>
2014-06-20 19:09:30 +0000

    [r=jason-hobbs][bug=][author=newell-jensen] Adding discover, power, and pxe boot support for MSCM.

--- src/provisioningserver/drivers/hardware/mscm.py
+++ src/provisioningserver/drivers/hardware/mscm.py
@@ -18,6 +18,8 @@ str = None
 __metaclass__ = type
 __all__ = []
 
+import re
+
 from paramiko import (
     AutoAddPolicy,
     SSHClient,
@@ -46,3 +48,58 @@ class MSCM_CLI_API:
             self._ssh.close()
 
         return output
+
+    def discover_nodes(self):
+        """Discover all available nodes.
+
+        Example of stdout from running "show node list":
+
+        'show node list\r\r\nSlot ID    Proc Manufacturer
+        Architecture         Memory Power Health\r\n----
+        ----- ---------------------- --------------------
+        ------ ----- ------\r\n 01  c1n1  Intel Corporation
+        x86 Architecture     32 GB  On    OK \r\n 02  c2n1
+        N/A                    No Asset Information \r\n\r\n'
+
+        The regex 'c\d+n\d' is finding the node_id's c1-45n1-8
+        """
+        node_list = self._run_cli_command("show node list")
+        return re.findall(r'c\d+n\d', node_list)
+
+    def get_node_macaddr(self, node):
+        """Get node MAC address(es).
+
+        Example of stdout from running "show node macaddr <xnode_id>":
+
+        'show node macaddr c1n1\r\r\nSlot ID    NIC 1 (Switch A)
+        NIC 2 (Switch B)  NIC 3 (Switch A)  NIC 4 (Switch B)\r\n
+        ---- ----- ----------------- ----------------- -----------------
+        -----------------\r\n  1  c1n1  a0:1d:48:b5:04:34 a0:1d:48:b5:04:35
+        a0:1d:48:b5:04:36 a0:1d:48:b5:04:37\r\n\r\n\r\n'
+
+        The regex '[\:]'.join(['[0-9A-F]{1,2}'] * 6) is finding
+        the MAC Addresses for the given node_id.
+        """
+        macs = self._run_cli_command("show node macaddr %s" % node)
+        return re.findall(r':'.join(['[0-9a-f]{2}'] * 6), macs)
+
+    def power_node_on(self, node):
+        """Power node on."""
+        return self._run_cli_command("set node power on %s" % node)
+
+    def power_node_off(self, node):
+        """Power node off."""
+        return self._run_cli_command("set node power off %s" % node)
+
+    def configure_node_boot_pxe(self, node):
+        """Configure PXE boot for node."""
+        return self._run_cli_command("set node boot pxe %s" % node)
+
+    def configure_node_bootonce_pxe(self, node):
+        """Configure PXE boot for node once."""
+        return self._run_cli_command("set node bootonce pxe %s" % node)
+
+    def get_node_arch(self, node):
+        """Get node architecture."""
+        node_detail = self._run_cli_command("show node detail %s" % node)
+        return node_detail.split('CPU: ')[1].splitlines()[0]
--- src/provisioningserver/drivers/hardware/tests/test_mscm.py
+++ src/provisioningserver/drivers/hardware/tests/test_mscm.py
@@ -14,6 +14,8 @@ str = None
 __metaclass__ = type
 __all__ = []
 
+from random import randint
+import re
 from StringIO import StringIO
 
 from maastesting.factory import factory
@@ -29,6 +31,23 @@ def make_mscm_api():
     username = factory.make_name('user')
     password = factory.make_name('password')
     return MSCM_CLI_API(host, username, password)
+
+
+def make_node_id():
+    """Make a node_id."""
+    return 'c%sn%s' % (randint(1, 45), randint(1, 8))
+
+
+def make_show_node_list(length=10):
+    """Make a fake return value for discover_nodes."""
+    return re.findall(r'c\d+n\d', ''.join(make_node_id()
+                                          for _ in xrange(length)))
+
+
+def make_show_node_macaddr(length=10):
+    """Make a fake return value for get_node_macaddr."""
+    return ''.join((factory.getRandomMACAddress() + ' ')
+                   for _ in xrange(length))
 
 
 class TestRunCliCommand(MAASTestCase):
@@ -66,3 +85,72 @@ class TestRunCliCommand(MAASTestCase):
         command = factory.make_name('command')
         self.assertRaises(Exception, api._run_cli_command, command)
         self.assertThat(ssh_mock.close, MockCalledOnceWith())
+
+
+class TestDiscoverNodes(MAASTestCase):
+    """Tests for ``MSCM_CLI_API.discover_nodes``."""
+
+    def test_discover_nodes(self):
+        api = make_mscm_api()
+        ssh_mock = self.patch(api, '_ssh')
+        expected = make_show_node_list()
+        stdout = StringIO(expected)
+        streams = factory.make_streams(stdout=stdout)
+        ssh_mock.exec_command = Mock(return_value=streams)
+        output = api.discover_nodes()
+        self.assertEqual(expected, output)
+
+
+class TestPowerAndConfigureNode(MAASTestCase):
+    """Tests for ``MSCM_CLI_API.power_node_on,
+    MSCM_CLI_API.power_node_off, ``MSCM_CLI_API.configure_node_boot_pxe
+    and MSCM_CLI_API.configure_node_bootonce_pxe``.
+    """
+
+    scenarios = [
+        ('power_node_on()',
+            dict(method='power_node_on')),
+        ('power_node_off()',
+            dict(method='power_node_off')),
+        ('configure_node_boot_pxe()',
+            dict(method='configure_node_boot_pxe')),
+        ('configure_node_bootonce_pxe()',
+            dict(method='configure_node_bootonce_pxe')),
+    ]
+
+    def test_returns_expected_outout(self):
+        api = make_mscm_api()
+        ssh_mock = self.patch(api, '_ssh')
+        expected = factory.make_name('output')
+        stdout = StringIO(expected)
+        streams = factory.make_streams(stdout=stdout)
+        ssh_mock.exec_command = Mock(return_value=streams)
+        output = getattr(api, self.method)(make_node_id())
+        self.assertEqual(expected, output)
+
+
+class TestNodeMACAddress(MAASTestCase):
+    """Tests for ``MSCM_CLI_API.get_node_macaddr``."""
+
+    def test_get_node_macaddr(self):
+        api = make_mscm_api()
+        expected = make_show_node_macaddr()
+        cli_mock = self.patch(api, '_run_cli_command')
+        cli_mock.return_value = expected
+        node_id = make_node_id()
+        output = api.get_node_macaddr(node_id)
+        self.assertEqual(re.findall(r':'.join(['[0-9a-f]{2}'] * 6),
+                                    expected), output)
+
+
+class TestNodeArch(MAASTestCase):
+    """Tests for ``MSCM_CLI_API.get_node_arch``."""
+
+    def test_get_node_arch(self):
+        api = make_mscm_api()
+        expected = '\r\n    CPU: Some Architecture\r\n'
+        cli_mock = self.patch(api, '_run_cli_command')
+        cli_mock.return_value = expected
+        node_id = make_node_id()
+        output = api.get_node_arch(node_id)
+        self.assertEqual(expected.split('CPU: ')[1].splitlines()[0], output)
