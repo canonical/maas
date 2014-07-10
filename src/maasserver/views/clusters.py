95e0049d9dbb2ae781d8cc37634f1f81f9eb93e1
jtv at canonical.com <jtv@canonical.com>
2014-07-10 13:43:01 +0000

    [r=gmb][bug=][author=jtv] When confirming deletion of a cluster interface, show its name, not its network interface.

--- src/maasserver/views/clusters.py
+++ src/maasserver/views/clusters.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2014 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Cluster views."""
@@ -206,7 +206,7 @@ class ClusterInterfaceDelete(DeleteView):
     def delete(self, request, *args, **kwargs):
         interface = self.get_object()
         interface.delete()
-        messages.info(request, "Interface %s deleted." % interface.interface)
+        messages.info(request, "Interface %s deleted." % interface.name)
         return HttpResponseRedirect(self.get_next_url())
 
 
