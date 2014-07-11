2907091e069a7e34ebecc89c03baa638c878f822
Gavin Panella <gavin.panella@canonical.com>
2014-07-11 21:24:54 +0100

    [r=allenap][bug=][author=allenap] Merge related RPC tests into the same test module.

--- src/provisioningserver/rpc/tests/test_osystems.py
+++ src/provisioningserver/rpc/tests/test_osystems.py
@@ -14,6 +14,8 @@ str = None
 __metaclass__ = type
 __all__ = []
 
+from collections import Iterable
+
 from maastesting.factory import factory
 from maastesting.matchers import MockCalledOnceWith
 from maastesting.testcase import MAASTestCase
@@ -23,6 +25,78 @@ from provisioningserver.rpc import (
     exceptions,
     osystems,
     )
+from provisioningserver.rpc.testing.doubles import StubOS
+
+
+class TestListOperatingSystemHelpers(MAASTestCase):
+
+    def test_gen_operating_systems_returns_dicts_for_registered_oses(self):
+        # Patch in some operating systems with some randomised data. See
+        # StubOS for details of the rules that are used to populate the
+        # non-random elements.
+        os1 = StubOS("kermit", [
+            ("statler", "Statler"),
+            ("waldorf", "Waldorf"),
+        ])
+        os2 = StubOS("fozzie", [
+            ("swedish-chef", "Swedish-Chef"),
+            ("beaker", "Beaker"),
+        ])
+        self.patch(
+            osystems, "OperatingSystemRegistry",
+            [(os1.name, os1), (os2.name, os2)])
+        # The `releases` field in the dict returned is populated by
+        # gen_operating_system_releases. That's not under test, so we
+        # mock it.
+        gen_operating_system_releases = self.patch(
+            osystems, "gen_operating_system_releases")
+        gen_operating_system_releases.return_value = sentinel.releases
+        # The operating systems are yielded in name order.
+        expected = [
+            {
+                "name": "fozzie",
+                "title": "Fozzie",
+                "releases": sentinel.releases,
+                "default_release": "swedish-chef",
+                "default_commissioning_release": "beaker",
+            },
+            {
+                "name": "kermit",
+                "title": "Kermit",
+                "releases": sentinel.releases,
+                "default_release": "statler",
+                "default_commissioning_release": "waldorf",
+            },
+        ]
+        observed = osystems.gen_operating_systems()
+        self.assertIsInstance(observed, Iterable)
+        self.assertEqual(expected, list(observed))
+
+    def test_gen_operating_system_releases_returns_dicts_for_releases(self):
+        # Use an operating system with some randomised data. See StubOS
+        # for details of the rules that are used to populate the
+        # non-random elements.
+        osystem = StubOS("fozzie", [
+            ("swedish-chef", "I Am The Swedish-Chef"),
+            ("beaker", "Beaker The Phreaker"),
+        ])
+        expected = [
+            {
+                "name": "swedish-chef",
+                "title": "I Am The Swedish-Chef",
+                "requires_license_key": False,
+                "can_commission": False,
+            },
+            {
+                "name": "beaker",
+                "title": "Beaker The Phreaker",
+                "requires_license_key": True,
+                "can_commission": True,
+            },
+        ]
+        observed = osystems.gen_operating_system_releases(osystem)
+        self.assertIsInstance(observed, Iterable)
+        self.assertEqual(expected, list(observed))
 
 
 class TestValidateLicenseKeyErrors(MAASTestCase):
