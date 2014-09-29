308b5cc1ef1f45ef088ca63c6f84a5807bba6053
jtv at canonical.com <jtv@canonical.com>
2014-09-29 09:51:55 +0000

    [r=allenap][bug=][author=jtv] Move the code for generating udev rules server-side from maasserver to provisioningserver.  Needed to allow the OS-specific code in the cluster to generate its own Curtin networking preseed.

--- src/maasserver/tests/test_networking_preseed.py
+++ src/maasserver/tests/test_networking_preseed.py
@@ -31,7 +31,6 @@ from maasserver.networking_preseed import (
     compose_debian_network_interfaces_file,
     compose_debian_network_interfaces_ipv4_stanza,
     compose_debian_network_interfaces_ipv6_stanza,
-    compose_linux_udev_rules_file,
     extract_mac_string,
     extract_network_interfaces,
     generate_dns_server_entry,
@@ -886,17 +885,3 @@ class TestComposeDebianNetworkInterfacesFile(MAASServerTestCase):
 
         self.assertIn('auto %s' % interface, interfaces_file)
         self.assertEqual(1, interfaces_file.count('auto %s' % interface))
-
-
-class TestComposeLinuxUdevRulesFile(MAASServerTestCase):
-
-    def test__generates_udev_rule(self):
-        interface = factory.make_name('eth')
-        mac = factory.make_mac_address()
-        expected_rule = (
-            '\nSUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", '
-            'ATTR{address}=="%(mac)s", NAME="%(interface)s"\n'
-            ) % {'mac': mac, 'interface': interface}
-        self.assertIn(
-            expected_rule,
-            compose_linux_udev_rules_file([(interface, mac)]))
