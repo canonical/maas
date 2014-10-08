0f6555b2c2406ef2dc60340674c0fd7850ce4e9b
Gavin Panella <gavin.panella@canonical.com>
2014-10-08 15:20:32 +0000

    [r=rvb,jtv][bug=][author=allenap] New helper, make_bullet_list().

--- src/provisioningserver/utils/tests/test_text.py
+++ src/provisioningserver/utils/tests/test_text.py
@@ -14,9 +14,14 @@ str = None
 __metaclass__ = type
 __all__ = []
 
+from textwrap import dedent
+
 from maastesting.factory import factory
 from maastesting.testcase import MAASTestCase
-from provisioningserver.utils.text import normalise_whitespace
+from provisioningserver.utils.text import (
+    make_bullet_list,
+    normalise_whitespace,
+    )
 
 
 class TestNormaliseWhitespace(MAASTestCase):
@@ -39,3 +44,36 @@ class TestNormaliseWhitespace(MAASTestCase):
     def test__treats_punctuation_as_non_space(self):
         punctuation = '.?;:!'
         self.assertEqual(punctuation, normalise_whitespace(punctuation))
+
+
+class TestMakeBulletList(MAASTestCase):
+
+    def test__returns_empty_string_when_there_are_no_messages(self):
+        self.assertEqual("", make_bullet_list([]))
+
+    def test__wraps_at_72_columns(self):
+        lines = make_bullet_list([" -" * 50]).splitlines()
+        self.assertEqual(72, max(len(line) for line in lines))
+
+    def test__fills_and_formats(self):
+        messages = [
+            """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas a lorem pellentesque, dapibus lorem ut, blandit ex.""",
+            """Nulla tristique quam sed suscipit cursus""",
+            """Integer euismod viverra ipsum, id placerat ante interdum vitae.
+            Mauris fermentum ut nisi vitae tincidunt. Maecenas posuere lacus
+            vel est dignissim vehicula. Vestibulum tristique, massa non
+            facilisis mattis, nisi lacus lacinia neque, nec convallis risus
+            turpis id metus. Aenean semper sapien sed volutpat volutpat.""",
+        ]
+        bullet_list = make_bullet_list(messages)
+        bullet_list_expected = dedent("""\
+        * Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Maecenas a lorem pellentesque, dapibus lorem ut, blandit ex.
+        * Nulla tristique quam sed suscipit cursus
+        * Integer euismod viverra ipsum, id placerat ante interdum vitae.
+          Mauris fermentum ut nisi vitae tincidunt. Maecenas posuere lacus
+          vel est dignissim vehicula. Vestibulum tristique, massa non
+          facilisis mattis, nisi lacus lacinia neque, nec convallis risus
+          turpis id metus. Aenean semper sapien sed volutpat volutpat.""")
+        self.assertEqual(bullet_list_expected, bullet_list)
--- src/provisioningserver/utils/text.py
+++ src/provisioningserver/utils/text.py
@@ -13,10 +13,27 @@ str = None
 
 __metaclass__ = type
 __all__ = [
+    'make_bullet_list',
     'normalise_whitespace',
     ]
+
+from textwrap import TextWrapper
 
 
 def normalise_whitespace(text):
     """Replace any whitespace sequence in `text` with just a single space."""
     return ' '.join(text.split())
+
+
+def make_bullet_list(messages):
+    """Join `messages` into a bullet list.
+
+    Each message is reformatted to 70 columns wide, indented by 2 columns,
+    making 72 columns in all. The first line of each message is denoted by a
+    asterisk in the first column.
+
+    :type messages: An iterable of strings.
+    :returns: A string.
+    """
+    fill = TextWrapper(72, initial_indent="* ", subsequent_indent="  ").fill
+    return "\n".join(fill(message) for message in messages)
