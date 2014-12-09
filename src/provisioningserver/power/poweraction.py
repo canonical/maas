88e6e33094ebacafe6964d5ff89a294fb4e4a411
Gavin Panella <gavin.panella@canonical.com>
2014-12-09 09:54:49 +0000

    Backport r3405 from lp:maas: Pipe script commands to /bin/sh instead of using shell=True.
    This should also prevent errors when the scripts are very long.

--- src/provisioningserver/power/poweraction.py
+++ src/provisioningserver/power/poweraction.py
@@ -114,13 +114,17 @@ class PowerAction:
         """
         # This might need retrying but it could be better to leave that
         # to the individual scripts.
-        try:
-            output = subprocess.check_output(
-                commands, shell=True, stderr=subprocess.STDOUT, close_fds=True)
-        except subprocess.CalledProcessError as e:
-            raise PowerActionFail(self, e)
-        # This output is only examined in tests, execute just ignores it
-        return output
+        shell = ("/bin/sh",)
+        process = subprocess.Popen(
+            shell, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT, close_fds=True)
+        output, _ = process.communicate(commands)
+        if process.wait() == 0:
+            return output.strip()
+        else:
+            raise PowerActionFail(
+                self, subprocess.CalledProcessError(
+                    process.returncode, shell, output))
 
     def execute(self, **kwargs):
         """Execute the template.
