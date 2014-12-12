78f75fac3110c94598335d448949b188392025a1
Blake Rouse <blake.rouse@canonical.com>
2014-12-12 18:57:06 +0000

    [r=jason-hobbs][bug=][author=blake-rouse] Implemented a RPC debug panel for Django Debug Panel. Allows monitoring RPC calls during a request.

--- src/maas/development.py
+++ src/maas/development.py
@@ -88,6 +88,22 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     }
 
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'maasserver.rpc.testing.debug_panel.RPCPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+)
+
 # Make all nodes' metadata visible.  This is not safe; do not enable it
 # on a production MAAS.
 ALLOW_UNSAFE_METADATA_ACCESS = True
