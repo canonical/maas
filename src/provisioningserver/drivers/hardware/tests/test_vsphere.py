6515ad920397a4721ac14189c1da2f6aaea955fd
Mike Pontillo <mike.pontillo@canonical.com>
2015-03-03 05:58:16 +0000

    [r=andreserl][bug=][author=mpontillo] This commit adds low-level support for vSphere probe-and-enlist. (Includes communication using python-pyvmomi API and corresponding tests, but NOT API support in MAAS.)

---
+++ src/provisioningserver/drivers/hardware/tests/test_vsphere.py
@@ -0,0 +1,319 @@
+# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for `provisioningserver.drivers.hardware.vsphere`.
+"""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = []
+
+
+import random
+
+from maastesting.factory import factory
+from maastesting.testcase import (
+    MAASTestCase,
+    MAASTwistedRunTest,
+    )
+from provisioningserver.drivers.hardware import vsphere
+from provisioningserver.drivers.hardware.vsphere import VspherePyvmomi
+from provisioningserver.utils.twisted import asynchronous
+from testtools import ExpectedException
+from testtools.matchers import (
+    Equals,
+    Is,
+    IsInstance,
+    Not,
+    )
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.threads import deferToThread
+
+
+try:
+    import pyVmomi
+    import pyVim.connect as vmomi_api
+except ImportError:
+    pyVmomi = None
+    vmomi_api = None
+
+
+class FakeVmomiVMSummaryConfig(object):
+    def __init__(self, name):
+        self.name = name
+        self.guestId = random.choice(["otherLinux64Guest", "otherLinuxGuest"])
+        has_instance_uuid = random.choice([True, True, False])
+        if has_instance_uuid:
+            self.instanceUuid = factory.make_UUID()
+        self.uuid = factory.make_UUID()
+
+
+class FakeVmomiVMSummary(object):
+    def __init__(self, name):
+        self.config = FakeVmomiVMSummaryConfig(name)
+
+
+class FakeVmomiVMRuntime(object):
+    def __init__(self):
+        # add an invalid power state into the mix
+        self.powerState = random.choice(
+            ["poweredOn",
+             "poweredOff",
+             "suspended",
+             "warp9"])
+
+
+class FakeVmomiVMConfigHardwareDevice(object):
+    def __init__(self):
+        pass
+
+
+class FakeVmomiNic(FakeVmomiVMConfigHardwareDevice):
+    def __init__(self):
+        super(FakeVmomiNic, self).__init__()
+        self.macAddress = factory.make_mac_address()
+
+
+class FakeVmomiVMConfigHardware(object):
+    def __init__(self, nics=None):
+        self.device = []
+
+        if nics is None:
+            nics = random.choice([1, 1, 1, 2, 2, 3])
+
+        for i in range(0, nics):
+            self.device.append(FakeVmomiNic())
+
+        # add a few random non-NICs into the mix
+        for i in range(0, random.choice([0, 1, 3, 5, 15])):
+            self.device.append(FakeVmomiVMConfigHardwareDevice())
+
+        random.shuffle(self.device)
+
+
+class FakeVmomiVMConfig(object):
+    def __init__(self, nics=None):
+        self.hardware = FakeVmomiVMConfigHardware(nics=nics)
+
+
+class FakeVmomiVM(object):
+    def __init__(self, name=None, nics=None):
+
+        if name is None:
+            self._name = factory.make_hostname()
+        else:
+            self._name = name
+
+        self.summary = FakeVmomiVMSummary(self._name)
+        self.runtime = FakeVmomiVMRuntime()
+        self.config = FakeVmomiVMConfig(nics=nics)
+
+    def PowerOn(self):
+        self.runtime.powerState = "poweredOn"
+
+    def PowerOff(self):
+        self.runtime.powerState = "poweredOff"
+
+
+class FakeVmomiVmFolder(object):
+    def __init__(self, servers=0):
+        self.childEntity = []
+        for i in range(0, servers):
+            vm = FakeVmomiVM()
+            self.childEntity.append(vm)
+
+
+class FakeVmomiDatacenter(object):
+    def __init__(self, servers=0):
+        self.vmFolder = FakeVmomiVmFolder(servers=servers)
+
+
+class FakeVmomiRootFolder(object):
+    def __init__(self, servers=0):
+        self.childEntity = [FakeVmomiDatacenter(servers=servers)]
+
+
+class FakeVmomiSearchIndex(object):
+    def __init__(self, content):
+        self.vms_by_instance_uuid = {}
+        self.vms_by_uuid = {}
+
+        for child in content.rootFolder.childEntity:
+            if hasattr(child, 'vmFolder'):
+                datacenter = child
+                vm_folder = datacenter.vmFolder
+                vm_list = vm_folder.childEntity
+                for vm in vm_list:
+                    if hasattr(vm.summary.config, 'instanceUuid') \
+                            and vm.summary.config.instanceUuid is not None:
+                        self.vms_by_instance_uuid[
+                            vm.summary.config.instanceUuid] = vm
+                    if hasattr(vm.summary.config, 'uuid')\
+                            and vm.summary.config.uuid is not None:
+                        self.vms_by_uuid[vm.summary.config.uuid] = vm
+
+    def FindByUuid(self, datacenter, uuid, search_vms,
+                   search_by_instance_uuid):
+        assert datacenter is None
+        assert uuid is not None
+        assert search_vms is True
+        if search_by_instance_uuid:
+            if uuid not in self.vms_by_instance_uuid:
+                return None
+            return self.vms_by_instance_uuid[uuid]
+        else:
+            if uuid not in self.vms_by_uuid:
+                return None
+            return self.vms_by_uuid[uuid]
+
+
+class FakeVmomiContent(object):
+    def __init__(self, servers=0):
+        self.rootFolder = FakeVmomiRootFolder(servers=servers)
+        self.searchIndex = FakeVmomiSearchIndex(self)
+
+
+class FakeVmomiServiceInstance(object):
+    def __init__(self, servers=0):
+        self.content = FakeVmomiContent(servers=servers)
+
+    def RetrieveContent(self):
+        return self.content
+
+
+class TestVspherePyvmomi(MAASTestCase):
+    """Tests for vSphere probe-and-enlist, and power query/control using
+    the python-pyvmomi API."""
+
+    run_tests_with = MAASTwistedRunTest.make_factory(timeout=5)
+
+    def configure_vmomi_api(self, servers=10):
+        mock_vmomi_api = self.patch(vsphere, 'vmomi_api')
+        mock_vmomi_api.SmartConnect.return_value = FakeVmomiServiceInstance(
+            servers=servers)
+        return mock_vmomi_api
+
+    def setUp(self):
+        super(TestVspherePyvmomi, self).setUp()
+        if vmomi_api is None:
+            self.skipTest('cannot test vSphere without python-pyvmomi')
+
+    def test_api_connection(self):
+        mock_vmomi_api = self.configure_vmomi_api(servers=0)
+        api = VspherePyvmomi(
+            factory.make_hostname(),
+            factory.make_username(),
+            factory.make_username())
+        api.connect()
+        self.expectThat(
+            api.service_instance,
+            IsInstance(FakeVmomiServiceInstance))
+        self.expectThat(api.is_connected(), Equals(True))
+        api.disconnect()
+        self.expectThat(mock_vmomi_api.SmartConnect.called, Equals(True))
+        self.expectThat(mock_vmomi_api.Disconnect.called, Equals(True))
+
+    def test_api_failed_connection(self):
+        mock_vmomi_api = self.patch(vsphere, 'vmomi_api')
+        mock_vmomi_api.SmartConnect.return_value = None
+        api = VspherePyvmomi(
+            factory.make_hostname(),
+            factory.make_username(),
+            factory.make_username())
+        with ExpectedException(vsphere.VsphereError):
+            api.connect()
+        self.expectThat(api.service_instance, Is(None))
+        self.expectThat(api.is_connected(), Equals(False))
+        api.disconnect()
+        self.expectThat(mock_vmomi_api.SmartConnect.called, Equals(True))
+        self.expectThat(mock_vmomi_api.Disconnect.called, Equals(True))
+
+    def test_get_vsphere_servers_empty(self):
+        self.configure_vmomi_api(servers=0)
+        servers = vsphere.get_vsphere_servers(
+            factory.make_hostname(),
+            factory.make_username(),
+            factory.make_username(),
+            port=8443, protocol='https')
+        self.expectThat(servers, Equals({}))
+
+    def test_get_vsphere_servers(self):
+        self.configure_vmomi_api(servers=10)
+
+        servers = vsphere.get_vsphere_servers(
+            factory.make_hostname(),
+            factory.make_username(),
+            factory.make_username())
+        self.expectThat(servers, Not(Equals({})))
+
+    def test_power_control(self):
+        mock_vmomi_api = self.configure_vmomi_api(servers=100)
+
+        host = factory.make_hostname()
+        username = factory.make_username()
+        password = factory.make_username()
+
+        servers = vsphere.get_vsphere_servers(host, username, password)
+
+        # here we're grabbing indexes only available in the private mock object
+        search_index = \
+            mock_vmomi_api.SmartConnect.return_value.content.searchIndex
+
+        bios_uuids = search_index.vms_by_uuid.keys()
+        instance_uuids = search_index.vms_by_instance_uuid.keys()
+
+        # at least one should have a randomly-invalid state (just checking
+        # for coverage, but since it's random, don't want to assert)
+        for uuid in bios_uuids:
+            vsphere.power_query_vsphere(host, username, password, uuid)
+        for uuid in instance_uuids:
+            vsphere.power_query_vsphere(host, username, password, uuid)
+
+        # turn on a set of VMs, then verify they are on
+        for uuid in bios_uuids:
+            vsphere.power_control_vsphere(host, username, password, uuid, "on")
+
+        for uuid in bios_uuids:
+            state = vsphere.power_query_vsphere(host, username, password, uuid)
+            self.expectThat(state, Equals("on"))
+
+        # turn off a set of VMs, then verify they are off
+        for uuid in instance_uuids:
+            vsphere.power_control_vsphere(host, username, password, uuid,
+                                          "off")
+        for uuid in instance_uuids:
+            state = vsphere.power_query_vsphere(host, username, password, uuid)
+            self.expectThat(state, Equals("off"))
+
+        self.expectThat(servers, Not(Equals({})))
+
+    @inlineCallbacks
+    def test_probe_and_enlist(self):
+        num_servers = 100
+        self.configure_vmomi_api(servers=num_servers)
+        mock_create_node = self.patch(vsphere, 'create_node')
+        system_id = factory.make_name('system_id')
+        mock_create_node.side_effect = asynchronous(lambda *args: system_id)
+        mock_commission_node = self.patch(vsphere, 'commission_node')
+
+        host = factory.make_hostname()
+        username = factory.make_username()
+        password = factory.make_username()
+
+        yield deferToThread(
+            vsphere.probe_vsphere_and_enlist,
+            factory.make_username(),
+            host,
+            username,
+            password,
+            accept_all=True)
+
+        self.expectThat(mock_create_node.call_count, Equals(num_servers))
+        self.expectThat(mock_commission_node.call_count, Equals(num_servers))
---
+++ src/provisioningserver/drivers/hardware/vsphere.py
@@ -0,0 +1,367 @@
+# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = [
+    'power_control_vsphere',
+    'power_query_vsphere',
+    'probe_vsphere_and_enlist',
+    ]
+
+from abc import abstractmethod
+from collections import OrderedDict
+import traceback
+
+from provisioningserver.logger import get_maas_logger
+from provisioningserver.utils import (
+    commission_node,
+    create_node,
+    )
+from provisioningserver.utils.twisted import synchronous
+
+
+try:
+    import pyVmomi
+    import pyVim.connect as vmomi_api
+except ImportError:
+    pyVmomi = None
+    vmomi_api = None
+
+maaslog = get_maas_logger("drivers.vsphere")
+
+
+class VsphereError(Exception):
+    """Failure talking to the vSphere API. """
+
+
+class VsphereAPI(object):
+    """Abstract base class to represent a MAAS-capable VMware API. The API
+    must be capable of:
+    - Gathering names, UUID, and MAC addresses of each virtual machine
+    - Powering on/off VMs
+    - Checking the power status of VMs
+    """
+    def __init__(self, host, username, password,
+                 port=None, protocol=None):
+        """
+        :param host: The vSphere host to connect to
+        :type host: string
+        :param port: The port on the vSphere host to connect to
+        :type port: integer
+        :param username: A username authorized for the specified vSphere host
+        :type username: string
+        :param password: The password corresponding to the supplied username
+        :type password: string
+        :param protocol: The protocol to use (default: 'https')
+        :type protocol: string
+        """
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.protocol = protocol
+
+    @abstractmethod
+    def connect(self):
+        """Connects to the vSphere API"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_connected(self):
+        """Returns True if the vSphere API is thought to be connected"""
+        raise NotImplementedError
+
+    def disconnect(self):
+        """Disconnects from the vSphere API"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_vm_by_uuid(self, uuid):
+        """
+        Searches for a VM that matches the specified UUID. The UUID can be
+        either an instance UUID, or a BIOS UUID. If found, returns an object
+        to represent the VM. Otherwise, returns None.
+        :return: an opaque object representing the VM
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def get_maas_power_state(vm):
+        """
+        Returns the MAAS representation of the power status for the
+        specified virtual machine.
+        :return: string ('on', 'off', or 'error')
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def set_power_state(vm, power_change):
+        """
+        Sets the power state for the specified VM to the specified value.
+        :param:power_change: the new desired state ('on' or 'off')
+        :except:VsphereError: if the power status could not be changed
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all_vm_properties(self):
+        """
+        Creates dictionary that catalogs every virtual machine present on
+        the vSphere server. Each key is a machine name, and each value is a
+        dictionary containing the following keys:
+         - uuid: a UUID for the VM (to be used for power management)
+         - macs: a list of MAC addresses associated with this VM
+         - architecture: amd64 or i386 (depending on the guest ID)
+         - power_state: the current power state of the VM ("on" or "off")
+        :return: a dictionary as specified above
+        """
+        raise NotImplementedError
+
+
+class VspherePyvmomi(VsphereAPI):
+    def __init__(
+            self, host, username, password, port=None, protocol=None):
+        super(VspherePyvmomi, self).__init__(host, username, password,
+                                             port=port, protocol=protocol)
+        self.service_instance = None
+
+    def connect(self):
+        # place optional arguments in a dictionary to pass to the
+        # VMware API call; otherwise the API will see 'None' and fail.
+        extra_args = {}
+        if self.port is not None:
+            extra_args['port'] = self.port
+
+        if self.protocol is not None:
+            extra_args['protocol'] = self.protocol
+
+        self.service_instance = vmomi_api.SmartConnect(host=self.host,
+                                                       user=self.username,
+                                                       pwd=self.password,
+                                                       **extra_args)
+
+        if not self.service_instance:
+            raise VsphereError("Could not connect to vSphere service API")
+
+        return self.service_instance is not None
+
+    def is_connected(self):
+        return self.service_instance is not None
+
+    def disconnect(self):
+        vmomi_api.Disconnect(self.service_instance)
+        self.service_instance = None
+
+    @staticmethod
+    def _probe_network_cards(vm):
+        mac_addresses = []
+        for device in vm.config.hardware.device:
+            if hasattr(device, 'macAddress'):
+                mac = device.macAddress
+                if mac is not None and mac != "":
+                    mac_addresses.append(mac)
+        return mac_addresses
+
+    @staticmethod
+    def _get_uuid(vm):
+        # In vCenter environments, using the BIOS UUID (uuid) is deprecated.
+        # But we can use it as a fallback, since the API supports both.
+        if hasattr(vm.summary.config, 'instanceUuid') \
+                and vm.summary.config.instanceUuid is not None:
+            return vm.summary.config.instanceUuid
+        elif hasattr(vm.summary.config, 'uuid') \
+                and vm.summary.config.uuid is not None:
+            return vm.summary.config.uuid
+        return None
+
+    def find_vm_by_uuid(self, uuid):
+        content = self.service_instance.RetrieveContent()
+
+        # First search using the instance UUID
+        vm = content.searchIndex.FindByUuid(None, uuid, True, True)
+
+        if vm is None:
+            # ... otherwise, try using the BIOS UUID
+            vm = content.searchIndex.FindByUuid(None, uuid, True, False)
+        return vm
+
+    @staticmethod
+    def _get_power_state(vm):
+        return vm.runtime.powerState
+
+    @staticmethod
+    def pyvmomi_to_maas_powerstate(power_state):
+        """Returns a MAAS power state given the specified pyvmomi state"""
+        if power_state == 'poweredOn':
+            return "on"
+        elif power_state == 'poweredOff':
+            return "off"
+        elif power_state == 'suspended':
+            return "on"  # TODO: model this in MAAS
+        else:
+            return "error"
+
+    @staticmethod
+    def get_maas_power_state(vm):
+        return VspherePyvmomi.pyvmomi_to_maas_powerstate(vm.runtime.powerState)
+
+    @staticmethod
+    def set_power_state(vm, power_change):
+        if vm is not None:
+            if power_change == 'on':
+                vm.PowerOn()
+            elif power_change == 'off':
+                vm.PowerOff()
+            else:
+                raise VsphereError("set_power_state: Invalid power_change "
+                                   "state: {state}".format(power_change))
+
+    def _get_vm_properties(self, vm):
+        """Gathers the properties for the specified VM, for inclusion into
+        the dictionary containing the properties of all VMs."""
+        properties = {}
+
+        properties['uuid'] = self._get_uuid(vm)
+
+        if "64" in vm.summary.config.guestId:
+            properties['architecture'] = "amd64"
+        else:
+            properties['architecture'] = "i386"
+
+        properties['power_state'] = self.pyvmomi_to_maas_powerstate(
+            self._get_power_state(vm))
+
+        properties['macs'] = self._probe_network_cards(vm)
+
+        # These aren't needed now, but we might want them one day...
+        # properties['cpus'] = vm.summary.config.numCpu
+        # properties['ram'] = vm.summary.config.memorySizeMB
+
+        return properties
+
+    def get_all_vm_properties(self):
+        # Using an OrderedDict() in case the order that virtual machines
+        # are returned in is important to the user.
+        virtual_machines = OrderedDict()
+
+        content = self.service_instance.RetrieveContent()
+        for child in content.rootFolder.childEntity:
+            if hasattr(child, 'vmFolder'):
+                datacenter = child
+                vm_folder = datacenter.vmFolder
+                vm_list = vm_folder.childEntity
+                for vm in vm_list:
+                    vm_name = vm.summary.config.name
+                    vm_properties = self._get_vm_properties(vm)
+                    virtual_machines[vm_name] = vm_properties
+
+        return virtual_machines
+
+
+def _get_vsphere_api(
+        host, username, password, port=None, protocol=None):
+    if pyVmomi is not None:
+        # Attempt to detect the best available VMware API
+        return VspherePyvmomi(
+            host, username, password, port=port, protocol=protocol)
+    else:
+        raise VsphereError("Could not find a suitable "
+                           "vSphere API (install python-pyvmomi)")
+
+
+def get_vsphere_servers(
+        host, username, password, port=None, protocol=None):
+    servers = {}
+    api = _get_vsphere_api(
+        host, username, password, port=port, protocol=protocol)
+
+    if api.connect():
+        try:
+            servers = api.get_all_vm_properties()
+        finally:
+            api.disconnect()
+    return servers
+
+
+@synchronous
+def probe_vsphere_and_enlist(
+        user, host, username, password, accept_all=False):
+
+    maaslog.info(
+        "Probing for vSphere nodes with arguments "
+        "host=%s, username=%s, password=%s",
+        host, username, password)
+
+    servers = get_vsphere_servers(host, username, password)
+
+    maaslog.info("Found {count} vSphere servers".format(count=len(servers)))
+
+    for system_id in servers:
+        properties = servers[system_id]
+        params = {
+            'power_address': host,
+            'power_user': username,
+            'power_pass': password,
+            'power_uuid': properties['uuid'],
+        }
+        maaslog.info(
+            "Found vSphere node with macs %s; adding to MAAS with "
+            "params : %s", properties['macs'], params)
+
+        system_id = create_node(properties['macs'], properties['architecture'],
+                                'vsphere', params).wait(30)
+
+        if accept_all and system_id is not None:
+            commission_node(system_id, user).wait(30)
+
+
+def power_control_vsphere(
+        host, username, password, uuid, power_change,
+        port=None, protocol=None):
+    api = _get_vsphere_api(
+        host, username, password, port=port, protocol=protocol)
+
+    if api.connect():
+        try:
+            vm = api.find_vm_by_uuid(uuid)
+            if vm is None:
+                raise VsphereError("Failed to find VM based on UUID: {uuid}"
+                                   .format(uuid=uuid))
+
+            api.set_power_state(vm, power_change)
+        except:
+            raise VsphereError(
+                "Failed to set power state to {state} for uuid={uuid}"
+                .format(state=power_change, uuid=uuid), traceback.format_exc())
+        finally:
+            api.disconnect()
+
+
+def power_query_vsphere(
+        host, username, password, uuid, port=None, protocol=None):
+    """Return the power state for the VM with the specified UUID,
+     using the vSphere API."""
+    api = _get_vsphere_api(
+        host, username, password, port=port, protocol=protocol)
+
+    if api.connect():
+        try:
+            vm = api.find_vm_by_uuid(uuid)
+            if vm is not None:
+                return api.get_maas_power_state(vm)
+        except:
+            raise VsphereError(
+                "Failed to get power state for uuid={uuid}"
+                .format(uuid=uuid), traceback.format_exc())
+        finally:
+            api.disconnect()
