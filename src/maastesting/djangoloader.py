9e68e166ff39035f5812cb4b3d7ac74ff713f49a
Gavin Panella <gavin.panella@canonical.com>
2015-03-09 17:44:32 +0000

    [r=blake-rouse][bug=][author=allenap] Ensure that the testing PostgreSQL cluster is torn-down if there's an error creating it or any of the databases which reside within.

--- src/maastesting/djangoloader.py
+++ src/maastesting/djangoloader.py
@@ -42,16 +42,31 @@ class MAASDjangoTestRunner(NoseTestSuiteRunner):
         """Fire up the db cluster, then punt to original implementation."""
         self.cluster = ClusterFixture("db", preserve=True)
         self.cluster.setUp()
-        # Create a database in the PostgreSQL cluster for each database
-        # connection configured in Django's settings that points to the same
-        # datadir.
-        for database in settings.DATABASES.values():
-            if database["HOST"] == self.cluster.datadir:
-                self.cluster.createdb(database["NAME"])
-        return super(MAASDjangoTestRunner, self).setup_databases(
-            *args, **kwargs)
+        try:
+            # Create a database in the PostgreSQL cluster for each database
+            # connection configured in Django's settings that points to the
+            # same datadir.
+            for database in settings.DATABASES.values():
+                if database["HOST"] == self.cluster.datadir:
+                    self.cluster.createdb(database["NAME"])
+            # Call-up to super-classes.
+            up = super(MAASDjangoTestRunner, self)
+            return up.setup_databases(*args, **kwargs)
+        except:
+            # Clean-up the cluster now, or it'll be left running; django-nose
+            # does not know to clean it up itself, and lacks a fixture-like
+            # mechanism to aid with reverting a half-configured environment.
+            self.cluster.cleanUp()
+            # Now we can let the original error wreak havoc.
+            raise
 
     def teardown_databases(self, *args, **kwargs):
+        """Tear-down the test database cluster.
+
+        This is *not* called if there's a failure during bring-up of any of
+        the test databases, hence there is also tear-down code embedded in
+        `setup_databases`.
+        """
         super(MAASDjangoTestRunner, self).teardown_databases(*args, **kwargs)
         self.cluster.cleanUp()
 
