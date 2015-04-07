b3c609fd821ecbb768817ea51d737972f3839f83
Blake Rouse <blake.rouse@canonical.com>
2015-04-07 17:40:22 +0000

    [r=andreserl][bug=1439359][author=blake-rouse] Set the correct boot source selections and start importing of boot resources if the cluster already has boot images on start up of the region.

--- src/maasserver/start_up.py
+++ src/maasserver/start_up.py
@@ -25,10 +25,19 @@ from maasserver import (
     locks,
     security,
     )
-from maasserver.bootresources import ensure_boot_source_definition
+from maasserver.bootresources import (
+    ensure_boot_source_definition,
+    import_resources,
+    )
 from maasserver.dns.config import write_full_dns_config
 from maasserver.fields import register_mac_type
-from maasserver.models import NodeGroup
+from maasserver.models import (
+    BootResource,
+    BootSource,
+    BootSourceSelection,
+    NodeGroup,
+    )
+from provisioningserver.rpc.boot_images import list_boot_images
 from provisioningserver.upgrade_cluster import create_gnupg_home
 
 
@@ -59,6 +68,59 @@ def start_up():
     eventloop.start().wait(10)
 
 
+def start_import_on_upgrade():
+    """Starts importing `BootResource`s on upgrade from MAAS where the boot
+    images where only stored on the clusters."""
+    # Do nothing, because `BootResource`s already exist.
+    if BootResource.objects.exists():
+        return
+
+    # Do nothing if the cluster on the machine does not have
+    # boot images present.
+    boot_images = list_boot_images()
+    if len(boot_images) == 0:
+        return
+
+    # Build the selections that need to be set based on the images
+    # that live on the cluster.
+    osystems = dict()
+    for image in boot_images:
+        osystem = image["osystem"]
+        if osystem not in osystems:
+            osystems[osystem] = {
+                "arches": set(),
+                "releases": set(),
+                "labels": set(),
+                }
+        osystems[osystem]["arches"].add(
+            image["architecture"])
+        osystems[osystem]["releases"].add(
+            image["release"])
+        osystems[osystem]["labels"].add(
+            image["label"])
+
+    # We have no way to truly know which boot source this came
+    # from, but since this should only occur on upgrade we
+    # take the first source, which will be the default source and
+    # apply the selection to that source.
+    boot_source = BootSource.objects.first()
+
+    # Clear all current selections and create the new selections
+    # based on the information retrieved from list_boot_images.
+    boot_source.bootsourceselection_set.all().delete()
+    for osystem, options in osystems.items():
+        for release in options["releases"]:
+            BootSourceSelection.objects.create(
+                boot_source=boot_source, os=osystem,
+                release=release, arches=list(options["arches"]),
+                subarches=["*"], labels=list(options["labels"]))
+
+    # Start the import process for the user, since the cluster
+    # already has images. Even though the cluster is usable the
+    # region will not be usable until it has boot images as well.
+    import_resources()
+
+
 def inner_start_up():
     """Startup jobs that must run serialized w.r.t. other starting servers."""
     # Register our MAC data type with psycopg.
@@ -75,6 +137,9 @@ def inner_start_up():
 
     # If no boot-source definitions yet, create the default definition.
     ensure_boot_source_definition()
+
+    # Start import on upgrade if needed.
+    start_import_on_upgrade()
 
     # Regenerate MAAS's DNS configuration.  This should be reentrant, really.
     write_full_dns_config(reload_retry=True)
