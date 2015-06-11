fe3230fc91a50fc52105010578d14bb19959c034
Raphael Badin <raphael.badin@canonical.com>
2015-06-11 10:59:50 +0000

    [r=allenap][bug=][author=rvb] Change doc version to '1.8'.

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ copyright = u'2012-2015, MAAS Developers'
 # built documents.
 #
 # The short X.Y version.
-version = doc_versions.items()[0][0]
+version = '1.8'
 # The full version, including alpha/beta/rc tags.
 release = version
 
