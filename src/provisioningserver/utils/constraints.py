7d1cc9befe32fd4c0a6b6b05f6045b05cf7dd696
Mike Pontillo <mike.pontillo@canonical.com>
2015-10-27 19:22:23 +0000

    [r=allenap][bug=][author=mpontillo] Add a generic labeled constraint map parser.
    This allows parsing of strings of the following format, and converts them into Python dictionaries:
    
    <label1>:<key1>=<value1>[,<key2>=<value2>,..][;<label2>:...]
    
    A user-specified exception (which defaults to ValueError) is thrown if parsing fails.

---
+++ src/provisioningserver/utils/constraints.py
@@ -0,0 +1,99 @@
+# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Generic helpers for working with constraint strings."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = [
+    'parse_labeled_constraint_map',
+    ]
+
+import re
+
+
+def parse_labeled_constraint_map(value, exception_type=ValueError):
+    """Returns a dictionary of constraints, given the specified constraint
+    value. Validates that the following conditions hold true:
+
+     - The constraint string is non-empty
+     - The constraint string is in the format:
+         <label>:<key>=<value>[,<key2>=<value2>[,...]]'
+     - Constraint labels must only be defined once
+     - The ';', and ',', characters are reserved, and are not allowed to occur
+       in any keys or values.
+
+    The returned dictionary will be in the format:
+
+    {
+        <label1>: { <key1>: [<value1>],
+                    <key2>: [<value2>, <value3>] }
+        <label2>:  { ... },
+        ...
+    }
+
+    When multiple keys are contained within a constraint, the values will be
+    returned (in the order specified) inside a list.
+
+    Single values will also be returned inside a list, for consistency.
+
+    :return:dict
+    """
+    if value is None or not isinstance(value, (unicode, bytes)):
+        return None
+    if len(value) <= 1:
+        return {}
+    result = {}
+    constraints = value.split(';')
+    for constraint in constraints:
+        tokens = constraint.split(':', 1)
+        if len(tokens) != 2:
+            raise exception_type(
+                "Malformed constraint: '%s' (required format: "
+                "'<label>:<key>=<value>[,<key2>=<value2>[,...]]')"
+                % constraint)
+        label = tokens[0]
+        validate_constraint_label_name(label, exception_type=exception_type)
+        if label in result:
+            raise exception_type(
+                "Constraint label defined more than once: '%s'" % label)
+        key_value_pairs = tokens[1].split(',')
+        labeled_constraint = _parse_key_value_pairs(
+            key_value_pairs, exception_type=exception_type)
+        result[label] = labeled_constraint
+    return result
+
+
+def _parse_key_value_pairs(kvps, exception_type=ValueError):
+    """Given the specified list of key/value pairs, returns a dictionary
+    that maps each key to its value.
+    """
+    key_value_pairs = {}
+    for kvp in kvps:
+        tokens = kvp.split('=', 1)
+        if len(tokens) != 2:
+            raise exception_type(
+                "Malformed key/value pair in constraint: '%s'" % kvp)
+        key, value = tokens
+        value_list = key_value_pairs.get(key, [])
+        value_list.append(value)
+        key_value_pairs[key] = value_list
+    return key_value_pairs
+
+
+def validate_constraint_label_name(label_name, exception_type=ValueError):
+    """Throws the specified exception_type (default is ValueError) if the
+    label name is invalid.
+    """
+    if not re.match(r'[a-zA-Z0-9]+[a-zA-Z0-9_-]*$', label_name):
+        raise exception_type(
+            "Invalid label name: '%s' (Must begin with an alphanumeric "
+            "character, and include only alphanumeric characters, dashes, and "
+            "underscores.)" % label_name)
---
+++ src/provisioningserver/utils/tests/test_constraints.py
@@ -0,0 +1,124 @@
+# Copyright 2014-2015 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for constraints helpers."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+str = None
+
+__metaclass__ = type
+__all__ = []
+
+from maastesting.testcase import MAASTestCase
+from provisioningserver.utils.constraints import (
+    parse_labeled_constraint_map,
+    validate_constraint_label_name,
+)
+from testtools import ExpectedException
+from testtools.matchers import (
+    Equals,
+    Is,
+)
+
+
+class ConstraintTestException(Exception):
+    """Dummy exception class used to test that specifying an exception class
+    to throw works properly."""
+
+
+class TestValidateLabelName(MAASTestCase):
+    EXPECTED_BAD_NAMES = [
+        '',
+        ' ',
+        '_',
+        '-',
+        ' ',
+        'a ',
+        'a ',
+        '-a',
+        '*',
+        '%',
+    ]
+
+    EXPECTED_GOOD_NAMES = [
+        'a',
+        '0',
+        'A',
+        'a-',
+        'a-b',
+        'a_b',
+        'a_b',
+    ]
+
+    def test__rejects_bad_names(self):
+        for name in self.EXPECTED_BAD_NAMES:
+            with ExpectedException(
+                    ConstraintTestException, msg='name=%s' % name):
+                validate_constraint_label_name(
+                    name, exception_type=ConstraintTestException)
+
+    def test__accepts_good_names(self):
+        for name in self.EXPECTED_GOOD_NAMES:
+            validate_constraint_label_name(name)
+
+
+class TestGetLabeledConstraintsMap(MAASTestCase):
+
+    def test__missing_key_value_pair_raises(self):
+        with ExpectedException(ConstraintTestException):
+            parse_labeled_constraint_map(
+                "a:bc", exception_type=ConstraintTestException)
+
+    def test__duplicate_label_raises(self):
+        with ExpectedException(ConstraintTestException):
+            parse_labeled_constraint_map(
+                "a:b=c;a:d=e", exception_type=ConstraintTestException)
+
+    def test__invalid_label_raises(self):
+        with ExpectedException(ConstraintTestException):
+            parse_labeled_constraint_map(
+                "*:b=c", exception_type=ConstraintTestException)
+
+    def test__label_with_no_constraints_raises(self):
+        with ExpectedException(ConstraintTestException):
+            parse_labeled_constraint_map(
+                "a:", exception_type=ConstraintTestException)
+
+    def test__single_value_map(self):
+        result = parse_labeled_constraint_map("a:b=c")
+        self.assertThat(result, Equals({"a": {"b": ["c"]}}))
+
+    def test__non_string_returns_None(self):
+        result = parse_labeled_constraint_map(dict())
+        self.assertThat(result, Is(None))
+
+    def test__empty_string_returns_empty_map(self):
+        result = parse_labeled_constraint_map("")
+        self.assertThat(result, Equals({}))
+
+    def test__multiple_value_map(self):
+        result = parse_labeled_constraint_map("a:b=c,d=e")
+        self.assertThat(result, Equals({"a": {"b": ["c"], "d": ["e"]}}))
+
+    def test__multiple_value_map_with_duplicate_keys_appends_to_list(self):
+        result = parse_labeled_constraint_map("a:a=abc,a=def,a=ghi")
+        self.assertThat(result, Equals({"a": {"a": ["abc", "def", "ghi"]}}))
+
+    def test__multiple_label_map(self):
+        result = parse_labeled_constraint_map("foo:a=b;bar:c=d")
+        self.assertThat(result, Equals({
+            "foo": {"a": ["b"]},
+            "bar": {"c": ["d"]},
+        }))
+
+    def test__multiple_value_map_multiple_label_map(self):
+        result = parse_labeled_constraint_map("foo:a=b,c=d;bar:e=f,g=h")
+        self.assertThat(result, Equals({
+            "foo": {"a": ["b"], "c": ["d"]},
+            "bar": {"e": ["f"], "g": ["h"]},
+        }))
