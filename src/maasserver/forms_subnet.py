cd1236e412a23e50dfa8fa20acbf1004f11ebe9e
Lee Trager <lee.trager@canonical.com>
2015-12-03 00:09:09 +0000

    [r=ltrager][bug=1521833][author=ltrager] Backport from trunk: Fix bug where updates to the subnet would remove the first DNS server

--- src/maasserver/forms_subnet.py
+++ src/maasserver/forms_subnet.py
@@ -59,6 +59,10 @@ class SubnetForm(MAASModelForm):
 
     def clean(self):
         cleaned_data = super(SubnetForm, self).clean()
+        # The djorm_pgarray.fields.ArrayField form has a bug which leaves out
+        # the first entry.
+        if 'dns_servers' in self.data and self.data['dns_servers'] != '':
+            cleaned_data['dns_servers'] = self.data.getlist('dns_servers')
         cleaned_data = self._clean_name(cleaned_data)
         cleaned_data = self._clean_dns_servers(cleaned_data)
         if self.instance.id is None:
