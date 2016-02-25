67025cdad54cebcd69f044f35fef687cbea0135b
Newell Jensen newell.jensen at canonical.com <Newell Jensen newell.jensen@canonical.com>
2016-02-25 22:18:08 +0000

    [r=blake-rouse][bug=][author=newell-jensen] Add data migration for ether_wake to manual power type.

---
+++ src/maasserver/migrations/builtin/maasserver/0035_convert_ether_wake_to_manual_power_type.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import (
+    migrations,
+    models,
+)
+
+
+def convert_ether_wake_to_manual_power_type(apps, schema_editor):
+    BMC = apps.get_model("maasserver", "BMC")
+    for bmc in BMC.objects.filter(power_type='ether_wake'):
+        bmc.power_type = 'manual'
+        bmc.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('maasserver', '0034_rename_mount_params_as_mount_options'),
+    ]
+
+    operations = [
+        migrations.RunPython(convert_ether_wake_to_manual_power_type),
+    ]
