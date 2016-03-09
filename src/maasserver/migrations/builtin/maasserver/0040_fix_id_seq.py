952569e1ab534d3443c18e3ceb79478665f2f449
Mike Pontillo <mike.pontillo@canonical.com>
2016-03-09 08:44:41 +0000

    [r=andreserl][bug=][author=mpontillo] Fix VLAN ID sequence to start at 5001 again. (This feature disappeared when we went to Django 1.8, since it was inserted via custom SQL in a previous migration.)

---
+++ src/maasserver/migrations/builtin/maasserver/0040_fix_id_seq.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import (
+    migrations,
+    models,
+)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('maasserver', '0039_create_template_and_versionedtextfile_models'),
+    ]
+
+    operations = [
+        migrations.RunSQL("""\
+            SELECT pg_catalog.setval(
+                'maasserver_vlan_id_seq', 
+                GREATEST(
+                    5001, (SELECT MAX(id)+1 from maasserver_vlan)), false);
+            """)
+    ]
