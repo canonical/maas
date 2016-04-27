00e9780ab07fc464894862dd2f0a16541b5a01dd
Gavin Panella <gavin.panella@canonical.com>
2016-04-27 20:22:00 +0000

    [r=blake-rouse][bug=][author=allenap] New RunTest derivative to check that a crochet-managed reactor is clean.
    Based on work by Blake in lp:~blake-rouse/maas/tests-clean-reactor.

--- src/maastesting/runtest.py
+++ src/maastesting/runtest.py
@@ -4,18 +4,56 @@
 """Test executors for MAAS."""
 
 __all__ = [
+    'MAASCrochetRunTest',
     'MAASRunTest',
     'MAASTwistedRunTest',
     ]
 
 import sys
+import threading
 import types
 
 from testtools import (
     deferredruntest,
     runtest,
 )
-from twisted.internet import defer
+from twisted.internet import (
+    defer,
+    interfaces,
+    reactor,
+)
+from twisted.internet.base import DelayedCall
+from twisted.internet.process import reapAllProcesses
+from twisted.internet.task import LoopingCall
+from twisted.internet.threads import blockingCallFromThread
+
+
+def DelayedCall_bytes(call, __str__=DelayedCall.__str__):
+    string = __str__(call)
+    if isinstance(string, str):
+        return string.encode("utf-8", "surrogateescape")
+    else:
+        return string
+
+
+def DelayedCall_str(call, __str__=DelayedCall.__str__):
+    string = __str__(call)
+    if isinstance(string, bytes):
+        return string.decode("utf-8", "surrogateescape")
+    else:
+        return string
+
+
+# Work around https://twistedmatrix.com/trac/ticket/8306.
+DelayedCall.__bytes__ = DelayedCall_bytes
+DelayedCall.__str__ = DelayedCall_str
+
+# Work around https://twistedmatrix.com/trac/ticket/8307.
+DelayedCall.creator = ()
+
+# Ask Twisted to store debug information about `DelayedCall`s.
+# This greatly helps debugging Twisted errors.
+DelayedCall.debug = True
 
 
 class InvalidTest(Exception):
@@ -29,6 +67,20 @@ def check_for_generator(result):
             "decorated with inlineCallbacks?")
     else:
         return result
+
+
+def call_belongs_to_crochet(call):
+    """Return True when `call` belongs to crochet.
+
+    Crochet schedules a looping call that calls `reapAllProcesses` every 0.1
+    seconds. This checks if this `DelayedCall` matches this signature.
+
+    :type call: :class:`DelayedCall`
+    """
+    if isinstance(call.func, LoopingCall):
+        return call.func.f is reapAllProcesses
+    else:
+        return False
 
 
 class MAASRunTest(runtest.RunTest):
@@ -60,6 +112,129 @@ class MAASRunTest(runtest.RunTest):
             return check_for_generator(result)
         except:
             return self._got_user_exception(sys.exc_info())
+
+
+class MAASCrochetReactorStalled(Exception):
+    """Raised when the reactor appears to be stalled."""
+
+
+class MAASCrochetDirtyReactorError(Exception):
+    """Passed to `addError` when the reactor is unclean after a test.
+
+    :ivar delayedCalls: A list of strings desribing those delayed calls which
+        weren't cleaned up.
+    :ivar selectables: A list of strings describing those selectables which
+        weren't cleaned up.
+    :ivar threadpools: A list of strings describing those threadpools still in
+        use.
+    """
+
+    def __init__(self, delayedCalls, selectables, threadpools):
+        super(MAASCrochetDirtyReactorError, self).__init__()
+        self.delayedCalls = delayedCalls
+        self.selectables = selectables
+        self.threadpools = threadpools
+
+    def __str__(self):
+        """Return a multi-line message describing all of the unclean state."""
+        msg = "Reactor was unclean."
+        if len(self.delayedCalls) > 0:
+            msg += ("\nDelayedCalls: (set "
+                    "twisted.internet.base.DelayedCall.debug = True to "
+                    "debug)\n")
+            msg += "\n".join(self.delayedCalls)
+        if len(self.selectables) > 0:
+            msg += "\nSelectables:\n"
+            msg += "\n".join(self.selectables)
+        if len(self.threadpools) > 0:
+            msg += "\nThreadpools:\n"
+            msg += "\n".join(self.threadpools)
+        return msg
+
+
+class MAASCrochetRunTest(MAASRunTest):
+    """A specialisation of `MAASRunTest`.
+
+    In addition to `MAASRunTest`'s behaviour, this also checks that the
+    Twisted reactor is clean after each test, in much the same way as
+    testtools' `AsynchronousDeferredRunTest` does. This is, however, adapted
+    to working with a reactor running under crochet's control.
+    """
+
+    def _run_core(self):
+        """Override testtools' `_run_core`.
+
+        Check that the reactor is clean and that no delayed calls are still
+        laying around. This is different from `AsynchronousDeferredRunTest`
+        that does more advanced things with the reactor. This just simply
+        checks if its clean.
+        """
+        super(MAASCrochetRunTest, self)._run_core()
+        try:
+            self._clean()
+        except:
+            self._got_user_exception(sys.exc_info())
+
+    def _clean(self):
+        # Spin a bit to flush out imminent delayed calls.
+        if not self._tickReactor(0.5):
+            raise MAASCrochetReactorStalled(
+                "Reactor did not respond after 500ms.")
+        # Find leftover delayed calls, selectables, and threadpools in use.
+        dirty = blockingCallFromThread(reactor, self._cleanReactor)
+        if any(len(items) > 0 for items in dirty):
+            raise MAASCrochetDirtyReactorError(*dirty)
+
+    def _tickReactor(self, timeout):
+        ticked = threading.Event()
+        reactor.callFromThread(ticked.set)
+        return ticked.wait(timeout)
+
+    def _cleanReactor(self):
+        """Return leftover delayed calls, selectables, and threadpools."""
+        return (
+            [str(leftover) for leftover in self._cleanPending()],
+            [repr(leftover) for leftover in self._cleanSelectables()],
+            [repr(leftover) for leftover in self._cleanThreads()],
+        )
+
+    def _cleanPending(self):
+        """Cancel all pending calls and return their string representations.
+
+        Delayed calls belonging to crochet are ignored.
+        """
+        for call in reactor.getDelayedCalls():
+            if call.active() and not call_belongs_to_crochet(call):
+                yield call
+                call.cancel()
+
+    def _cleanSelectables(self):
+        """Remove all selectables and return their string representation.
+
+        Kill any of them that were processes.
+        """
+        for sel in reactor.removeAll():
+            if interfaces.IProcessTransport.providedBy(sel):
+                sel.signalProcess('KILL')
+            yield sel
+
+    def _cleanThreads(self):
+        for poolName in "threadpool", "threadpoolForDatabase":
+            try:
+                pool = getattr(reactor, poolName)
+            except AttributeError:
+                pass  # Pool not configured.
+            else:
+                lock = getattr(pool, "lock", None)
+                if isinstance(lock, defer.DeferredLock):
+                    if lock.locked:
+                        yield pool
+                elif isinstance(lock, defer.DeferredSemaphore):
+                    if lock.tokens != lock.limit:
+                        yield pool
+                else:
+                    if len(pool.working) != 0:
+                        yield pool
 
 
 class MAASTwistedRunTest(deferredruntest.AsynchronousDeferredRunTest):
