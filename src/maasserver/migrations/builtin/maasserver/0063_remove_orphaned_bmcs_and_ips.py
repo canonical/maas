8e3dc55f1ef816d982f7c9b0e6a42ce602a8f35f
Jeffrey C Jones <jeff.jones@canonical.com>
2016-06-04 02:17:13 +0000

    [r=blake-rouse,trapnine,mpontillo][bug=1586555][author=trapnine] Deleting a Node will cascade delete any exclusively owned BMC.

---
+++ src/maasserver/migrations/builtin/maasserver/0063_remove_orphaned_bmcs_and_ips.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django.db import migrations
+
+# Copied from Node model.
+def remove_orphaned_bmcs(apps, schema_editor):
+    Node = apps.get_model("maasserver", "Node")
+    BMC = apps.get_model("maasserver", "BMC")
+    used_bmcs = Node.objects.values_list('bmc_id', flat=True).distinct()
+    BMC.objects.exclude(id__in=used_bmcs).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('maasserver', '0062_fix_bootsource_daily_label'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_orphaned_bmcs),
+    ]
