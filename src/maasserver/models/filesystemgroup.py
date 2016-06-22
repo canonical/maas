e3d2ae9c02ab2d600601a4c2a96072551300f0c9
Gavin Panella <gavin.panella@canonical.com>
2016-06-22 17:49:19 +0000

    [r=allenap][bug=][author=allenap] Update copyright notices.

--- src/maasserver/models/filesystemgroup.py
+++ src/maasserver/models/filesystemgroup.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2015-2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Model for a filesystem group. Contains a set of filesystems that create
--- templates/script.py
+++ templates/script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.5
-# Copyright 2012-2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """..."""
