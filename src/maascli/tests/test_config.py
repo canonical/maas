c93d918320db5cf1f4521cedd5f7fb54986fcad3
Mike Pontillo <mike.pontillo@canonical.com>
2016-07-13 00:51:06 +0000

    [r=mpontillo][bug=1598937][author=mpontillo] Backport fix for bug #1598937 from trunk.

--- src/maascli/tests/test_config.py
+++ src/maascli/tests/test_config.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Tests for `maascli.config`."""
@@ -6,10 +6,19 @@
 __all__ = []
 
 import contextlib
+from contextlib import contextmanager
 import os.path
 import sqlite3
+from unittest.mock import call
 
-from maascli import api
+from maascli import (
+    api,
+    utils,
+)
+from maastesting.matchers import (
+    MockCalledOnceWith,
+    MockCallsMatch,
+)
 from maastesting.testcase import MAASTestCase
 from twisted.python.filepath import FilePath
 
@@ -98,3 +107,42 @@ class TestProfileConfig(MAASTestCase):
         with api.ProfileConfig.open(config_file):
             perms = FilePath(config_file).getPermissions()
             self.assertEqual("rw-r--r--", perms.shorthand())
+
+    def test_open_permissions_as_user_invoking_sudo(self):
+        # ProfileConfig.open() touches the database as user invoking `sudo`.
+
+        @contextmanager
+        def empty_context():
+            yield  # Do absolutely nothing.
+
+        self.patch_autospec(utils, "sudo_uid").side_effect = empty_context
+        self.patch_autospec(utils, "sudo_gid").side_effect = empty_context
+
+        config_file = os.path.join(self.make_dir(), "config")
+        with api.ProfileConfig.open(config_file):
+            # The sudo_uid and sudo_gid contexts have been used.
+            self.assertThat(utils.sudo_uid, MockCalledOnceWith())
+            self.assertThat(utils.sudo_gid, MockCalledOnceWith())
+
+    def test_open_permissions_as_user_invoking_sudo_retries_if_failed(self):
+        # ProfileConfig.open() touches the database as user invoking `sudo`,
+        # but falls back to the current UID if the operation fails.
+
+        @contextmanager
+        def empty_context():
+            yield  # Do absolutely nothing.
+
+        self.patch_autospec(utils, "sudo_uid").side_effect = empty_context
+        self.patch_autospec(utils, "sudo_gid").side_effect = empty_context
+        self.patch_autospec(api.ProfileConfig, "create_database")
+        api.ProfileConfig.create_database.side_effect = (
+            PermissionError,
+            None
+        )
+        config_file = os.path.join(self.make_dir(), "config")
+        with api.ProfileConfig.open(config_file):
+            self.assertThat(
+                api.ProfileConfig.create_database, MockCallsMatch(
+                    call(config_file),
+                    call(config_file)
+                ))
