cbafa1447bf6c2c8aa17867d8c4c1c25496da36a
Lee Trager <lee.trager@canonical.com>
2016-11-01 19:18:03 +0000

    [r=blake-rouse][bug=1636992][author=ltrager] Check if first boot source signal by id and column count.

--- src/maasserver/models/signals/bootsources.py
+++ src/maasserver/models/signals/bootsources.py
@@ -9,7 +9,6 @@ __all__ = [
 
 from django.db.models.signals import (
     post_delete,
-    post_init,
     post_save,
 )
 from maasserver.bootsources import cache_boot_sources
@@ -22,14 +21,14 @@ from twisted.internet import reactor
 signals = SignalsManager()
 
 
-def is_first_run(sender, instance, *args, **kwargs):
-    """Store whether or not this is the first BootSource added to MAAS.
-
-    On first run the ImportResourceService sets the default BootSource then
+def save_boot_source_cache(sender, instance, *args, **kwargs):
+    """On first run the ImportResourceService sets the default BootSource then
     caches the stream's contents as normal. Setting the default BootSource
     triggers this signal. This prevents updating the cache twice.
     """
-    instance.__is_first_run = BootSource.objects.count() == 0
+    # Don't run if the first row and newly created.
+    if instance.id != 1 and BootSource.objects.count() != 0:
+        update_boot_source_cache(sender, instance, *args, **kwargs)
 
 
 def update_boot_source_cache(sender, instance, *args, **kwargs):
@@ -38,12 +37,10 @@ def update_boot_source_cache(sender, instance, *args, **kwargs):
     This only begins after a successful commit to the database, and is then
     run in a thread. Nothing waits for its completion.
     """
-    if not isinstance(instance, BootSource) or not instance.__is_first_run:
-        post_commit_do(reactor.callLater, 0, cache_boot_sources)
+    post_commit_do(reactor.callLater, 0, cache_boot_sources)
 
 
-signals.watch(post_init, is_first_run, BootSource)
-signals.watch(post_save, update_boot_source_cache, BootSource)
+signals.watch(post_save, save_boot_source_cache, BootSource)
 signals.watch(post_delete, update_boot_source_cache, BootSource)
 signals.watch_config(update_boot_source_cache, "enable_http_proxy")
 signals.watch_config(update_boot_source_cache, "http_proxy")
--- src/maasserver/models/signals/tests/test_bootsources.py
+++ src/maasserver/models/signals/tests/test_bootsources.py
@@ -5,13 +5,18 @@
 
 __all__ = []
 
+from django.db import connection
 from maasserver.bootsources import cache_boot_sources
-from maasserver.models import signals
+from maasserver.models import (
+    BootSource,
+    signals,
+)
 from maasserver.models.config import Config
 from maasserver.testing.factory import factory
 from maasserver.testing.testcase import MAASServerTestCase
 from maastesting.matchers import (
     MockCalledOnceWith,
+    MockCalledWith,
     MockNotCalled,
 )
 from twisted.internet import reactor
@@ -21,20 +26,41 @@ class TestBootSourceSignals(MAASServerTestCase):
     """Tests for the `BootSource` model's signals."""
 
     def test_doesnt_update_on_initial_BootSource_create(self):
+        # The way MAAS detects if the BootSource is the initial creation is by
+        # looking at its id. Since Postgres always increments the id only the
+        # initial BootSource create(default) will have id=1. When running
+        # multiple tests the database may be rolled back but Postgres still
+        # increments ids as normal. This resets the sequence to 1.
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "ALTER SEQUENCE %s_id_seq RESTART WITH 1" %
+                BootSource._meta.db_table)
         post_commit_do = self.patch(signals.bootsources, "post_commit_do")
         factory.make_BootSource(keyring_data=factory.make_bytes())
         self.assertThat(post_commit_do, MockNotCalled())
 
     def test_arranges_for_update_on_BootSource_create(self):
+        post_commit_do = self.patch(signals.bootsources, "post_commit_do")
         factory.make_BootSource(keyring_data=factory.make_bytes())
+        factory.make_BootSource(keyring_data=factory.make_bytes())
+        self.assertThat(post_commit_do, MockCalledWith(
+            reactor.callLater, 0, cache_boot_sources))
+
+    def test_arranges_for_update_always_when_empty(self):
+        self.patch(signals.bootsources, "post_commit_do")
+        # Create then delete a boot source cache to get over initial ignore
+        # on create.
+        boot_source = factory.make_BootSource(
+            keyring_data=factory.make_bytes())
+        boot_source.delete()
         post_commit_do = self.patch(signals.bootsources, "post_commit_do")
         factory.make_BootSource(keyring_data=factory.make_bytes())
         self.assertThat(post_commit_do, MockCalledOnceWith(
             reactor.callLater, 0, cache_boot_sources))
 
     def test_arranges_for_update_on_BootSource_update(self):
-        factory.make_BootSource(keyring_data=factory.make_bytes())
         self.patch(signals.bootsources, "post_commit_do")
+        factory.make_BootSource(keyring_data=factory.make_bytes())
         boot_source = factory.make_BootSource(
             keyring_data=factory.make_bytes())
         post_commit_do = self.patch(signals.bootsources, "post_commit_do")
@@ -44,8 +70,8 @@ class TestBootSourceSignals(MAASServerTestCase):
             reactor.callLater, 0, cache_boot_sources))
 
     def test_arranges_for_update_on_BootSource_delete(self):
-        factory.make_BootSource(keyring_data=factory.make_bytes())
         self.patch(signals.bootsources, "post_commit_do")
+        factory.make_BootSource(keyring_data=factory.make_bytes())
         boot_source = factory.make_BootSource(
             keyring_data=factory.make_bytes())
         post_commit_do = self.patch(signals.bootsources, "post_commit_do")
