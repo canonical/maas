c311c4f8c8956cf115a982191eb31d598deae1cd
Lee Trager <lee.trager@canonical.com>
2017-03-29 17:11:04 +0000

    [r=mpontillo][bug=][author=ltrager] Fix test_compose_preseed_for_commissioning_node_manages_etc_hosts
    When node status is testing it checks the current_testing_script_set to see if it should reboot or shutdown. When writing this test I forgot that and didn't create a script_set for it to use causing the test to fail.

--- src/maasserver/tests/test_compose_preseed.py
+++ src/maasserver/tests/test_compose_preseed.py
@@ -217,9 +217,8 @@ class TestComposePreseed(MAASServerTestCase):
         rack_controller = factory.make_RackController()
         node = factory.make_Node(
             interface=True, status=random.choice([
-                NODE_STATUS.COMMISSIONING, NODE_STATUS.RESCUE_MODE,
-                # XXX: Should also include TESTING?
-            ]))
+                NODE_STATUS.COMMISSIONING, NODE_STATUS.TESTING,
+                NODE_STATUS.RESCUE_MODE]), with_empty_script_sets=True)
         nic = node.get_boot_interface()
         nic.vlan.dhcp_on = True
         nic.vlan.primary_rack = rack_controller
