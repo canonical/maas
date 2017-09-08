a52798f57ad5819bafd0e81c32edd52373129a16
Blake Rouse <blake.rouse@canonical.com>
2017-09-08 19:26:38 +0000

    Add an index on the created field on the events table.
    Since the order of events on a node cannot be determined by its ID, the created field is used to sort the events in the correct order. Without an index on this field it makes the query take a long time. This adds the index to help the sorting of events by created time.

---
+++ src/maasserver/migrations/builtin/maasserver/0128_events_created_index.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import (
+    migrations,
+    models,
+)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('maasserver', '0127_nodemetadata'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            "CREATE INDEX maasserver_event__created ON maasserver_event(created)",
+            "DROP INDEX maasserver_event__created"
+        )
+    ]
