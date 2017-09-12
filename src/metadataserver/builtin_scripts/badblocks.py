6c056fdb2ec2b5098a68d26bcfbdbc6a2069446f
Newell Jensen <newell.jensen@canonical.com>
2017-09-12 20:01:18 +0000

    Update hardware testing builtin script badblocks so that it no longer uses threads and instead takes a storage parameter.

--- src/metadataserver/builtin_scripts/badblocks.py
+++ src/metadataserver/builtin_scripts/badblocks.py
@@ -3,6 +3,7 @@
 # {{name}} - {{description}}
 #
 # Author: Lee Trager <lee.trager@canonical.com>
+#         Newell Jensen <newell.jensen@canonical.com>
 #
 # Copyright (C) 2017 Canonical
 #
@@ -35,141 +36,73 @@
 # destructive: {{if 'destructive' in name}}True{{else}}False{{endif}}
 # --- End MAAS 1.0 script metadata ---
 
+import argparse
+import os
 import re
-import shlex
 from subprocess import (
-    CalledProcessError,
     check_output,
     DEVNULL,
     PIPE,
     Popen,
     STDOUT,
-    TimeoutExpired,
 )
 import sys
-from threading import Thread
 
-# Short running commands which are used to query info about the drive should
-# work in under a minute otherwise assume a failure.
-TIMEOUT = 60
+import yaml
 
 
-class RunBadBlocks(Thread):
-
-    def __init__(self, drive, destructive=False):
-        super().__init__(name=drive['PATH'])
-        self.drive = drive
-        self.destructive = destructive
-        self.output = b''
-        self.returncode = None
-
-    def run(self):
-        blocksize = check_output(
-            ['sudo', '-n', 'blockdev', '--getbsz', self.drive['PATH']],
-            timeout=TIMEOUT, stderr=DEVNULL).strip()
-        if self.destructive:
-            cmd = [
-                'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-w',
-                self.drive['PATH']]
-        else:
-            cmd = [
-                'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-n',
-                self.drive['PATH']]
-        # Run badblocks and capture its output. Once all threads have completed
-        # output the results serially so output is proerly grouped.
-        with Popen(cmd, stdout=PIPE, stderr=STDOUT) as proc:
-            self.output, _ = proc.communicate()
-            self.returncode = proc.returncode
+REGEX = b", ([0-9]+) bad blocks found"
 
 
-def list_drives():
-    """List all drives available to test
-
-    :return: A list of drives that have SMART data.
-    """
-    # Gather a list of connected ISCSI drives to ignore.
-    try:
-        output = check_output(
-            ['sudo', '-n', 'iscsiadm', '-m', 'session', '-P', '3'],
-            timeout=TIMEOUT, stderr=DEVNULL)
-    except (TimeoutExpired, CalledProcessError):
-        # If this command failed ISCSI is most likely not running/installed.
-        # Ignore the error and move on.
-        iscsi_drives = []
+def run_badblocks(storage, destructive=False):
+    """Run badblocks against storage drive."""
+    result_path = os.environ.get("RESULT_PATH")
+    blocksize = check_output(
+        ['sudo', '-n', 'blockdev', '--getbsz', storage],
+        stderr=DEVNULL).strip().decode()
+    if destructive:
+        cmd = [
+            'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-w',
+            storage]
     else:
-        iscsi_drives = re.findall(
-            'Attached scsi disk (?P<disk>\w+)', output.decode('utf-8'))
+        cmd = [
+            'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-n',
+            storage]
 
-    try:
-        lsblk_output = check_output(
-            [
-                'lsblk', '--exclude', '1,2,7', '-d', '-P', '-o',
-                'NAME,RO,MODEL,SERIAL',
-            ], timeout=TIMEOUT).decode('utf-8')
-    except CalledProcessError:
-        # The SERIAL column is unsupported in the Trusty version of lsblk. Try
-        # again without it.
-        lsblk_output = check_output(
-            [
-                'lsblk', '--exclude', '1,2,7', '-d', '-P', '-o',
-                'NAME,RO,MODEL',
-            ], timeout=TIMEOUT).decode('utf-8')
-    drives = []
-    for line in lsblk_output.splitlines():
-        drive = {}
-        for part in shlex.split(line):
-            key, value = part.split('=')
-            drive[key.strip()] = value.strip()
-        if drive['NAME'] not in iscsi_drives and drive['RO'] == '0':
-            drive['PATH'] = '/dev/%s' % drive['NAME']
-            drives.append(drive)
+    print('Running command: %s\n' % ' '.join(cmd))
+    proc = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+    stdout, _ = proc.communicate()
 
-    return drives
+    # Print stdout to the console.
+    if stdout is not None:
+        print(stdout.decode())
 
+    if result_path is not None:
+        # Parse the results for the desired information and
+        # then wrtie this to the results file.
+        match = re.search(REGEX, stdout)
+        if match is not None:
+            results = {
+                'results': {
+                    'badblocks': int(match.group(1).decode()),
+                }
+            }
+            with open(result_path, 'w') as results_file:
+                yaml.safe_dump(results, results_file)
 
-def run_badblocks(destructive=False):
-    """Run badblocks against all drives on the system.
-
-    Runs badblocks against all drives on the system each in their own thread.
-    Once badblocks has finished output the result.
-
-    :return: The number of drives which badblocks detected as failures.
-    """
-    threads = []
-    for drive in list_drives():
-        thread = RunBadBlocks(drive, destructive)
-        thread.start()
-        threads.append(thread)
-
-    badblock_failures = 0
-    for thread in threads:
-        thread.join()
-        dashes = '-' * int((80.0 - (2 + len(thread.drive['PATH']))) / 2)
-        print('%s %s %s' % (dashes, thread.drive['PATH'], dashes))
-        print('Model:  %s' % thread.drive['MODEL'])
-        # The SERIAL column is only available in newer versions of lsblk. This
-        # can be removed with Trusty support.
-        if 'SERIAL' in thread.drive:
-            print('Serial: %s' % thread.drive['SERIAL'])
-        print()
-
-        if thread.returncode != 0:
-            badblock_failures += 1
-            print('Badblocks exited with %d!' % thread.returncode)
-            print()
-        print(thread.output.decode('utf-8'))
-
-    return badblock_failures
+    return proc.returncode
 
 
 if __name__ == '__main__':
-    # Determine if badblocks should run destructively from the first argument
-    # or script name.
-    if len(sys.argv) > 1 and sys.argv[1] == 'destructive':
-        destructive = True
-    elif 'destructive' in sys.argv[0]:
+    # Determine if badblocks should run destructively from the script name.
+    if 'destructive' in sys.argv[0]:
         destructive = True
     else:
         destructive = False
 
-    sys.exit(run_badblocks(destructive))
+    parser = argparse.ArgumentParser(description='Badblocks Hardware Testing.')
+    parser.add_argument(
+        '--storage', dest='storage',
+        help='path to storage device you want to test. e.g. /dev/sda')
+    args = parser.parse_args()
+    sys.exit(run_badblocks(args.storage, destructive))
--- src/metadataserver/builtin_scripts/tests/test_badblocks.py
+++ src/metadataserver/builtin_scripts/tests/test_badblocks.py
@@ -5,217 +5,112 @@
 
 __all__ = []
 
+
+import io
+import os
+import random
+import re
 from subprocess import (
-    CalledProcessError,
     DEVNULL,
     PIPE,
-    Popen,
     STDOUT,
-    TimeoutExpired,
 )
-from unittest.mock import call
+from textwrap import dedent
+from unittest.mock import ANY
 
-from maasserver.testing.factory import factory
-from maastesting.matchers import (
-    MockCalledOnceWith,
-    MockCallsMatch,
-)
+from maastesting.factory import factory
+from maastesting.matchers import MockCalledOnceWith
 from maastesting.testcase import MAASTestCase
 from metadataserver.builtin_scripts import badblocks
+import yaml
+
+
+BADBLOCKS_OUTPUT = dedent("""
+    Checking for bad blocks in non-destructive read-write mode
+    From block 0 to 5242879
+    Testing with random pattern:
+    Pass completed, 19 bad blocks found. (0/0/19 errors)
+    """)
 
 
 class TestRunBadBlocks(MAASTestCase):
 
-    def make_drive(self, name=None, model=None, serial=None, with_path=False):
-        if name is None:
-            name = factory.make_name('NAME')
-        if model is None:
-            model = factory.make_name('MODEL')
-        if serial is None:
-            serial = factory.make_name('SERIAL')
-        drive = {
-            'NAME': name,
-            'RO': '0',
-            'MODEL': model,
-            'SERIAL': serial,
+    def test_run_badblocks_nondestructive_and_writes_results_file(self):
+        storage = factory.make_name('storage')
+        self.patch(os, "environ", {
+            "RESULT_PATH": factory.make_name()
+        })
+        mock_check_output = self.patch(badblocks, "check_output")
+        blocksize = str(random.choice([1024, 2048, 4096]))
+        mock_check_output.return_value = blocksize.encode('utf-8')
+        cmd = [
+            'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-n', storage]
+        mock_popen = self.patch(badblocks, "Popen")
+        proc = mock_popen.return_value
+        proc.communicate.return_value = (
+            BADBLOCKS_OUTPUT.encode('utf-8'), None)
+        proc.returncode = 0
+        match = re.search(badblocks.REGEX, BADBLOCKS_OUTPUT.encode('utf-8'))
+        mock_open = self.patch(badblocks, "open")
+        mock_open.return_value = io.StringIO()
+        mock_yaml_safe_dump = self.patch(yaml, "safe_dump")
+        results = {
+            'results': {
+                'badblocks': int(match.group(1).decode()),
+            }
         }
-        if with_path:
-            drive['PATH'] = '/dev/%s' % drive['NAME']
-        return drive
 
-    def make_iscsi_line(self, drive=None):
-        if drive is None:
-            drive = self.make_drive()
-        line = (
-            'Attached scsi disk %s        State: running' % drive['NAME'])
-        return line.encode()
+        self.assertEquals(0, badblocks.run_badblocks(storage))
+        self.assertThat(mock_check_output, MockCalledOnceWith(
+            ['sudo', '-n', 'blockdev', '--getbsz', storage],
+            stderr=DEVNULL))
+        self.assertThat(mock_popen, MockCalledOnceWith(
+            cmd, stdout=PIPE, stderr=STDOUT))
+        self.assertThat(mock_open, MockCalledOnceWith(ANY, "w"))
+        self.assertThat(mock_yaml_safe_dump, MockCalledOnceWith(
+            results, mock_open.return_value))
 
-    def make_lsblk_line(self, drive=None):
-        if drive is None:
-            drive = self.make_drive()
-        line = ' '.join(
-            ['%s=%s' % (key, value) for key, value in drive.items()])
-        return line.encode()
+    def test_run_badblocks_destructive(self):
+        storage = factory.make_name('storage')
+        mock_check_output = self.patch(badblocks, "check_output")
+        blocksize = str(random.choice([1024, 2048, 4096]))
+        mock_check_output.return_value = blocksize.encode('utf-8')
+        cmd = [
+            'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-w', storage]
+        mock_popen = self.patch(badblocks, "Popen")
+        proc = mock_popen.return_value
+        proc.communicate.return_value = (
+            BADBLOCKS_OUTPUT.encode('utf-8'), None)
+        proc.returncode = 0
 
-    def test_list_drives(self):
-        mock_check_output = self.patch(badblocks, 'check_output')
-        drive = self.make_drive()
-        mock_check_output.side_effect = [
-            self.make_iscsi_line(),
-            self.make_lsblk_line(drive),
-        ]
-        self.assertDictEqual(
-            {'PATH': '/dev/%s' % drive['NAME'], **drive},
-            badblocks.list_drives()[0])
-        self.assertThat(
-            mock_check_output, MockCallsMatch(
-                call(
-                    ['sudo', '-n', 'iscsiadm', '-m', 'session', '-P', '3'],
-                    timeout=badblocks.TIMEOUT, stderr=DEVNULL),
-                call(
-                    [
-                        'lsblk', '--exclude', '1,2,7', '-d', '-P', '-o',
-                        'NAME,RO,MODEL,SERIAL',
-                    ],
-                    timeout=badblocks.TIMEOUT)))
+        self.assertEquals(
+            0, badblocks.run_badblocks(storage, destructive=True))
+        self.assertThat(mock_check_output, MockCalledOnceWith(
+            ['sudo', '-n', 'blockdev', '--getbsz', storage],
+            stderr=DEVNULL))
+        self.assertThat(mock_popen, MockCalledOnceWith(
+            cmd, stdout=PIPE, stderr=STDOUT))
 
-    def test_list_supported_drives_ignores_iscsiadm_timeout(self):
-        mock_check_output = self.patch(badblocks, 'check_output')
-        drive = self.make_drive()
-        mock_check_output.side_effect = [
-            TimeoutExpired('iscsiadm', 60),
-            self.make_lsblk_line(drive)
-        ]
-        self.assertDictEqual(
-            {'PATH': '/dev/%s' % drive['NAME'], **drive},
-            badblocks.list_drives()[0])
-        self.assertThat(
-            mock_check_output, MockCallsMatch(
-                call(
-                    ['sudo', '-n', 'iscsiadm', '-m', 'session', '-P', '3'],
-                    timeout=badblocks.TIMEOUT, stderr=DEVNULL),
-                call(
-                    [
-                        'lsblk', '--exclude', '1,2,7', '-d', '-P', '-o',
-                        'NAME,RO,MODEL,SERIAL',
-                    ],
-                    timeout=badblocks.TIMEOUT)))
+    def test_run_badblocks_exits_if_no_regex_match_found(self):
+        self.patch(os, "environ", {
+            "RESULT_PATH": factory.make_name()
+        })
+        storage = factory.make_name('storage')
+        mock_check_output = self.patch(badblocks, "check_output")
+        blocksize = str(random.choice([1024, 2048, 4096]))
+        mock_check_output.return_value = blocksize.encode('utf-8')
+        cmd = [
+            'sudo', '-n', 'badblocks', '-b', blocksize, '-v', '-n', storage]
+        mock_popen = self.patch(badblocks, "Popen")
+        proc = mock_popen.return_value
+        proc.communicate.return_value = (
+            BADBLOCKS_OUTPUT.encode('utf-8'), None)
+        proc.returncode = 0
+        mock_re_search = self.patch(re, "search")
+        mock_re_search.return_value = None
 
-    def test_list_supported_drives_ignores_iscsiadm_errors(self):
-        mock_check_output = self.patch(badblocks, 'check_output')
-        drive = self.make_drive()
-        mock_check_output.side_effect = [
-            CalledProcessError('iscsiadm', 60),
-            self.make_lsblk_line(drive)
-        ]
-        self.assertDictEqual(
-            {'PATH': '/dev/%s' % drive['NAME'], **drive},
-            badblocks.list_drives()[0])
-        self.assertThat(
-            mock_check_output, MockCallsMatch(
-                call(
-                    ['sudo', '-n', 'iscsiadm', '-m', 'session', '-P', '3'],
-                    timeout=badblocks.TIMEOUT, stderr=DEVNULL),
-                call(
-                    [
-                        'lsblk', '--exclude', '1,2,7', '-d', '-P', '-o',
-                        'NAME,RO,MODEL,SERIAL',
-                    ],
-                    timeout=badblocks.TIMEOUT)))
-
-    def test_run_destructive(self):
-        drive = self.make_drive(with_path=True)
-        run_bad_blocks = badblocks.RunBadBlocks(drive, True)
-        output = factory.make_string()
-        self.patch(badblocks, 'check_output').return_value = '4096'
-        mock_popen = self.patch(badblocks, 'Popen')
-        mock_popen.return_value = Popen(['echo', '-n', output], stdout=PIPE)
-
-        run_bad_blocks.run()
-
-        self.assertEquals(output.encode(), run_bad_blocks.output)
-        self.assertEquals(0, run_bad_blocks.returncode)
-        self.assertThat(
-            mock_popen,
-            MockCalledOnceWith(
-                [
-                    'sudo', '-n', 'badblocks', '-b', '4096', '-v', '-w',
-                    drive['PATH'],
-                ], stdout=PIPE, stderr=STDOUT))
-
-    def test_run_nondestructive(self):
-        drive = self.make_drive(with_path=True)
-        run_bad_blocks = badblocks.RunBadBlocks(drive, False)
-        output = factory.make_string()
-        self.patch(badblocks, 'check_output').return_value = '4096'
-        mock_popen = self.patch(badblocks, 'Popen')
-        mock_popen.return_value = Popen(['echo', '-n', output], stdout=PIPE)
-
-        run_bad_blocks.run()
-
-        self.assertEquals(output.encode(), run_bad_blocks.output)
-        self.assertEquals(0, run_bad_blocks.returncode)
-        self.assertThat(
-            mock_popen,
-            MockCalledOnceWith(
-                [
-                    'sudo', '-n', 'badblocks', '-b', '4096', '-v', '-n',
-                    drive['PATH'],
-                ], stdout=PIPE, stderr=STDOUT))
-
-    def test_run_badblocks(self):
-        drive = {
-            'NAME': factory.make_name('NAME'),
-            'PATH': factory.make_name('PATH'),
-            'MODEL': factory.make_name('MODEL'),
-            'SERIAL': factory.make_name('SERIAL'),
-        }
-        self.patch(badblocks, 'list_drives').return_value = [drive]
-        output = factory.make_string()
-        self.patch(badblocks, 'check_output').return_value = '4096'
-        mock_popen = self.patch(badblocks, 'Popen')
-        mock_popen.return_value = Popen(['echo', '-n', output], stdout=PIPE)
-        mock_print = self.patch(badblocks, 'print')
-
-        self.assertEquals(0, badblocks.run_badblocks())
-
-        dashes = '-' * int((80.0 - (2 + len(drive['PATH']))) / 2)
-        header = '%s %s %s' % (dashes, drive['PATH'], dashes)
-        self.assertThat(
-            mock_print,
-            MockCallsMatch(
-                call(header),
-                call('Model:  %s' % drive['MODEL']),
-                call('Serial: %s' % drive['SERIAL']),
-                call(),
-                call(output)))
-
-    def test_run_badblocks_outputs_failure(self):
-        drive = {
-            'NAME': factory.make_name('NAME'),
-            'PATH': factory.make_name('PATH'),
-            'MODEL': factory.make_name('MODEL'),
-            'SERIAL': factory.make_name('SERIAL'),
-        }
-        self.patch(badblocks, 'list_drives').return_value = [drive]
-        output = factory.make_string()
-        self.patch(badblocks, 'check_output').return_value = '4096'
-        mock_popen = self.patch(badblocks, 'Popen')
-        mock_popen.return_value = Popen(
-            'echo -n %s; exit 1' % output, stdout=PIPE, shell=True)
-        mock_print = self.patch(badblocks, 'print')
-
-        self.assertEquals(1, badblocks.run_badblocks())
-
-        dashes = '-' * int((80.0 - (2 + len(drive['PATH']))) / 2)
-        header = '%s %s %s' % (dashes, drive['PATH'], dashes)
-        self.assertThat(
-            mock_print,
-            MockCallsMatch(
-                call(header),
-                call('Model:  %s' % drive['MODEL']),
-                call('Serial: %s' % drive['SERIAL']),
-                call(),
-                call('Badblocks exited with 1!'),
-                call(),
-                call(output)))
+        self.assertEquals(0, badblocks.run_badblocks(storage))
+        self.assertThat(mock_popen, MockCalledOnceWith(
+            cmd, stdout=PIPE, stderr=STDOUT))
+        self.assertThat(mock_re_search, MockCalledOnceWith(
+            badblocks.REGEX, BADBLOCKS_OUTPUT.encode('utf-8')))
