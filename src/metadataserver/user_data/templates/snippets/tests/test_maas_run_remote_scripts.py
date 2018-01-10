canonical/maas

commit 593570ca1cd4246c3524afbd219b7245e6d12381

Redownload scripts after finishing commissioning and handle 204 status.
After finishing commissioning redownload the tarball of scripts. This allows
the region to process commissioning script output and add new commissioning
and testing scripts based on available hardware. If there are no new scripts
to run the region will now return 204 which the script runner acknowledges by
successfully terminating.

src/metadataserver/user_data/templates/snippets/maas_run_remote_scripts.py
@@ -22,6 +22,7 @@
 
 import copy
 from datetime import timedelta
+import http.client
 from io import BytesIO
 import json
 import os
@@ -68,6 +69,7 @@ except ImportError:
 
 def fail(msg):
     sys.stderr.write("FAIL: %s" % msg)
+    sys.stderr.flush()
     sys.exit(1)
 
 
@@ -90,14 +92,21 @@ def output_and_send(error, send_result=True, *args, **kwargs):
 def download_and_extract_tar(url, creds, scripts_dir):
     """Download and extract a tar from the given URL.
 
-    The URL may contain a compressed or uncompressed tar.
+    The URL may contain a compressed or uncompressed tar. Returns false when
+    there is no content.
     """
     sys.stdout.write(
         "Downloading and extracting %s to %s\n" % (url, scripts_dir))
-    binary = BytesIO(geturl(url, creds))
+    sys.stdout.flush()
+    ret = geturl(url, creds)
+    if ret.status == int(http.client.NO_CONTENT):
+        return False
+    binary = BytesIO(ret.read())
 
     with tarfile.open(mode='r|*', fileobj=binary) as tar:
         tar.extractall(scripts_dir)
+
+    return True
 
 
 def run_and_check(cmd, scripts, send_result=True):
@@ -324,6 +333,7 @@ def run_script(script, scripts_dir, send_result=True):
     env['RESULT_PATH'] = script['result_path']
     env['DOWNLOAD_PATH'] = script['download_path']
     env['RUNTIME'] = str(timeout_seconds)
+    env['HAS_STARTED'] = str(script.get('has_started', False))
 
     try:
         script_arguments = parse_parameters(script, scripts_dir)
@@ -381,6 +391,7 @@ def run_script(script, scripts_dir, send_result=True):
             'Failed to execute %s: %d' % (
                 script['msg_name'], args['exit_status']), **args)
         sys.stdout.write('%s\n' % stderr)
+        sys.stdout.flush()
         return False
     except TimeoutExpired:
         args['status'] = 'TIMEDOUT'
@@ -547,33 +558,36 @@ def run_scripts_from_metadata(
     commissioning_scripts = scripts.get('commissioning_scripts')
     if commissioning_scripts is not None:
         sys.stdout.write('Starting commissioning scripts...\n')
+        sys.stdout.flush()
         fail_count += run_scripts(
             url, creds, scripts_dir, out_dir, commissioning_scripts,
             send_result)
 
+    if fail_count != 0:
+        output_and_send(
+            '%d commissioning scripts failed to run' % fail_count, send_result,
+            url, creds, 'FAILED')
+        return fail_count
+
+    # After commissioning has successfully finished redownload the scripts tar
+    # in case new hardware was discovered that has an associated script with
+    # the for_hardware field.
+    if commissioning_scripts is not None and download:
+        if not download_and_extract_tar(
+                "%s/maas-scripts/" % url, creds, scripts_dir):
+            return fail_count
+        return run_scripts_from_metadata(
+            url, creds, scripts_dir, out_dir, send_result, download)
+
     testing_scripts = scripts.get('testing_scripts')
-    if fail_count == 0 and testing_scripts is not None:
+    if testing_scripts is not None:
         # If the node status was COMMISSIONING transition the node into TESTING
         # status. If the node is already in TESTING status this is ignored.
         if send_result:
             signal_wrapper(url, creds, 'TESTING')
 
-        # If commissioning previously ran and a test script uses a storage
-        # parameter redownload the script tar as the storage devices may have
-        # changed causing different ScriptResults.
-        if commissioning_scripts is not None and download:
-            for test_script in testing_scripts:
-                for param in test_script.get('parameters', {}).values():
-                    if param['type'] == 'storage':
-                        sys.stdout.write(
-                            "Commissioning complete; updating test "
-                            "scripts...\n")
-                        download_and_extract_tar(
-                            "%s/maas-scripts/" % url, creds, scripts_dir)
-                        return run_scripts_from_metadata(
-                            url, creds, scripts_dir, out_dir, send_result)
-
         sys.stdout.write("Starting testing scripts...\n")
+        sys.stdout.flush()
         fail_count += run_scripts(
             url, creds, scripts_dir, out_dir, testing_scripts, send_result)
 
@@ -584,8 +598,8 @@ def run_scripts_from_metadata(
             'All scripts successfully ran', send_result, url, creds, 'OK')
     else:
         output_and_send(
-            '%d scripts failed to run' % fail_count, send_result, url, creds,
-            'FAILED')
+            '%d test scripts failed to run' % fail_count, send_result, url,
+            creds, 'FAILED')
 
     return fail_count
 
@@ -709,12 +723,14 @@ def main():
     out_dir = os.path.join(args.storage_directory, 'out')
     os.makedirs(out_dir, exist_ok=True)
 
+    has_content = True
     if not args.no_download:
-        download_and_extract_tar(
+        has_content = download_and_extract_tar(
             "%s/maas-scripts/" % url, creds, scripts_dir)
-    run_scripts_from_metadata(
-        url, creds, scripts_dir, out_dir, not args.no_send,
-        not args.no_download)
+    if has_content:
+        run_scripts_from_metadata(
+            url, creds, scripts_dir, out_dir, not args.no_send,
+            not args.no_download)
 
     heart_beat.stop()
 

src/metadataserver/user_data/templates/snippets/tests/test_maas_run_remote_scripts.py
@@ -7,6 +7,7 @@ __all__ = []
 
 import copy
 from datetime import timedelta
+import http.client
 from io import BytesIO
 import json
 import os
@@ -18,6 +19,7 @@ import time
 from unittest.mock import (
     ANY,
     call,
+    MagicMock,
 )
 from zipfile import ZipFile
 
@@ -72,6 +74,7 @@ def make_script(
         'parallel': parallel,
         'hardware_type': hardware_type,
         'args': {},
+        'has_started': factory.pick_bool(),
     }
     ret['msg_name'] = '%s (id: %s, script_version_id: %s)' % (
         name, script_result_id, script_version_id)
@@ -147,6 +150,8 @@ class TestInstallDependencies(MAASTestCase):
         super().setUp()
         self.mock_output_and_send = self.patch(
             maas_run_remote_scripts, 'output_and_send')
+        self.patch(maas_run_remote_scripts.sys.stdout, 'write')
+        self.patch(maas_run_remote_scripts.sys.stderr, 'write')
 
     def test_run_and_check(self):
         scripts_dir = self.useFixture(TempDirectory()).path
@@ -679,6 +684,7 @@ class TestRunScript(MAASTestCase):
         self.assertEquals(script['result_path'], env['RESULT_PATH'])
         self.assertEquals(script['download_path'], env['DOWNLOAD_PATH'])
         self.assertEquals(str(script['timeout_seconds']), env['RUNTIME'])
+        self.assertEquals(str(script['has_started']), env['HAS_STARTED'])
         self.assertIn('PATH', env)
 
     def test_run_script_only_sends_result_when_avail(self):
@@ -930,6 +936,7 @@ class TestRunScripts(MAASTestCase):
             'timeout_seconds': script['timeout_seconds'],
             'parallel': script['parallel'],
             'hardware_type': script['hardware_type'],
+            'has_started': script['has_started'],
         }]
         run_scripts(url, creds, scripts_dir, out_dir, scripts)
         scripts[0].pop('thread', None)
@@ -965,10 +972,25 @@ class TestRunScriptsFromMetadata(MAASTestCase):
             f.write(json.dumps({'1.0': index_json}))
         return index_json
 
+    def mock_download_and_extract_tar(self, url, creds, scripts_dir):
+        """Simulate redownloading a scripts tarball after finishing commiss."""
+        index_path = os.path.join(scripts_dir, 'index.json')
+        with open(index_path, 'r') as f:
+            index_json = json.loads(f.read())
+        index_json['1.0'].pop('commissioning_scripts', None)
+        os.remove(index_path)
+        with open(index_path, 'w') as f:
+            f.write(json.dumps(index_json))
+        return True
+
     def test_run_scripts_from_metadata(self):
         scripts_dir = self.useFixture(TempDirectory()).path
         self.mock_run_scripts.return_value = 0
         index_json = self.make_index_json(scripts_dir)
+        mock_download_and_extract_tar = self.patch(
+            maas_run_remote_scripts, 'download_and_extract_tar')
+        mock_download_and_extract_tar.side_effect = (
+            self.mock_download_and_extract_tar)
 
         # Don't need to give the url, creds, or out_dir as we're not running
         # the scripts and sending the results.
@@ -987,6 +1009,8 @@ class TestRunScriptsFromMetadata(MAASTestCase):
         self.assertThat(self.mock_signal, MockAnyCall(None, None, 'TESTING'))
         self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
             'All scripts successfully ran', True, None, None, 'OK'))
+        self.assertThat(mock_download_and_extract_tar, MockCalledOnceWith(
+            'None/maas-scripts/', None, scripts_dir))
 
     def test_run_scripts_from_metadata_doesnt_run_tests_on_commiss_fail(self):
         scripts_dir = self.useFixture(TempDirectory()).path
@@ -1005,8 +1029,8 @@ class TestRunScriptsFromMetadata(MAASTestCase):
                 index_json['commissioning_scripts'], True))
         self.assertThat(self.mock_signal, MockNotCalled())
         self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
-            '%s scripts failed to run' % fail_count, True, None, None,
-            'FAILED'))
+            '%s commissioning scripts failed to run' % fail_count, True, None,
+            None, 'FAILED'))
 
     def test_run_scripts_from_metadata_redownloads_after_commiss(self):
         scripts_dir = self.useFixture(TempDirectory()).path
@@ -1043,32 +1067,6 @@ class TestRunScriptsFromMetadata(MAASTestCase):
         self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
             'All scripts successfully ran', True, None, None, 'OK'))
 
-    def test_run_scripts_from_metadata_only_redownloads_when_needed(self):
-        scripts_dir = self.useFixture(TempDirectory()).path
-        self.mock_run_scripts.return_value = 0
-        mock_download_and_extract_tar = self.patch(
-            maas_run_remote_scripts, 'download_and_extract_tar')
-        index_json = self.make_index_json(scripts_dir)
-
-        # Don't need to give the url, creds, or out_dir as we're not running
-        # the scripts and sending the results.
-        run_scripts_from_metadata(None, None, scripts_dir, None)
-
-        self.assertThat(
-            self.mock_run_scripts,
-            MockAnyCall(
-                None, None, scripts_dir, None,
-                index_json['commissioning_scripts'], True))
-        self.assertThat(self.mock_signal, MockAnyCall(None, None, 'TESTING'))
-        self.assertThat(mock_download_and_extract_tar, MockNotCalled())
-        self.assertThat(
-            self.mock_run_scripts,
-            MockAnyCall(
-                None, None, scripts_dir, None,
-                index_json['testing_scripts'], True))
-        self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
-            'All scripts successfully ran', True, None, None, 'OK'))
-
     def test_run_scripts_from_metadata_does_nothing_on_empty(self):
         scripts_dir = self.useFixture(TempDirectory()).path
         self.make_index_json(scripts_dir, False, False)
@@ -1094,14 +1092,29 @@ class TestMaasRunRemoteScripts(MAASTestCase):
             tarinfo.mode = 0o755
             tar.addfile(tarinfo, BytesIO(file_content))
         mock_geturl = self.patch(maas_run_remote_scripts, 'geturl')
-        mock_geturl.return_value = binary.getvalue()
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = binary.getvalue()
+        mock_geturl.return_value = mm
 
         # geturl is mocked out so we don't need to give a url or creds.
-        download_and_extract_tar(None, None, scripts_dir)
+        self.assertTrue(download_and_extract_tar(None, None, scripts_dir))
 
         written_file_content = open(
             os.path.join(scripts_dir, 'test-file'), 'rb').read()
         self.assertEquals(file_content, written_file_content)
+
+    def test_download_and_extract_tar_returns_false_on_no_content(self):
+        self.patch(maas_run_remote_scripts.sys.stdout, 'write')
+        scripts_dir = self.useFixture(TempDirectory()).path
+        mock_geturl = self.patch(maas_run_remote_scripts, 'geturl')
+        mm = MagicMock()
+        mm.status = int(http.client.NO_CONTENT)
+        mm.read.return_value = b'No content'
+        mock_geturl.return_value = mm
+
+        # geturl is mocked out so we don't need to give a url or creds.
+        self.assertFalse(download_and_extract_tar(None, None, scripts_dir))
 
     def test_heartbeat(self):
         mock_signal = self.patch(maas_run_remote_scripts, 'signal')

src/provisioningserver/refresh/maas_api_helper.py
@@ -84,7 +84,7 @@ def geturl(url, creds, headers=None, data=None):
         authenticate_headers(url, headers, creds, clockskew)
         try:
             req = urllib.request.Request(url=url, data=data, headers=headers)
-            return urllib.request.urlopen(req).read()
+            return urllib.request.urlopen(req)
         except urllib.error.HTTPError as exc:
             error = exc
             if 'date' not in exc.headers:
@@ -251,11 +251,11 @@ def signal(
         params, ({} if files is None else files))
 
     try:
-        payload = geturl(url, creds=creds, headers=headers, data=data)
-        if payload != b'OK':
+        ret = geturl(url, creds=creds, headers=headers, data=data)
+        if ret.status != 200:
             raise SignalException(
-                "Unexpected result sending region commissioning data: %s" % (
-                    payload))
+                "Unexpected status(%d) sending region commissioning data: %s"
+                % (ret.status, ret.read().decode()))
     except urllib.error.HTTPError as exc:
         raise SignalException("HTTP error [%s]" % exc.code)
     except urllib.error.URLError as exc:

src/provisioningserver/refresh/tests/test_maas_api_helper.py
@@ -18,6 +18,7 @@ from subprocess import (
     TimeoutExpired,
 )
 import time
+from unittest.mock import MagicMock
 import urllib
 
 from lxml import etree
@@ -130,8 +131,8 @@ class TestGetUrl(MAASTestCase):
 
     def test_geturl_sends_request(self):
         self.assertEquals(
-            "mock response",
-            maas_api_helper.geturl("http://%s" % factory.make_hostname(), {}))
+            "mock response", maas_api_helper.geturl(
+                "http://%s" % factory.make_hostname(), {}).read())
 
     def test_geturl_raises_exception_on_failure(self):
         sleep = self.patch(maas_api_helper.time, 'sleep')
@@ -216,7 +217,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
 
@@ -236,7 +240,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         error = factory.make_name('error')
@@ -258,7 +265,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         script_result_id = random.randint(1, 1000)
@@ -281,7 +291,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         exit_status = random.randint(0, 255)
@@ -303,7 +316,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         script_version_id = random.randint(1, 1000)
@@ -326,7 +342,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         power_type = factory.make_name('power_type')
@@ -359,7 +378,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         power_type = 'moonshot'
@@ -392,7 +414,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = b'OK'
+        mm = MagicMock()
+        mm.status = 200
+        mm.read.return_value = b'OK'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
         files = {factory.make_name(): factory.make_bytes()}
@@ -413,7 +438,10 @@ class TestSignal(MAASTestCase):
             maas_api_helper, 'encode_multipart_data')
         mock_encode_multipart_data.return_value = None, None
         mock_geturl = self.patch(maas_api_helper, 'geturl')
-        mock_geturl.return_value = factory.make_name('bad_ret')
+        mm = MagicMock()
+        mm.status = 400
+        mm.read.return_value = b'bad_ret'
+        mock_geturl.return_value = mm
 
         status = factory.make_name('status')
 

commit 0907127bd6dccc96a1b99e56bf583f399f1a94e2

maas_run_remote_scripts should always send a final signal.
593570c introduced a regression where if no test script was selected
maas_run_remote_scripts would not send a final signal telling MAAS
commissioning had finished. This left the machine stuck in
commissioning until it timed out.

src/metadataserver/user_data/templates/snippets/maas_run_remote_scripts.py
@@ -20,6 +20,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import argparse
 import copy
 from datetime import timedelta
 import http.client
@@ -591,16 +592,6 @@ def run_scripts_from_metadata(
         fail_count += run_scripts(
             url, creds, scripts_dir, out_dir, testing_scripts, send_result)
 
-    # Signal success or failure after all scripts have ran. This tells the
-    # region to transistion the status.
-    if fail_count == 0:
-        output_and_send(
-            'All scripts successfully ran', send_result, url, creds, 'OK')
-    else:
-        output_and_send(
-            '%d test scripts failed to run' % fail_count, send_result, url,
-            creds, 'FAILED')
-
     return fail_count
 
 
@@ -650,8 +641,6 @@ class HeartBeat(Thread):
 
 
 def main():
-    import argparse
-
     parser = argparse.ArgumentParser(
         description='Download and run scripts from the MAAS metadata service.')
     parser.add_argument(
@@ -724,13 +713,24 @@ def main():
     os.makedirs(out_dir, exist_ok=True)
 
     has_content = True
+    fail_count = 0
     if not args.no_download:
         has_content = download_and_extract_tar(
             "%s/maas-scripts/" % url, creds, scripts_dir)
     if has_content:
-        run_scripts_from_metadata(
+        fail_count = run_scripts_from_metadata(
             url, creds, scripts_dir, out_dir, not args.no_send,
             not args.no_download)
+
+    # Signal success or failure after all scripts have ran. This tells the
+    # region to transistion the status.
+    if fail_count == 0:
+        output_and_send(
+            'All scripts successfully ran', not args.no_send, url, creds, 'OK')
+    else:
+        output_and_send(
+            '%d test scripts failed to run' % fail_count, not args.no_send,
+            url, creds, 'FAILED')
 
     heart_beat.stop()
 

src/metadataserver/user_data/templates/snippets/tests/test_maas_run_remote_scripts.py
@@ -1007,8 +1007,6 @@ class TestRunScriptsFromMetadata(MAASTestCase):
                 None, None, scripts_dir, None,
                 index_json['testing_scripts'], True))
         self.assertThat(self.mock_signal, MockAnyCall(None, None, 'TESTING'))
-        self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
-            'All scripts successfully ran', True, None, None, 'OK'))
         self.assertThat(mock_download_and_extract_tar, MockCalledOnceWith(
             'None/maas-scripts/', None, scripts_dir))
 
@@ -1064,19 +1062,6 @@ class TestRunScriptsFromMetadata(MAASTestCase):
             MockAnyCall(
                 None, None, scripts_dir, None,
                 index_json['testing_scripts'], True))
-        self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
-            'All scripts successfully ran', True, None, None, 'OK'))
-
-    def test_run_scripts_from_metadata_does_nothing_on_empty(self):
-        scripts_dir = self.useFixture(TempDirectory()).path
-        self.make_index_json(scripts_dir, False, False)
-
-        # Don't need to give the url, creds, or out_dir as we're not running
-        # the scripts and sending the results.
-        run_scripts_from_metadata(None, None, scripts_dir, None)
-
-        self.assertThat(self.mock_output_and_send, MockCalledOnceWith(
-            'All scripts successfully ran', True, None, None, 'OK'))
 
 
 class TestMaasRunRemoteScripts(MAASTestCase):
@@ -1142,3 +1127,49 @@ class TestMaasRunRemoteScripts(MAASTestCase):
         heart_beat.stop()
         self.assertLess(time.time() - start_time, 1)
         self.assertThat(mock_signal, MockCalledOnceWith(url, creds, 'WORKING'))
+
+    def test_main_signals_success(self):
+        self.patch(
+            maas_run_remote_scripts.argparse.ArgumentParser,
+            'parse_args')
+        self.patch(maas_run_remote_scripts, 'read_config')
+        self.patch(maas_run_remote_scripts, 'os')
+        self.patch(maas_run_remote_scripts, 'open')
+        self.patch(
+            maas_run_remote_scripts,
+            'download_and_extract_tar').return_value = True
+        self.patch(
+            maas_run_remote_scripts,
+            'run_scripts_from_metadata').return_value = 0
+        self.patch(maas_run_remote_scripts, 'signal')
+        mock_output_and_send = self.patch(
+            maas_run_remote_scripts, 'output_and_send')
+
+        maas_run_remote_scripts.main()
+
+        self.assertThat(mock_output_and_send, MockCalledOnceWith(
+            'All scripts successfully ran', ANY, ANY, ANY, 'OK'))
+
+    def test_main_signals_failure(self):
+        failures = random.randint(1, 100)
+        self.patch(
+            maas_run_remote_scripts.argparse.ArgumentParser,
+            'parse_args')
+        self.patch(maas_run_remote_scripts, 'read_config')
+        self.patch(maas_run_remote_scripts, 'os')
+        self.patch(maas_run_remote_scripts, 'open')
+        self.patch(
+            maas_run_remote_scripts,
+            'download_and_extract_tar').return_value = True
+        self.patch(
+            maas_run_remote_scripts,
+            'run_scripts_from_metadata').return_value = failures
+        self.patch(maas_run_remote_scripts, 'signal')
+        mock_output_and_send = self.patch(
+            maas_run_remote_scripts, 'output_and_send')
+
+        maas_run_remote_scripts.main()
+
+        self.assertThat(mock_output_and_send, MockCalledOnceWith(
+            '%d test scripts failed to run' % failures, ANY, ANY, ANY,
+            'FAILED'))