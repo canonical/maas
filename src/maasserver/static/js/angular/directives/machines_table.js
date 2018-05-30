391d01b778267187c13cb2d3793fa2a8e0019278
Alberto Donato <alberto.donato@canonical.com>
2018-05-30 10:06:10 +0000

    LP: #1774025 - drop toggle between owner and pool in machine listing

--- src/maasserver/static/js/angular/directives/machines_table.js
+++ src/maasserver/static/js/angular/directives/machines_table.js
@@ -40,10 +40,7 @@ angular.module('MAAS').directive('maasMachinesTable', [
 
         // Scope variables.
         scope.table = {
-            visibleColumns: {
-                'fqdn_mac': 'fqdn',
-                'owner_pool': 'owner',
-          },
+          column: 'fqdn',
           predicate: 'fqdn',
           reverse: false,
           allViewableChecked: false,
@@ -106,9 +103,9 @@ angular.module('MAAS').directive('maasMachinesTable', [
         };
 
         // Sets the viewable column or sorts.
-        scope.selectColumnOrSort = function(predicate, item) {
-            if(scope.table.visibleColumns[item] !== predicate) {
-                scope.table.visibleColumns[item] = predicate;
+        scope.selectColumnOrSort = function(predicate) {
+            if(scope.table.column !== predicate) {
+                scope.table.column = predicate;
             } else {
                 scope.sortTable(predicate);
             }
