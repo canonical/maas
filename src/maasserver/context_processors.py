10c2010b0c86608d42d8d4bba4711f066dc27065
Blake Rouse <blake.rouse@canonical.com>
2018-11-26 15:53:09 -0500

    Fix issue where none administrators are presented with the first user system configuration, but cannot update any settings or skip it.

--- src/maasserver/context_processors.py
+++ src/maasserver/context_processors.py
@@ -24,27 +24,35 @@ def global_options(request):
     uuid = RegionController.objects.get_or_create_uuid()
     with RegionConfiguration.open() as config:
         maas_url = config.maas_url
+    configs = Config.objects.get_configs([
+        'maas_name', 'enable_analytics',
+        'rpc_shared_secret', 'completed_intro'])
     user_completed_intro = False
+    completed_intro = configs['completed_intro']
     if not hasattr(request, 'user'):
         return {}
     if hasattr(request.user, 'userprofile'):
         user_completed_intro = request.user.userprofile.completed_intro
+    if not completed_intro and not request.user.is_superuser:
+        # Only administrators can completed the main intro, normal users
+        # cannot complete it so to them it has been done.
+        completed_intro = True
     if request.user.is_authenticated:
         analytics_user_id = '%s-user%d' % (uuid, request.user.id)
     else:
         analytics_user_id = '%s-anon' % uuid
     return {
         'global_options': {
-            'site_name': Config.objects.get_config('maas_name'),
-            'enable_analytics': Config.objects.get_config('enable_analytics'),
+            'site_name': configs['maas_name'],
+            'enable_analytics': configs['enable_analytics'],
         },
         'debug': settings.DEBUG,
         'version': version,
         'files_version': version.replace(" ", ""),
         'doc_version': get_maas_doc_version(),
         'register_url': maas_url,
-        'register_secret': Config.objects.get_config('rpc_shared_secret'),
-        'completed_intro': Config.objects.get_config('completed_intro'),
+        'register_secret': configs['rpc_shared_secret'],
+        'completed_intro': completed_intro,
         'user_completed_intro': user_completed_intro,
         'analytics_user_id': analytics_user_id,
         'maas_uuid': uuid
