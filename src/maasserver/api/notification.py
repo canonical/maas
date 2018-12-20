ad16616dfa0f1814e6df48cb85c840cb9d193b65
Spencer Seidel <joseph.seidel@canonical.com>
2018-12-20 02:11:40 +0000

    Added API annotations and examples to notifications.

--- src/maasserver/api/notification.py
+++ src/maasserver/api/notification.py
@@ -44,36 +44,58 @@ class NotificationsHandler(OperationsHandler):
         return ('notifications_handler', [])
 
     def read(self, request):
-        """List notifications relevant to the invoking user.
+        """@description-title List notifications
+        @description List notifications relevant to the invoking user.
 
         Notifications that have been dismissed are *not* returned.
+
+        @success (http-status-code) "server-success" 200
+        @success (json) "success-json" A JSON object containing a list of
+        notification objects.
+        @success-example "success-json" [exkey=notifications-read] placeholder
+        text
         """
         return Notification.objects.find_for_user(request.user).order_by('id')
 
     @admin_method
     def create(self, request):
-        """Create a notification.
+        """@description-title Create a notification
+        @description Create a new notification.
 
         This is available to admins *only*.
 
-        :param message: The message for this notification. May contain basic
-            HTML; this will be sanitised before display.
-        :param context: Optional JSON context. The root object *must* be an
-            object (i.e. a mapping). The values herein can be referenced by
-            `message` with Python's "format" (not %) codes.
-        :param category: Optional category. Choose from: error, warning,
-            success, or info. Defaults to info.
+        Note: One of the ``user``, ``users`` or ``admins`` parameters must be
+        set to True for the notification to be visible to anyone.
 
-        :param ident: Optional unique identifier for this notification.
-        :param user: Optional user ID this notification is intended for. By
-            default it will not be targeted to any individual user.
-        :param users: Optional boolean, true to notify all users, defaults to
-            false, i.e. not targeted to all users.
-        :param admins: Optional boolean, true to notify all admins, defaults to
-            false, i.e. not targeted to all admins.
+        @param (string) "message" [required=true] The message for this
+        notification. May contain basic HTML, such as formatting. This string
+        will be sanitised before display so that it doesn't break MAAS HTML.
 
-        Note: if neither user nor users nor admins is set, the notification
-        will not be seen by anyone.
+        @param (string) "context" [required=false] Optional JSON context. The
+        root object *must* be an object (i.e. a mapping). The values herein can
+        be referenced by ``message`` with Python's "format" (not %) codes.
+
+        @param (string) "category" [required=false] Choose from: ``error``,
+        ``warning``, ``success``, or ``info``. Defaults to ``info``.
+
+        @param (string) "ident" [required=false] Unique identifier for this
+        notification.
+
+        @param (string) "user" [required=false] User ID this notification is
+        intended for. By default it will not be targeted to any individual
+        user.
+
+        @param (boolean) "users" [required=false] True to notify all users,
+        defaults to false, i.e. not targeted to all users.
+
+        @param (boolean) "admins" [required=false] True to notify all admins,
+        defaults to false, i.e. not targeted to all admins.
+
+        @success (http-status-code) "server-success" 200
+        @success (json) "success-json" A JSON object containing a new
+        notification object.
+        @success-example "success-json" [exkey=notifications-create]
+        placeholder text
         """
         form = NotificationForm(data=request.data)
         if form.is_valid():
@@ -92,7 +114,22 @@ class NotificationHandler(OperationsHandler):
     fields = DISPLAYED_NOTIFICATION_FIELDS
 
     def read(self, request, id):
-        """Read a specific notification."""
+        """@description-title Read a notification
+        @description Read a notification with the given id.
+
+        @param (int) "{id}" [required=true] The notification id.
+
+        @success (http-status-code) "server-success" 200
+        @success (json) "success-json" A JSON object containing the requested
+        notification object.
+        @success-example "success-json" [exkey=notifications-read-by-id]
+        placeholder text
+
+        @error (http-status-code) "404" 404
+        @error (content) "not-found" The requested notification is not found.
+        @error-example "not-found"
+            Not Found
+        """
         notification = get_object_or_404(Notification, id=id)
         if notification.is_relevant_to(request.user):
             return notification
@@ -103,9 +140,50 @@ class NotificationHandler(OperationsHandler):
 
     @admin_method
     def update(self, request, id):
-        """Update a specific notification.
+        """@description-title Update a notification
+        @description Update a notification with a given id.
 
-        See `NotificationsHandler.create` for field information.
+        This is available to admins *only*.
+
+        Note: One of the ``user``, ``users`` or ``admins`` parameters must be
+        set to True for the notification to be visible to anyone.
+
+        @param (int) "{id}" [required=true] The notification id.
+
+        @param (string) "message" [required=true] The message for this
+        notification. May contain basic HTML, such as formatting. This string
+        will be sanitised before display so that it doesn't break MAAS HTML.
+
+        @param (string) "context" [required=false] Optional JSON context. The
+        root object *must* be an object (i.e. a mapping). The values herein can
+        be referenced by ``message`` with Python's "format" (not %) codes.
+
+        @param (string) "category" [required=false] Choose from: ``error``,
+        ``warning``, ``success``, or ``info``. Defaults to ``info``.
+
+        @param (string) "ident" [required=false] Unique identifier for this
+        notification.
+
+        @param (string) "user" [required=false] User ID this notification is
+        intended for. By default it will not be targeted to any individual
+        user.
+
+        @param (boolean) "users" [required=false] True to notify all users,
+        defaults to false, i.e. not targeted to all users.
+
+        @param (boolean) "admins" [required=false] True to notify all admins,
+        defaults to false, i.e. not targeted to all admins.
+
+        @success (http-status-code) "server-success" 200
+        @success (json) "success-json" A JSON object containing the updated
+        notification object.
+        @success-example "success-json" [exkey=notifications-update]
+        placeholder text
+
+        @error (http-status-code) "404" 404
+        @error (content) "not-found" The requested notification is not found.
+        @error-example "not-found"
+            Not Found
         """
         notification = get_object_or_404(Notification, id=id)
         form = NotificationForm(
@@ -117,19 +195,41 @@ class NotificationHandler(OperationsHandler):
 
     @admin_method
     def delete(self, request, id):
-        """Delete a specific notification."""
+        """@description-title Delete a notification
+        @description Delete a notification with a given id.
+
+        @param (int) "{id}" [required=true] The notification id.
+
+        @success (http-status-code) "server-success" 204
+
+        @error (http-status-code) "404" 404
+        @error (content) "not-found" The requested notification is not found.
+        @error-example "not-found"
+            Not Found
+        """
         notification = get_object_or_404(Notification, id=id)
         notification.delete()
         return rc.DELETED
 
     @operation(idempotent=False)
     def dismiss(self, request, id):
-        """Dismiss a specific notification.
-
-        Returns HTTP 403 FORBIDDEN if this notification is not relevant
-        (targeted) to the invoking user.
+        """@description-title Dismiss a notification
+        @description Dismiss a notification with the given id.
 
         It is safe to call multiple times for the same notification.
+
+        @param (int) "{id}" [required=true] The notification id.
+
+        @success (http-status-code) "server-success" 200
+
+        @error (http-status-code) "403" 403
+        @error (content) "no-perms" The notification is not relevant to the
+        invoking user.
+
+        @error (http-status-code) "404" 404
+        @error (content) "not-found" The requested notification is not found.
+        @error-example "not-found"
+            Not Found
         """
         notification = get_object_or_404(Notification, id=id)
         if notification.is_relevant_to(request.user):
