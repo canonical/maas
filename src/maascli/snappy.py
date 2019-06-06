bac43f3a32ffe80cb83fc8f4c5de696d497927c5
Alberto Donato <alberto.donato@canonical.com>
2019-06-06 07:48:09 +0000

    drop shebang and exec permission from maascli/snappy.py

--- src/maascli/snappy.py
+++ src/maascli/snappy.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-# Copyright 2017 Canonical Ltd.  This software is licensed under the
-# GNU Affero General Public License version 3 (see the file LICENSE).
+# Copyright 2017-2019 Canonical Ltd.  This software is licensed under the GNU
+# Affero General Public License version 3 (see the file LICENSE).
 
 """Snap management commands."""
 
