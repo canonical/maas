aa8501faeb6ec3732621f26d36e9d8270b06cb0e
Alberto Donato <alberto.donato@canonical.com>
2019-09-02 16:50:27 +0000

    drop monkeypatch for django host validator regexp (fixed upstream in 1.11)

--- src/maasserver/monkey.py
+++ src/maasserver/monkey.py
@@ -11,23 +11,9 @@ __all__ = [
 
 from collections import OrderedDict
 import inspect
-import re
 
 from provisioningserver.monkey import add_patches_to_twisted
 import yaml
-
-
-fixed_re = re.compile(r"^([a-z0-9.-]+|\[[a-f0-9]*:[a-f0-9:\.]+\])(:\d+)?$")
-
-
-def fix_django_http_request():
-    """Add support for ipv6-formatted ipv4 addresses to django requests.
-
-       See https://bugs.launchpad.net/ubuntu/+source/python-django/+bug/1611923
-    """
-    import django.http.request
-    if not django.http.request.host_validation_re.match("[::ffff:127.0.0.1]"):
-        django.http.request.host_validation_re = fixed_re
 
 
 def fix_piston_emitter_related():
@@ -83,7 +69,6 @@ def fix_ordereddict_yaml_representer():
 
 def add_patches():
     add_patches_to_twisted()
-    fix_django_http_request()
     fix_piston_emitter_related()
     fix_piston_consumer_delete()
     fix_ordereddict_yaml_representer()
