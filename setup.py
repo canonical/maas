6e1dba4ab571efde1445e67d3724a33b0bb92f0c
Adam Collard <adam.collard@canonical.com>
2020-02-17 14:44:45 +0000

    Release MAAS 2.7.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name="maas",
-    version="2.7.0rc7",
+    version="2.7.0",
     url="https://maas.io/",
     license="AGPLv3",
     description="Metal As A Service",
