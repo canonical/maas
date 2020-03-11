canonical/maas

commit 182d29396be7019615dd3eaa8f8afda0d33d86cc

LP: #1759091 - Limit the amount of queries used during a list operation.
Handlers can now specify a list_queryset which is used when the client calls
list(). The machine, device, and controller handlers have been updated to
only output data needed for the listing page for that node type. All node
types now have their query count tested for list and get.

Backport of 3a0114c for LP: #1830365

src/maasserver/websockets/handlers/machine.py
@@ -54,7 +54,6 @@ from maasserver.forms.interface import (
 from maasserver.forms.interface_link import InterfaceLinkForm
 from maasserver.models.blockdevice import BlockDevice
 from maasserver.models.cacheset import CacheSet
-from maasserver.models.config import Config
 from maasserver.models.filesystem import Filesystem
 from maasserver.models.filesystemgroup import VolumeGroup
 from maasserver.models.interface import Interface
@@ -96,7 +95,20 @@ class MachineHandler(NodeHandler):
 
     class Meta(NodeHandler.Meta):
         abstract = False
-        queryset = node_prefetch(Machine.objects.all()).select_related('bmc')
+        queryset = node_prefetch(Machine.objects.all())
+        list_queryset = (
+            Machine.objects.select_related(
+                'boot_interface', 'owner', 'zone', 'domain')
+            .prefetch_related('blockdevice_set__iscsiblockdevice')
+            .prefetch_related('blockdevice_set__physicalblockdevice')
+            .prefetch_related('blockdevice_set__virtualblockdevice')
+            .prefetch_related(
+                'interface_set__ip_addresses__subnet__vlan__space')
+            .prefetch_related(
+                'interface_set__ip_addresses__subnet__vlan__fabric')
+            .prefetch_related('interface_set__vlan__fabric')
+            .prefetch_related('tags')
+        )
         allowed_methods = [
             'list',
             'get',
@@ -174,42 +186,24 @@ class MachineHandler(NodeHandler):
             "machine",
         ]
 
-    def get_queryset(self):
+    def get_queryset(self, for_list=False):
         """Return `QuerySet` for devices only viewable by `user`."""
         return Machine.objects.get_nodes(
-            self.user, NODE_PERMISSION.VIEW, from_nodes=self._meta.queryset)
-
-    def list(self, params):
-        """List objects.
-
-        Caches default_osystem and default_distro_series so only 2 queries are
-        made for the whole list of nodes.
-
-        Caches the hardware status so only one additional query is needed for
-        all nodes.
-        """
-        self.default_osystem = Config.objects.get_config('default_osystem')
-        self.default_distro_series = Config.objects.get_config(
-            'default_distro_series')
-        return super(MachineHandler, self).list(params)
+            self.user, NODE_PERMISSION.VIEW,
+            from_nodes=super().get_queryset(for_list=for_list))
 
     def dehydrate(self, obj, data, for_list=False):
         """Add extra fields to `data`."""
         data = super(MachineHandler, self).dehydrate(
             obj, data, for_list=for_list)
-        bmc = obj.bmc
-        if bmc is not None and bmc.bmc_type == BMC_TYPE.POD:
-            data['pod'] = bmc.id
 
-        if not for_list:
-            # Add info specific to a machine.
-            data["show_os_info"] = self.dehydrate_show_os_info(obj)
-            devices = [
-                self.dehydrate_device(device)
-                for device in obj.children.all()
-            ]
-            data["devices"] = sorted(
-                devices, key=itemgetter("fqdn"))
+        if obj.is_machine or not for_list:
+            boot_interface = obj.get_boot_interface()
+            if boot_interface is not None:
+                data["pxe_mac"] = "%s" % boot_interface.mac_address
+                data["pxe_mac_vendor"] = obj.get_pxe_mac_vendor()
+            else:
+                data["pxe_mac"] = data["pxe_mac_vendor"] = ""
 
         cpu_script_results = [
             script_result for script_result in
@@ -263,6 +257,18 @@ class MachineHandler(NodeHandler):
                 self.dehydrate_hardware_status_tooltip(script_results))
         else:
             data["status_tooltip"] = ""
+
+        if not for_list:
+            if obj.bmc is not None and obj.bmc.bmc_type == BMC_TYPE.POD:
+                data['pod'] = self.dehydrate_pod(obj.bmc)
+            # Add info specific to a machine.
+            data["show_os_info"] = self.dehydrate_show_os_info(obj)
+            devices = [
+                self.dehydrate_device(device)
+                for device in obj.children.all()
+            ]
+            data["devices"] = sorted(
+                devices, key=itemgetter("fqdn"))
 
         return data
 

src/maasserver/websockets/handlers/tests/test_machine.py
@@ -150,11 +150,12 @@ class TestMachineHandler(MAASServerTestCase):
 
         boot_interface = node.get_boot_interface()
         pxe_mac_vendor = node.get_pxe_mac_vendor()
+        subnets = handler.get_all_subnets(node)
+
         blockdevices = [
             blockdevice.actual_instance
             for blockdevice in node.blockdevice_set.all()
             ]
-        driver = get_third_party_driver(node)
         disks = [
             handler.dehydrate_blockdevice(blockdevice, node)
             for blockdevice in blockdevices
@@ -167,8 +168,10 @@ class TestMachineHandler(MAASServerTestCase):
             for cache_set in CacheSet.objects.get_cache_sets_for_node(node)
         ]
         disks = sorted(disks, key=itemgetter("name"))
-        subnets = handler.get_all_subnets(node)
-        commissioning_scripts = node.get_latest_commissioning_script_results
+        driver = get_third_party_driver(node)
+
+        commissioning_scripts = (
+            node.get_latest_commissioning_script_results)
         commissioning_scripts = commissioning_scripts.exclude(
             status=SCRIPT_STATUS.ABORTED)
         testing_scripts = node.get_latest_testing_script_results
@@ -179,6 +182,7 @@ class TestMachineHandler(MAASServerTestCase):
             if (script_result.name in script_output_nsmap and
                     script_result.status == SCRIPT_STATUS.PASSED):
                 log_results.add(script_result.name)
+
         data = {
             "actions": list(compile_node_actions(node, handler.user).keys()),
             "architecture": node.architecture,
@@ -186,18 +190,20 @@ class TestMachineHandler(MAASServerTestCase):
             "boot_disk": node.boot_disk,
             "bios_boot_method": node.bios_boot_method,
             "commissioning_script_count": commissioning_scripts.count(),
-            "commissioning_status": get_status_from_qs(commissioning_scripts),
+            "commissioning_status": get_status_from_qs(
+                commissioning_scripts),
             "commissioning_status_tooltip": (
                 handler.dehydrate_hardware_status_tooltip(
                     commissioning_scripts).replace(
                         'test', 'commissioning script')),
             "current_commissioning_script_set": (
                 node.current_commissioning_script_set_id),
+            "current_testing_script_set": node.current_testing_script_set_id,
             "testing_script_count": testing_scripts.count(),
             "testing_status": get_status_from_qs(testing_scripts),
             "testing_status_tooltip": (
-                handler.dehydrate_hardware_status_tooltip(testing_scripts)),
-            "current_testing_script_set": node.current_testing_script_set_id,
+                handler.dehydrate_hardware_status_tooltip(
+                    testing_scripts)),
             "current_installation_script_set": (
                 node.current_installation_script_set_id),
             "installation_status": (
@@ -230,7 +236,7 @@ class TestMachineHandler(MAASServerTestCase):
             "supported_filesystems": [
                 {'key': key, 'ui': ui}
                 for key, ui in FILESYSTEM_FORMAT_TYPE_CHOICES],
-            "distro_series": node.get_distro_series(),
+            "distro_series": node.distro_series,
             "error": node.error,
             "error_description": node.error_description,
             "events": handler.dehydrate_events(node),
@@ -250,10 +256,9 @@ class TestMachineHandler(MAASServerTestCase):
             "license_key": node.license_key,
             "link_type": NODE_TYPE_TO_LINK_TYPE[node.node_type],
             "memory": node.display_memory(),
-            "metadata": {},
             "node_type_display": node.get_node_type_display(),
             "min_hwe_kernel": node.min_hwe_kernel,
-            "osystem": node.get_osystem(),
+            "osystem": node.osystem,
             "owner": handler.dehydrate_owner(node.owner),
             "power_parameters": handler.dehydrate_power_parameters(
                 node.power_parameters),
@@ -290,7 +295,6 @@ class TestMachineHandler(MAASServerTestCase):
             "updated": dehydrate_datetime(node.updated),
             "zone": handler.dehydrate_zone(node.zone),
             "default_user": node.default_user,
-            "dhcp_on": node.interface_set.filter(vlan__dhcp_on=True).exists(),
         }
         bmc = node.bmc
         if bmc is not None and bmc.bmc_type == BMC_TYPE.POD:
@@ -330,6 +334,13 @@ class TestMachineHandler(MAASServerTestCase):
             for key in list(data):
                 if key not in allowed_fields:
                     del data[key]
+        else:
+            data.update({
+                "dhcp_on": node.interface_set.filter(
+                    vlan__dhcp_on=True).exists(),
+                "grouped_storages": handler.get_grouped_storages(blockdevices),
+                "metadata": {},
+            })
 
         cpu_script_results = [
             script_result for script_result in
@@ -391,8 +402,6 @@ class TestMachineHandler(MAASServerTestCase):
                     script_results))
         else:
             data["status_tooltip"] = ""
-
-        data["grouped_storages"] = handler.get_grouped_storages(blockdevices)
 
         return data
 
@@ -492,10 +501,39 @@ class TestMachineHandler(MAASServerTestCase):
         # number means regiond has to do more work slowing down its process
         # and slowing down the client waiting for the response.
         self.assertEqual(
-            queries_one, 12,
+            queries_one, 8,
             "Number of queries has changed; make sure this is expected.")
         self.assertEqual(
-            queries_total, 12,
+            queries_total, 8,
+            "Number of queries has changed; make sure this is expected.")
+
+    def test_get_num_queries_is_the_expected_number(self):
+        owner = factory.make_User()
+        node = factory.make_Node(owner=owner)
+        commissioning_script_set = factory.make_ScriptSet(
+            node=node, result_type=RESULT_TYPE.COMMISSIONING)
+        testing_script_set = factory.make_ScriptSet(
+            node=node, result_type=RESULT_TYPE.TESTING)
+        node.current_commissioning_script_set = commissioning_script_set
+        node.current_testing_script_set = testing_script_set
+        node.save()
+        for __ in range(10):
+            factory.make_ScriptResult(
+                status=SCRIPT_STATUS.PASSED,
+                script_set=commissioning_script_set)
+            factory.make_ScriptResult(
+                status=SCRIPT_STATUS.PASSED,
+                script_set=testing_script_set)
+
+        handler = MachineHandler(owner, {})
+        queries, _ = count_queries(handler.get, {'system_id': node.system_id})
+        # This check is to notify the developer that a change was made that
+        # affects the number of queries performed when doing a node get.
+        # It is important to keep this number as low as possible. A larger
+        # number means regiond has to do more work slowing down its process
+        # and slowing down the client waiting for the response.
+        self.assertEqual(
+            queries, 47,
             "Number of queries has changed; make sure this is expected.")
 
     def test_trigger_update_updates_script_result_cache(self):
@@ -518,6 +556,7 @@ class TestMachineHandler(MAASServerTestCase):
 
         handler = MachineHandler(owner, {})
         # Simulate a trigger pushing an update to the UI
+        handler.cache = {'active_pk': node.system_id}
         _, _, ret = handler.on_listen_for_active_pk(
             'update', node.system_id, node)
         self.assertEquals(ret['commissioning_script_count'], 10)
@@ -1627,7 +1666,7 @@ class TestMachineHandler(MAASServerTestCase):
         # number means regiond has to do more work slowing down its process
         # and slowing down the client waiting for the response.
         self.assertEqual(
-            query_10_count, 15,
+            query_10_count, 11,
             "Number of queries has changed; make sure this is expected.")
         self.assertEqual(
             query_10_count, query_20_count,

src/maasserver/third_party_drivers.py
@@ -155,13 +155,14 @@ def populate_kernel_opts(driver):
     return driver
 
 
-def get_third_party_driver(node):
+def get_third_party_driver(node, detected_aliases=None):
     """Determine which, if any, third party driver is required.
 
     Use the node's modaliases strings to determine if a third party
     driver is required.
     """
-    detected_aliases = node_modaliases(node)
+    if detected_aliases is None:
+        detected_aliases = node_modaliases(node)
 
     third_party_drivers_config = DriversConfig.load_from_cache()
     third_party_drivers = third_party_drivers_config['drivers']

src/maasserver/websockets/base.py
@@ -80,6 +80,7 @@ class HandlerOptions(object):
     handler_name = None
     object_class = None
     queryset = None
+    list_queryset = None
     pk = 'id'
     pk_type = int
     fields = None
@@ -280,19 +281,22 @@ class Handler(metaclass=HandlerMetaclass):
             })
         pk = params[self._meta.pk]
         try:
-            obj = self.get_queryset().get(**{
+            obj = self.get_queryset(for_list=False).get(**{
                 self._meta.pk: pk,
                 })
         except self._meta.object_class.DoesNotExist:
             raise HandlerDoesNotExistError(pk)
         return obj
 
-    def get_queryset(self):
+    def get_queryset(self, for_list=False):
         """Return `QuerySet` used by this handler.
 
         Override if you need to modify the queryset based on the current user.
         """
-        return self._meta.queryset
+        if for_list and self._meta.list_queryset is not None:
+            return self._meta.list_queryset
+        else:
+            return self._meta.queryset
 
     def get_form_class(self, action):
         """Return the form class used for `action`.
@@ -349,7 +353,7 @@ class Handler(metaclass=HandlerMetaclass):
         :param offset: Offset into the queryset to return.
         :param limit: Maximum number of objects to return.
         """
-        queryset = self.get_queryset()
+        queryset = self.get_queryset(for_list=True)
         queryset = queryset.order_by(self._meta.batch_key)
         if "start" in params:
             queryset = queryset.filter(**{

src/maasserver/websockets/handlers/controller.py
@@ -24,8 +24,12 @@ class ControllerHandler(MachineHandler):
     class Meta(MachineHandler.Meta):
         abstract = False
         queryset = node_prefetch(
-            Controller.controllers.all().prefetch_related("interface_set"),
-            'controllerinfo'
+            Controller.controllers.all().prefetch_related(
+                "service_set"), "controllerinfo")
+        list_queryset = (
+            Controller.controllers.all()
+            .select_related("controllerinfo", "domain")
+            .prefetch_related("service_set")
         )
         allowed_methods = [
             'list',
@@ -87,10 +91,14 @@ class ControllerHandler(MachineHandler):
         else:
             raise HandlerError("Unknown action: %s" % action)
 
-    def get_queryset(self):
+    def get_queryset(self, for_list=False):
         """Return `QuerySet` for controllers only viewable by `user`."""
+        if for_list:
+            qs = self._meta.list_queryset
+        else:
+            qs = self._meta.queryset
         return Controller.controllers.get_nodes(
-            self.user, NODE_PERMISSION.VIEW, from_nodes=self._meta.queryset)
+            self.user, NODE_PERMISSION.VIEW, from_nodes=qs)
 
     def dehydrate(self, obj, data, for_list=False):
         obj = obj.as_self()
@@ -105,14 +113,15 @@ class ControllerHandler(MachineHandler):
             if version.is_snap:
                 long_version += " (snap)"
             data["version__long"] = long_version
-        data["vlan_ids"] = [
-            interface.vlan_id
-            for interface in obj.interface_set.all()
-        ]
         data["service_ids"] = [
             service.id
             for service in obj.service_set.all()
         ]
+        if not for_list:
+            data["vlan_ids"] = [
+                interface.vlan_id
+                for interface in obj.interface_set.all()
+            ]
         return data
 
     def check_images(self, params):

src/maasserver/websockets/handlers/device.py
@@ -37,10 +37,7 @@ from maasserver.websockets.base import (
     HandlerPermissionError,
     HandlerValidationError,
 )
-from maasserver.websockets.handlers.node import (
-    node_prefetch,
-    NodeHandler,
-)
+from maasserver.websockets.handlers.node import NodeHandler
 from netaddr import EUI
 from provisioningserver.logger import get_maas_logger
 
@@ -65,7 +62,16 @@ class DeviceHandler(NodeHandler):
 
     class Meta(NodeHandler.Meta):
         abstract = False
-        queryset = node_prefetch(Device.objects.filter(parent=None))
+        queryset = (
+            Device.objects.filter(parent=None).select_related(
+                'boot_interface', 'owner', 'zone', 'domain')
+            .prefetch_related(
+                'interface_set__ip_addresses__subnet__vlan__space')
+            .prefetch_related(
+                'interface_set__ip_addresses__subnet__vlan__fabric')
+            .prefetch_related('interface_set__vlan__fabric')
+            .prefetch_related('tags')
+        )
         allowed_methods = [
             'list',
             'get',
@@ -80,6 +86,7 @@ class DeviceHandler(NodeHandler):
             'update',
             'action']
         exclude = [
+            "bmc",
             "creation_type",
             "type",
             "boot_interface",
@@ -144,10 +151,11 @@ class DeviceHandler(NodeHandler):
         getpk = attrgetter(self._meta.pk)
         self.cache["loaded_pks"].update(getpk(obj) for obj in objs)
 
-    def get_queryset(self):
+    def get_queryset(self, for_list=False):
         """Return `QuerySet` for devices only viewable by `user`."""
         return Device.objects.get_nodes(
-            self.user, NODE_PERMISSION.VIEW, from_nodes=self._meta.queryset)
+            self.user, NODE_PERMISSION.VIEW, from_nodes=super().get_queryset(
+                for_list=for_list))
 
     def dehydrate_parent(self, parent):
         if parent is None:

src/maasserver/websockets/handlers/event.py
@@ -75,7 +75,7 @@ class EventHandler(TimestampedModelHandler):
         """
         node = self.get_node(params)
         self.cache['node_ids'].append(node.id)
-        queryset = self.get_queryset()
+        queryset = self.get_queryset(for_list=True)
         queryset = queryset.filter(node=node)
         queryset = queryset.order_by('-id')
 

commit 49c39fc9e63ea114d38480a5a832be7066a124f9

Fix regression introduced in 182d29396be7019
Fixes LP:1835480, don't call dehydrate_pod method which doesn't exist

src/maasserver/websockets/handlers/machine.py
@@ -260,7 +260,7 @@ class MachineHandler(NodeHandler):
 
         if not for_list:
             if obj.bmc is not None and obj.bmc.bmc_type == BMC_TYPE.POD:
-                data['pod'] = self.dehydrate_pod(obj.bmc)
+                data['pod'] = obj.bmc.id
             # Add info specific to a machine.
             data["show_os_info"] = self.dehydrate_show_os_info(obj)
             devices = [

src/maasserver/websockets/handlers/tests/test_machine.py
@@ -2965,6 +2965,16 @@ class TestMachineHandler(MAASServerTestCase):
                     "disk_type": Equals('iscsi')
                     })]))
 
+    def test_dehydrate_machine_on_pod(self):
+        user = factory.make_admin()
+        handler = MachineHandler(user, {})
+        pod = factory.make_Pod()
+        architecture = make_usable_architecture(self)
+        node = factory.make_Node(interface=True, architecture=architecture)
+        node.bmc = pod
+        observed = handler.get({"system_id": node.system_id})
+        self.assertEqual(observed['pod'], pod.id)
+
 
 class TestMachineHandlerCheckPower(MAASTransactionServerTestCase):
 