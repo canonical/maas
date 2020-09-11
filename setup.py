0ae9047d51393d41504b9449c7fd8307ed2c0ac2
Lee Trager <lee.trager@canonical.com>
2020-09-11 18:15:49 +0000

    Release 2.9.0 beta2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name="maas",
-    version="2.9.0b1",
+    version="2.9.0b2",
     url="https://maas.io/",
     license="AGPLv3",
     description="Metal As A Service",
