b1c430a314ebe432d5461035908d1cd41a844929
Dougal Matthews <dougal.matthews@canonical.com>
2020-10-29 20:25:45 +0000

    LP 1741913: Patch twisted.web.http.Request to include the upstream fix
    See https://github.com/twisted/twisted/commit/169fd1d93b7af06bf0f6893b193ce19970881868

--- src/maasserver/monkey.py
+++ src/maasserver/monkey.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2017 Canonical Ltd.  This software is licensed under the
+# Copyright 2016-2020 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """
@@ -10,6 +10,7 @@ __all__ = ["add_patches"]
 from collections import OrderedDict
 import inspect
 
+from twisted.web import http
 import yaml
 
 from provisioningserver.monkey import add_patches_to_twisted
@@ -95,9 +96,40 @@ def fix_ordereddict_yaml_representer():
     yaml.add_representer(OrderedDict, dumper, Dumper=yaml.SafeDumper)
 
 
+OriginalRequest = http.Request
+
+
+class PatchedRequest(OriginalRequest):
+    def write(self, data):
+        if self.finished:
+            raise RuntimeError(
+                "Request.write called on a request after "
+                "Request.finish was called."
+            )
+
+        if self._disconnected:
+            # Don't attempt to write any data to a disconnected client.
+            # The RuntimeError exception will be thrown as usual when
+            # request.finish is called
+            return
+
+        return OriginalRequest.write(self, data)
+
+
+def fix_twisted_disconnect_write():
+    """
+    Patch twisted.web.http.Request to include the upstream fix
+
+    See https://github.com/twisted/twisted/commit/169fd1d93b7af06bf0f6893b193ce19970881868
+    """
+
+    http.Request = PatchedRequest
+
+
 def add_patches():
     add_patches_to_twisted()
     fix_django_deferred_attribute()
     fix_piston_emitter_related()
     fix_piston_consumer_delete()
     fix_ordereddict_yaml_representer()
+    fix_twisted_disconnect_write()
---
+++ src/maasserver/tests/test_monkey.py
@@ -0,0 +1,40 @@
+# Copyright 2020 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Test monkey patches."""
+
+__all__ = []
+
+from twisted import version as twisted_version
+from twisted.internet.error import ConnectionLost
+from twisted.python.failure import Failure
+from twisted.web import http
+from twisted.web.test.requesthelper import DummyChannel
+
+from maastesting.testcase import MAASTestCase
+
+
+class TestTwistedDisconnectPatch(MAASTestCase):
+    def test_write_after_connection_lost(self):
+        """
+        Calling L{Request.write} after L{Request.connectionLost} has been
+        called should not throw an exception. L{RuntimeError} will be raised
+        when finish is called on the request.
+
+        NOTE: This test is taken from the upstream fix to verify the monkey
+              patch: https://github.com/twisted/twisted/commit/169fd1d93b7af06bf0f6893b193ce19970881868
+        """
+        channel = DummyChannel()
+        req = http.Request(channel, False)
+        req.connectionLost(Failure(ConnectionLost("The end.")))
+        req.write(b"foobar")
+        self.assertRaises(RuntimeError, req.finish)
+
+    def test_twisted_version(self):
+
+        self.assertLessEqual(
+            (twisted_version.major, twisted_version.minor),
+            (19, 7),
+            "The fix_twisted_disconnect_write monkey patch is not longer "
+            "required in Twisted versions >= 19.7.0",
+        )
