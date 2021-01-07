0ebf4fb25030c18431ad1acc49a3985b875e2b8f
Lee Trager <lee.trager@canonical.com>
2021-01-07 22:09:10 +0000

    LP: #1910473 - Don't assume NUMA nodes maintain linear continuity.
    Cherry pick of ed1ad1bc2

--- src/metadataserver/builtin_scripts/hooks.py
+++ src/metadataserver/builtin_scripts/hooks.py
@@ -482,15 +482,17 @@ def _process_lxd_resources(node, data):
     # Update memory.
     node.memory, numa_nodes = _parse_memory(data.get("memory", {}), numa_nodes)
 
-    # Create or update NUMA nodes.
-    numa_nodes = [
-        NUMANode.objects.update_or_create(
+    # Create or update NUMA nodes. This must be kept as a dictionary as not all
+    # systems maintain linear continuity. e.g the PPC64 machine in our CI uses
+    # 0, 1, 16, 17.
+    numa_nodes = {
+        numa_index: NUMANode.objects.update_or_create(
             node=node,
             index=numa_index,
             defaults={"memory": numa_data.memory, "cores": numa_data.cores},
         )[0]
         for numa_index, numa_data in numa_nodes.items()
-    ]
+    }
 
     # Network interfaces
     # LP: #1849355 -- Don't update the node network information
