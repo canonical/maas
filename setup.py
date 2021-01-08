30352b798e5b5baec3da97992edf8637679eff75
Adam Collard <adam.collard@canonical.com>
2021-01-08 12:20:19 +0000

    Release 2.9.1 RC1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name="maas",
-    version="2.9.1b1",
+    version="2.9.1rc1",
     url="https://maas.io/",
     license="AGPLv3",
     description="Metal As A Service",
