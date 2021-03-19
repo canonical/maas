d6db43975fab67d1ccf63210739d48560a46cc5c
Adam Collard <adam.collard@canonical.com>
2021-03-19 15:50:52 +0000

    MD2 is active since 2.9.3.

--- src/maasserver/deprecations.py
+++ src/maasserver/deprecations.py
@@ -31,7 +31,7 @@ class Deprecation:
 DEPRECATIONS = {
     "RSD_REMOVAL": Deprecation(
         id="MD2",
-        since="2.9.4",
+        since="2.9.3",
         description=(
             "Support for RSD pods will be dropped in the next major release."
         ),
