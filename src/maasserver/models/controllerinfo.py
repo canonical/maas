bf744685af89a537a50c92cb6990e3c5d8372fb1
Alberto Donato <alberto.donato@canonical.com>
2021-04-22 13:06:05 +0000

    change logic for notifications about controllers with different versions
    This drops the old notification about different version in controller and adds
    a new notification which is shown when there are issues with controllers setup.
    Specifically:
    - controllers on different versions
    - controllers with different ugprades origins or cohorts
    - controllers that see different updates
    
    A separate notification will be added to signal the state of upgrades.

--- src/maasserver/models/controllerinfo.py
+++ src/maasserver/models/controllerinfo.py
@@ -1,8 +1,6 @@
 # Copyright 2017 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""ControllerInfo objects."""
-
 
 from datetime import datetime
 from typing import NamedTuple, Optional
@@ -16,9 +14,9 @@ from django.db.models import (
 )
 
 from maasserver import DefaultMeta
-from maasserver.enum import NODE_TYPE
 from maasserver.models.cleansave import CleanSave
 from maasserver.models.node import Node
+from maasserver.models.notification import Notification
 from maasserver.models.timestampedmodel import TimestampedModel
 from provisioningserver.enum import (
     CONTROLLER_INSTALL_TYPE,
@@ -32,39 +30,6 @@ class TargetVersion(NamedTuple):
 
     version: MAASVersion
     first_reported: Optional[datetime] = None
-
-
-class ControllerVersionInfo(NamedTuple):
-
-    hostname: str
-    system_id: str
-    version: str
-    maasversion: MAASVersion
-
-    def difference(self, other):
-        v1 = self.maasversion
-        v2 = other.maasversion
-        # No difference in the numeric versions
-        if v1 == v2:
-            return None, None
-        elif v1.short_version == v2.short_version:
-            # If versions match up to the qualifier version, show the full
-            # strings
-            return self.full_string, other.full_string
-        else:
-            # Only difference is the revision number, so just display the
-            # full string.
-            return (
-                self.maasversion.short_version,
-                other.maasversion.short_version,
-            )
-
-    @property
-    def full_string(self):
-        pretty_version = self.maasversion.short_version
-        if self.maasversion.extended_info:
-            pretty_version += f" ({self.maasversion.extended_info})"
-        return pretty_version
 
 
 class ControllerInfoManager(Manager):
@@ -165,131 +130,57 @@ class ControllerInfoManager(Manager):
             return TargetVersion(highest_update, update_first_reported)
         return TargetVersion(highest_version)
 
-    def get_controller_version_info(self):
-        versions = list(
-            self.select_related("node")
-            .filter(
-                node__node_type__in=(
-                    NODE_TYPE.RACK_CONTROLLER,
-                    NODE_TYPE.REGION_CONTROLLER,
-                    NODE_TYPE.REGION_AND_RACK_CONTROLLER,
-                ),
-            )
-            .exclude(version="")
-            .values_list("node__hostname", "node__system_id", "version")
-        )
-        for i in range(len(versions)):
-            version_info = list(versions[i])
-            version_info.append(MAASVersion.from_string(version_info[-1]))
-            versions[i] = ControllerVersionInfo(*version_info)
-        return sorted(versions, key=lambda version: version[-1], reverse=True)
 
-
-VERSION_NOTIFICATION_IDENT = "controller_out_of_date_"
-
-
-def create_or_update_version_notification(system_id, message, context):
-    # Circular imports.
-    from maasserver.models import Notification
-
-    ident = VERSION_NOTIFICATION_IDENT + system_id
-    existing_notification = Notification.objects.filter(ident=ident).first()
-    if existing_notification is not None:
-        existing_notification.message = message
-        existing_notification.context = context
-        existing_notification.save()
-    else:
-        Notification.objects.create_warning_for_admins(
-            message, context=context, ident=ident
-        )
-
-
-KNOWN_VERSION_MISMATCH_NOTIFICATION = (
-    "Controller <a href='l/controller/{system_id}'>{hostname}</a> is "
-    "running an older version of MAAS ({v1})."
-)
-
-UNKNOWN_VERSION_MISMATCH_NOTIFICATION = (
-    "Controller <a href='l/controller/{system_id}'>{hostname}</a> "
-    "is running an older version of MAAS (less than 2.3.0)."
-)
+UPGRADE_ISSUE_NOTIFICATION_IDENT = "upgrade_version_issue"
 
 
 def update_version_notifications():
-    notifications = {}
-    # Circular imports.
-    from maasserver.models import Controller, Notification
+    info = ControllerInfo.objects
+    multiple_install_types = info.values("install_type").distinct().count() > 1
+    multiple_origins = info.values("update_origin").distinct().count() > 1
+    multiple_cohorts = info.values("snap_cohort").distinct().count() > 1
+    multiple_versions = info.values("version").distinct().count() > 1
+    multiple_upgrade_versions = (
+        info.exclude(update_version="")
+        .values("update_version")
+        .distinct()
+        .count()
+        > 1
+    )
 
-    controller_system_ids = set(
-        Controller.objects.all().values_list("system_id", flat=True)
-    )
-    controller_version_info = (
-        ControllerInfo.objects.get_controller_version_info()
-    )
-    now_possibly_irrelevant_notifications = set(
-        Notification.objects.filter(
-            ident__startswith=VERSION_NOTIFICATION_IDENT
-        ).values_list("ident", flat=True)
-    )
-    just_one_controller = len(controller_system_ids) == 1
-    if len(controller_version_info) == 0 or just_one_controller:
-        # No information means no notifications should be presented, and
-        # any existing notifications should be removed.
-        Notification.objects.filter(
-            ident__in=now_possibly_irrelevant_notifications
-        ).delete()
-        return
-    # The list is sorted with the first element being the controller
-    # with the highest version. So we can use that to compare with
-    # the remaining controllers.
-    latest_controller_version_info = controller_version_info[0]
-    latest_version = latest_controller_version_info.maasversion
-    for controller in controller_version_info:
-        if controller.maasversion < latest_version:
-            v1, v2 = controller.difference(latest_controller_version_info)
-            context = dict(
-                message=KNOWN_VERSION_MISMATCH_NOTIFICATION,
-                hostname=controller.hostname,
-                v1=v1,
-                v2=v2,
-                system_id=controller.system_id,
-            )
-            notifications[controller.system_id] = context
-            now_possibly_irrelevant_notifications.discard(
-                VERSION_NOTIFICATION_IDENT + controller.system_id
-            )
-        else:
-            # This will indicate that a notification isn't required, or
-            # any existing notification should be deleted.
-            notifications[controller.system_id] = None
-            controller_system_ids.discard(controller.system_id)
-    for system_id, context in notifications.items():
-        ident = VERSION_NOTIFICATION_IDENT + system_id
-        if context is None:
-            Notification.objects.filter(ident=ident).delete()
-            continue
-        message = context.pop("message")
-        create_or_update_version_notification(system_id, message, context)
-        controller_system_ids.discard(system_id)
-    # The remaining items in the controller_system_ids set will be
-    # controllers old enough that we don't know their version.
-    for system_id in controller_system_ids:
-        controller = Controller.objects.filter(system_id=system_id).first()
-        message = UNKNOWN_VERSION_MISMATCH_NOTIFICATION
-        context = dict(
-            hostname=controller.hostname,
-            system_id=controller.system_id,
-            latest_version=(
-                latest_controller_version_info.maasversion.short_version
-            ),
+    def set_warning(reason, message):
+        defaults = {
+            "category": "warning",
+            "admins": True,
+            "message": f"{message}. <a href='l/controllers'>Review controllers.</a>",
+            "context": {"reason": reason},
+        }
+        notification, created = Notification.objects.get_or_create(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT,
+            defaults=defaults,
         )
-        create_or_update_version_notification(system_id, message, context)
-    # Delete any remaining notifications. These might be for controllers
-    # that no longer exist. Any current notifications will have been
-    # discarded from the set of existing notifications.
-    Notification.objects.filter(
-        ident__in=now_possibly_irrelevant_notifications
-    ).delete()
+        if not created and notification.context["reason"] != reason:
+            # create a new notification so that it shows even if users have
+            # dismissed the previous one
+            notification.delete()
+            Notification.objects.create(
+                ident=UPGRADE_ISSUE_NOTIFICATION_IDENT, **defaults
+            )
+
+    if any((multiple_install_types, multiple_origins, multiple_cohorts)):
+        set_warning(
+            "install_source", "Controllers have different installation sources"
+        )
+    elif multiple_upgrade_versions:
+        set_warning(
+            "upgrade_versions", "Controllers report different upgrade versions"
+        )
+    elif multiple_versions:
+        set_warning("versions", "Controllers have different versions")
+    else:
+        Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).delete()
 
 
 class ControllerInfo(CleanSave, TimestampedModel):
--- src/maasserver/models/tests/test_controllerinfo.py
+++ src/maasserver/models/tests/test_controllerinfo.py
@@ -2,16 +2,10 @@
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 
-from testtools.matchers import Equals, Is
-
 from maasserver.models import ControllerInfo, Notification
 from maasserver.models.controllerinfo import (
-    ControllerVersionInfo,
-    create_or_update_version_notification,
-    KNOWN_VERSION_MISMATCH_NOTIFICATION,
     TargetVersion,
-    UNKNOWN_VERSION_MISMATCH_NOTIFICATION,
-    VERSION_NOTIFICATION_IDENT,
+    UPGRADE_ISSUE_NOTIFICATION_IDENT,
 )
 from maasserver.testing.factory import factory
 from maasserver.testing.testcase import MAASServerTestCase
@@ -31,7 +25,7 @@ class TestControllerInfo(MAASServerTestCase):
     def test_set_version(self):
         controller = factory.make_RackController()
         ControllerInfo.objects.set_version(controller, "2.3.0")
-        self.assertThat(controller.version, Equals("2.3.0"))
+        self.assertEqual(controller.version, "2.3.0")
 
     def test_set_versions_info_snap(self):
         controller = factory.make_RackController()
@@ -396,157 +390,221 @@ class TestControllerInfo(MAASServerTestCase):
         )
 
 
-class TestGetControllerVersionInfo(MAASServerTestCase):
-    def test_sorts_controllerversioninfo_by_most_recent_version_first(self):
-        c1 = factory.make_RegionRackController()
-        ControllerInfo.objects.set_version(c1, "2.3.0")
-        c2 = factory.make_RegionRackController()
-        ControllerInfo.objects.set_version(c2, "2.4.0")
-        c3 = factory.make_RegionRackController()
-        ControllerInfo.objects.set_version(c3, "2.3.5")
-        version_info = ControllerInfo.objects.get_controller_version_info()
-        # Should have returend a list of ControllerVersionInfo objects.
-        for i in range(len(version_info)):
-            self.assertThat(
-                isinstance(version_info[i], ControllerVersionInfo), Is(True)
-            )
-        # The versions should be in descending order.
-        self.assertThat(version_info[0].version, Equals("2.4.0"))
-        self.assertThat(version_info[1].version, Equals("2.3.5"))
-        self.assertThat(version_info[2].version, Equals("2.3.0"))
-
-
-class TestCreateOrUpdateVersionNotification(MAASServerTestCase):
-    def test_create(self):
-        system_id = "xyzzy"
-        create_or_update_version_notification(
-            system_id,
-            "Fix your MAAS, you slacker! It's only version {ver}.",
-            context=dict(ver="1.9"),
-        )
-        expected_notification = Notification.objects.filter(
-            ident=VERSION_NOTIFICATION_IDENT + system_id
-        ).first()
-        self.assertThat(
-            expected_notification.render(),
-            Equals("Fix your MAAS, you slacker! It's only version 1.9."),
-        )
-
-    def test_update(self):
-        system_id = "xyzzy"
-        create_or_update_version_notification(
-            system_id,
-            "Fix your MAAS, you slacker! It's only version {ver}.",
-            context=dict(ver="1.9"),
-        )
-        # The second time should update.
-        create_or_update_version_notification(
-            system_id,
-            "I can't believe you. Still using MAAS {ver}?!",
-            context=dict(ver="2.0"),
-        )
-        expected_notification = Notification.objects.filter(
-            ident=VERSION_NOTIFICATION_IDENT + system_id
-        ).first()
-        self.assertThat(
-            expected_notification.render(),
-            Equals("I can't believe you. Still using MAAS 2.0?!"),
-        )
-
-
-def get_version_notifications():
-    return Notification.objects.filter(
-        ident__startswith=VERSION_NOTIFICATION_IDENT
-    )
-
-
 class TestUpdateVersionNotifications(MAASServerTestCase):
-    def test_single_controller_never_generates_notifications(self):
-        c1 = factory.make_RegionRackController()
-        self.assertThat(get_version_notifications().count(), Equals(0))
-        ControllerInfo.objects.set_version(c1, "2.3.0")
-        self.assertFalse(get_version_notifications().exists())
-
-    def test_out_of_date_controller_generates_concise_notification(self):
+    def test_same_upgrade(self):
         c1 = factory.make_RegionRackController()
         c2 = factory.make_RegionRackController()
-        ControllerInfo.objects.set_version(c1, "2.3.0-500-g1")
-        ControllerInfo.objects.set_version(c2, "2.3.1-500-g1")
-        self.assertThat(get_version_notifications().count(), Equals(1))
-        self.assertThat(
-            get_version_notifications().first().render(),
-            Equals(
-                KNOWN_VERSION_MISMATCH_NOTIFICATION.format(
-                    system_id=c1.system_id, hostname=c1.hostname, v1="2.3.0"
-                )
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            DebVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa"},
+                update={"version": "3.0.1-222-g.bbb"},
             ),
         )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            DebVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa"},
+                update={"version": "3.0.1-222-g.bbb"},
+            ),
+        )
+        self.assertFalse(
+            Notification.objects.filter(
+                ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+            ).exists()
+        )
 
-    def test_version_qualifiers_considered(self):
+    def test_different_versions(self):
         c1 = factory.make_RegionRackController()
         c2 = factory.make_RegionRackController()
-        # Note: the revno and git revision are intentionally identical here,
-        # so we know they don't affect the comparison of qualifiers, and we
-        # know that useless information won't appear in the notification.
-        ControllerInfo.objects.set_version(c1, "2.3.0~alpha1-500-g1")
-        ControllerInfo.objects.set_version(c2, "2.3.0~alpha2-500-g1")
-        self.assertThat(get_version_notifications().count(), Equals(1))
-        self.assertThat(
-            get_version_notifications().first().render(),
-            Equals(
-                KNOWN_VERSION_MISMATCH_NOTIFICATION.format(
-                    system_id=c1.system_id,
-                    hostname=c1.hostname,
-                    v1="2.3.0~alpha1",
-                )
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            DebVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa"},
             ),
         )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            DebVersionsInfo(
+                current={"version": "3.0.1-222-g.bbb"},
+            ),
+        )
+        notification = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers have different versions.",
+            notification.message,
+        )
 
-    def test_assumes_old_controller_if_version_unknown(self):
+    def test_different_upgrades(self):
         c1 = factory.make_RegionRackController()
         c2 = factory.make_RegionRackController()
-        ControllerInfo.objects.set_version(c1, "2.3.0")
-        self.assertThat(get_version_notifications().count(), Equals(1))
-        self.assertThat(
-            get_version_notifications().first().render(),
-            Equals(
-                UNKNOWN_VERSION_MISMATCH_NOTIFICATION.format(
-                    system_id=c2.system_id, hostname=c2.hostname
-                )
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            DebVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa"},
+                update={"version": "3.0.1-222-g.bbb"},
             ),
         )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            DebVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa"},
+                update={"version": "3.0.2-333-g.ccc"},
+            ),
+        )
+        notification = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers report different upgrade versions.",
+            notification.message,
+        )
 
-    def test_revno_differences_cause_full_version_to_be_shown(self):
+    def test_different_install_types(self):
         c1 = factory.make_RegionRackController()
         c2 = factory.make_RegionRackController()
-        ControllerInfo.objects.set_version(c1, "2.3.0~beta2-6000-g.123abc")
-        ControllerInfo.objects.set_version(c2, "2.3.0~beta2-6001-g.234bcd")
-        self.assertThat(get_version_notifications().count(), Equals(1))
-        self.assertThat(
-            get_version_notifications().first().render(),
-            Equals(
-                KNOWN_VERSION_MISMATCH_NOTIFICATION.format(
-                    system_id=c1.system_id,
-                    hostname=c1.hostname,
-                    v1="2.3.0~beta2 (6000-g.123abc)",
-                )
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            DebVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa"},
             ),
         )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+            ),
+        )
+        notification = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers have different installation sources.",
+            notification.message,
+        )
 
-    def test_upgrading_controller_causes_old_notifications_to_go_away(self):
+    def test_different_origins(self):
         c1 = factory.make_RegionRackController()
-        c2 = factory.make_RegionController()
-        ControllerInfo.objects.set_version(c1, "2.3.0~beta2-6000-g123abc")
-        ControllerInfo.objects.set_version(c2, "2.3.0~beta2-6001-g234bcd")
-        self.assertThat(get_version_notifications().count(), Equals(1))
-        ControllerInfo.objects.set_version(c1, "2.3.0~beta2-6001-g234bcd")
-        self.assertThat(get_version_notifications().count(), Equals(0))
+        c2 = factory.make_RegionRackController()
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                channel={"track": "3.0", "risk": "stable"},
+            ),
+        )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                channel={"track": "3.0", "risk": "beta"},
+            ),
+        )
+        notification = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers have different installation sources.",
+            notification.message,
+        )
 
-    def test_deleting_controller_causes_old_notifications_to_go_away(self):
+    def test_different_snap_cohorts(self):
         c1 = factory.make_RegionRackController()
-        c2 = factory.make_RegionController()
-        ControllerInfo.objects.set_version(c1, "2.3.0~beta2-6000-g123abc")
-        ControllerInfo.objects.set_version(c2, "2.3.0~beta2-6001-g234bcd")
-        self.assertThat(get_version_notifications().count(), Equals(1))
-        c2.delete()
-        self.assertThat(get_version_notifications().count(), Equals(0))
+        c2 = factory.make_RegionRackController()
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                cohort="abc",
+            ),
+        )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                cohort="xyz",
+            ),
+        )
+        notification = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers have different installation sources.",
+            notification.message,
+        )
+
+    def test_issue_resolved_removes_notification(self):
+        c1 = factory.make_RegionRackController()
+        c2 = factory.make_RegionRackController()
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                cohort="abc",
+            ),
+        )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+            ),
+        )
+        self.assertTrue(
+            Notification.objects.filter(
+                ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+            ).exists()
+        )
+        # remove the cohort
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+            ),
+        )
+        self.assertFalse(
+            Notification.objects.filter(
+                ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+            ).exists()
+        )
+
+    def test_different_issue_new_notification(self):
+        c1 = factory.make_RegionRackController()
+        c2 = factory.make_RegionRackController()
+        ControllerInfo.objects.set_versions_info(
+            c1,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                channel={"track": "3.0", "risk": "stable"},
+            ),
+        )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            SnapVersionsInfo(
+                current={"version": "3.0.0-111-g.aaa", "revision": "1234"},
+                channel={"track": "3.0", "risk": "beta"},
+            ),
+        )
+        notification1 = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers have different installation sources.",
+            notification1.message,
+        )
+        ControllerInfo.objects.set_versions_info(
+            c2,
+            SnapVersionsInfo(
+                current={"version": "3.0.2-222-g.bbb", "revision": "5678"},
+                channel={"track": "3.0", "risk": "stable"},
+            ),
+        )
+        notification2 = Notification.objects.filter(
+            ident=UPGRADE_ISSUE_NOTIFICATION_IDENT
+        ).first()
+        self.assertIn(
+            "Controllers have different versions.",
+            notification2.message,
+        )
+        self.assertNotEqual(notification1.id, notification2.id)
