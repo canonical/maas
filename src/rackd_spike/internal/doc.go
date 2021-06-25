7982ce088571e29f3fc4b2e2f486988704829f56
Christian Grabowski <christian.grabowski@canonical.com>
2021-06-25 17:00:11 -0400

    provide comments for internal and pkg

---
+++ src/rackd_spike/internal/doc.go
@@ -0,0 +1,3 @@
+package internal
+
+// The internal package provides logic internal to rackd, such as logic for interfacing directly network service or proxies
---
+++ src/rackd_spike/pkg/doc.go
@@ -0,0 +1,3 @@
+package pkg
+
+// The pkg package provides interfaces and defintiions for external communication, such as RPC handlers and clients
