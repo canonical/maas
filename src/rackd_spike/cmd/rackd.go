49fddb9e30f583e9495c6ad9ed9edd2e2b138b54
Christian Grabowski <christian.grabowski@canonical.com>
2021-06-25 16:27:59 -0400

    add Go rackd entrypoint

---
+++ src/rackd_spike/cmd/rackd.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+type opts struct {
+	Euid     bool
+	Version  bool
+	NoDaemon bool
+	NoSave   bool
+	GID      uint32
+	UID      uint32
+	Chroot   string
+	RunDir   string
+	LogFile  string
+	Logger   string
+	PIDFile  string
+	Syslog   string
+}
+
+var rootCMD = &cobra.Command{
+	Use:   "rackd",
+	Short: "rack controller daemon",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		log.Info().Msg("rackd started successfully")
+		sigChan := make(chan os.Signal)
+		signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+		<-sigChan
+		return nil
+	},
+}
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	rootCMD.Execute()
+}
