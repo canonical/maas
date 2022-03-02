7cd029803bd103ed995d2a0a51a34292f9682382
Alberto Donato <alberto.donato@canonical.com>
2022-03-02 16:09:56 +0000

    use hostnames instead of IPs for BMCs.
    This avoids conflicts with unique constraints when running the script multiple times

--- src/maasserver/testing/sampledata_new/vmhost.py
+++ src/maasserver/testing/sampledata_new/vmhost.py
@@ -2,7 +2,7 @@ from itertools import cycle
 
 from maasserver.models import Pod
 
-from .common import range_one
+from .common import make_name
 
 
 def make_vmhosts(count: int):
@@ -10,19 +10,21 @@ def make_vmhosts(count: int):
     power_types = cycle(("virsh", "lxd"))
     vmhosts = []
 
-    for n in range_one(count):
+    for _ in range(count):
         power_type = next(power_types)
-        ip = f"10.10.10.{n}"
+        hostname = make_name()
         if power_type == "virsh":
-            power_parameters = {"power_address": f"qemu+ssh://{ip}/system"}
+            power_parameters = {
+                "power_address": f"qemu+ssh://{hostname}/system"
+            }
         else:
             power_parameters = {
-                "power_address": f"{ip}:8443",
+                "power_address": f"{hostname}:8443",
                 "project": "maas",
             }
         vmhosts.append(
             Pod.objects.create(
-                name=f"{power_type}{n}",
+                name=hostname,
                 power_type=power_type,
                 power_parameters=power_parameters,
             )
