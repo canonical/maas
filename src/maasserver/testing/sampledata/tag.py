20c270a94ea55dbe720b61e4af20f14d0c048295
Alberto Donato <alberto.donato@canonical.com>
2022-03-04 12:14:02 +0000

    create sampledata tags in bulk

--- src/maasserver/testing/sampledata/tag.py
+++ src/maasserver/testing/sampledata/tag.py
@@ -1,9 +1,18 @@
+from datetime import datetime
+from typing import List
+
 from maasserver.models import Tag
 
 from .common import range_one
 
 
-def make_tags(count: int, prefix: str):
-    return [
-        Tag.objects.create(name=f"{prefix}{n:03}") for n in range_one(count)
-    ]
+def make_tags(count: int, prefix: str) -> List[Tag]:
+    now = datetime.utcnow()
+    return Tag.objects.bulk_create(
+        Tag(
+            name=f"{prefix}{n:03}",
+            created=now,
+            updated=now,
+        )
+        for n in range_one(count)
+    )
