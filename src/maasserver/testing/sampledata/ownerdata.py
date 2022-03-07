023bf1dbb4fd2f71a09ce4f317e1463880b2db01
Anton Troyanov <anton.troyanov@canonical.com>
2022-03-07 07:59:44 +0000

    Use single style for batch operations

--- src/maasserver/testing/sampledata/ownerdata.py
+++ src/maasserver/testing/sampledata/ownerdata.py
@@ -7,15 +7,10 @@ from .common import make_name, range_one
 
 
 def make_ownerdata(count: int, prefix: str, machines: List[Machine]):
-    ownerdata = []
     for machine in machines:
         if machine.status != NODE_STATUS.DEPLOYED:
             continue
-        for n in range_one(count):
-            ownerdata.append(
-                OwnerData(
-                    key=f"{prefix}{n:03}", value=make_name(), node=machine
-                )
-            )
-
-    OwnerData.objects.bulk_create(ownerdata)
+        OwnerData.objects.bulk_create(
+            OwnerData(key=f"{prefix}{n:03}", value=make_name(), node=machine)
+            for n in range_one(count)
+        )
