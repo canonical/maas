fb5fa6f5416e33e915e30dfe4e31e406e12e231a
Alberto Donato <alberto.donato@canonical.com>
2023-01-13 13:00:46 +0100

    django-based handler: improve queries, unpack some querysets in parallel

--- src/maasspike/django_light.py
+++ src/maasspike/django_light.py
@@ -1,9 +1,10 @@
 from collections.abc import Iterable
+from itertools import chain
 import logging
 
 from django.contrib.auth.models import User
 from django.contrib.postgres.aggregates import ArrayAgg
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db.models import (
     BigIntegerField,
     BooleanField,
@@ -16,6 +17,7 @@ from django.db.models import (
     Exists,
     FloatField,
     ForeignKey,
+    Func,
     GenericIPAddressField,
     IntegerField,
     Manager,
@@ -56,6 +58,13 @@ from metadataserver.enum import (
     SCRIPT_STATUS_CHOICES,
     SCRIPT_TYPE_CHOICES,
 )
+
+
+def JSONBBuildObject(fields_map):
+    fields = chain(*((Value(key), value) for key, value in fields_map.items()))
+    return Func(
+        *fields, function="jsonb_build_object", output_field=JSONField()
+    )
 
 
 def EnumValues(field, values_map, output_field=None, default=None):
@@ -385,31 +394,65 @@ def testing_status_dict(global_status=None):
 def list_machines(admin, limit=None):
     user_permissions = ["edit", "delete"] if admin.is_superuser else []
 
-    machines = Node.machines.order_by("id")
+    machines_qs = Node.machines.order_by("id")
     if limit is None:
         machine_ids = None
     else:
-        machine_ids = list(machines.values_list("id", flat=True)[:limit])
-        machines = machines.filter(id__in=machine_ids)
+        machine_ids = list(machines_qs.values_list("id", flat=True)[:limit])
+        machines_qs = machines_qs.filter(id__in=machine_ids)
 
-    entries = machines.values(
+    entries = _get_machines_qs(machines_qs, user_permissions)
+    storage_entries = _get_storage_qs(machines_qs)
+    network_entries = _get_network_qs(machines_qs)
+
+    boot_interfaces, interfaces_data = _get_interfaces_data(machine_ids)
+    testing_data = _get_testing_data(machine_ids=machine_ids)
+    vlan_data = _get_vlan_data(boot_interfaces)
+    ipaddress_data = _get_ipaddress_data(
+        boot_interfaces, machine_ids=machine_ids
+    )
+
+    result = []
+    # entries match in both querysets because of ordering by ID and containing
+    # all results
+    all_entries = zip(entries, storage_entries, network_entries)
+    for entry, storage_entry, network_entry in all_entries:
+        node_id = entry["id"]
+        # computed fields
+        entry |= storage_entry | network_entry
+        entry.update(interfaces_data[node_id])
+        entry["vlan"] = vlan_data.get(node_id)
+        entry["ip_addresses"] = ipaddress_data.get(node_id, [])
+
+        testing_statuses = testing_data.get(node_id, {})
+
+        def add_status(key, hardware_type):
+            entry[key] = testing_statuses.get(
+                hardware_type, testing_status_dict(global_status=-1)
+            )
+
+        add_status("cpu_test_status", HARDWARE_TYPE.CPU)
+        add_status("memory_test_status", HARDWARE_TYPE.MEMORY)
+        add_status("network_test_status", HARDWARE_TYPE.NETWORK)
+        add_status("storage_test_status", HARDWARE_TYPE.STORAGE)
+        add_status("testing_status", None)
+        result.append(entry)
+    return result
+
+
+def _get_machines_qs(machines_qs, user_permissions):
+    return machines_qs.values(
         "architecture",
         "cpu_count",
         "description",
         "distro_series",
-        "domain__id",
-        "domain__name",
         "error_description",
         "hostname",
         "id",
         "locked",
         "osystem",
-        "pool__id",
-        "pool__name",
         "power_state",
         "system_id",
-        "zone__id",
-        "zone__name",
     ).annotate(
         owner=Coalesce("owner__username", Value("")),
         parent=F("parent__system_id"),
@@ -429,6 +472,9 @@ def list_machines(admin, limit=None):
         tags=ArrayAgg("tags", distinct=True),
         link_type=Value("machine"),
         power_type=F("bmc__power_type"),
+        zone=JSONBBuildObject({"id": "zone__id", "name": "zone__name"}),
+        pool=JSONBBuildObject({"id": "pool__id", "name": "pool__name"}),
+        domain=JSONBBuildObject({"id": "domain__id", "name": "domain__name"}),
         status_message=Subquery(
             Event.objects.filter(
                 node=OuterRef("pk"), type__level__gte=logging.INFO
@@ -444,59 +490,20 @@ def list_machines(admin, limit=None):
             )
             .values("message")[:1]
         ),
+        permissions=Case(
+            When(
+                locked=True,
+                then=Value([], output_field=ArrayField(TextField())),
+            ),
+            default=Value(
+                user_permissions, output_field=ArrayField(TextField())
+            ),
+        ),
     )
 
-    storage_data = _get_storage_data(machine_ids=machine_ids)
-    boot_interfaces, interfaces_data = _get_interfaces_data(machine_ids)
-    networks_data = _get_networks_data(machine_ids=machine_ids)
-    testing_data = _get_testing_data(machine_ids=machine_ids)
-    vlan_data = _get_vlan_data(boot_interfaces)
-    ipaddress_data = _get_ipaddress_data(
-        boot_interfaces, machine_ids=machine_ids
-    )
 
-    result = []
-    for entry in entries:
-        node_id = entry["id"]
-        # extend existing dict from storage data, as it's not used elsewhere
-        new_entry = storage_data[node_id]
-
-        sub_entries = {}
-        for key, value in entry.items():
-            if "__" in key:
-                item, subkey = key.split("__")
-                sub_entries.setdefault(item, {})[subkey] = value
-            else:
-                new_entry[key] = value
-        new_entry.update(sub_entries)
-
-        # computed fields
-        new_entry.update(networks_data[node_id])
-        new_entry.update(interfaces_data[node_id])
-        new_entry["permissions"] = (
-            [] if new_entry["locked"] else user_permissions
-        )
-        new_entry["vlan"] = vlan_data.get(node_id)
-        new_entry["ip_addresses"] = ipaddress_data.get(node_id, [])
-
-        testing_statuses = testing_data.get(node_id, {})
-
-        def add_status(key, hardware_type):
-            new_entry[key] = testing_statuses.get(
-                hardware_type, testing_status_dict(global_status=-1)
-            )
-
-        add_status("cpu_test_status", HARDWARE_TYPE.CPU)
-        add_status("memory_test_status", HARDWARE_TYPE.MEMORY)
-        add_status("network_test_status", HARDWARE_TYPE.NETWORK)
-        add_status("storage_test_status", HARDWARE_TYPE.STORAGE)
-        add_status("testing_status", None)
-        result.append(new_entry)
-    return result
-
-
-def _get_storage_data(machine_ids):
-    entries = Node.machines.values_list("id").annotate(
+def _get_storage_qs(machines):
+    return machines.annotate(
         physical_disk_count=Count(
             "current_config__blockdevice__physicalblockdevice",
         ),
@@ -507,16 +514,26 @@ def _get_storage_data(machine_ids):
             ),
         )
         / Value(1000**3),
-    )
-    if machine_ids is not None:
-        entries = entries.filter(id__in=machine_ids)
-    return {
-        node_id: {
-            "physical_disk_count": physical_disk_count,
-            "storage": storage,
-        }
-        for node_id, physical_disk_count, storage in entries
-    }
+    ).values("physical_disk_count", "storage")
+
+
+def _get_network_qs(machines):
+    return machines.annotate(
+        fabrics=ArrayAgg(
+            "current_config__interface__vlan__fabric__name",
+            filter=Q(
+                current_config__interface__vlan__fabric__name__isnull=False
+            ),
+            distinct=True,
+        ),
+        spaces=ArrayAgg(
+            "current_config__interface__vlan__space__name",
+            filter=Q(
+                current_config__interface__vlan__space__name__isnull=False
+            ),
+            distinct=True,
+        ),
+    ).values("fabrics", "spaces")
 
 
 def _get_interfaces_data(machine_ids=None):
@@ -563,31 +580,6 @@ def _get_interfaces_data(machine_ids=None):
             ],
         }
     return boot_interfaces, interfaces_data
-
-
-def _get_networks_data(machine_ids=None):
-    network_entries = Node.machines.values_list("id").annotate(
-        fabrics=ArrayAgg(
-            "current_config__interface__vlan__fabric__name",
-            filter=Q(
-                current_config__interface__vlan__fabric__name__isnull=False
-            ),
-            distinct=True,
-        ),
-        spaces=ArrayAgg(
-            "current_config__interface__vlan__space__name",
-            filter=Q(
-                current_config__interface__vlan__space__name__isnull=False
-            ),
-            distinct=True,
-        ),
-    )
-    if machine_ids is not None:
-        network_entries = network_entries.filter(id__in=machine_ids)
-    return {
-        node_id: {"fabrics": fabrics, "spaces": spaces}
-        for node_id, fabrics, spaces in network_entries
-    }
 
 
 ORDERING_STATUS_MAP = {
