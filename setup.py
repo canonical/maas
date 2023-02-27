6972e1c4025beb1650cb52074abbfe6003fcd68f
Alberto Donato <alberto.donato@canonical.com>
2023-02-27 12:07:26 +0000

    run setup-py-upgrade and setup-cfg-fmt in format phase
    They ensure linting on setup.cfg and migrate anything that might be added to
    setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# Copyright 2012-2021 Canonical Ltd.  This software is licensed under the
-# GNU Affero General Public License version 3 (see the file LICENSE).
-
 from setuptools import setup
 
 setup()
