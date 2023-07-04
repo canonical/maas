3976865b04cfe4b710eb7d38f0e269b5ae584cae
Alberto Donato <alberto.donato@canonical.com>
2023-07-04 08:04:59 +0000

    add apiserver client

---
+++ src/maasapiserver/client.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from urllib.parse import quote_plus
+
+from requests.models import Response
+from requests.utils import add_dict_to_cookiejar
+from requests_unixsocket import Session
+
+from .settings import api_service_socket_path
+
+
+class APIServerClient:
+    def __init__(
+        self,
+        session_id: str,
+        session: Optional[Session] = None,
+        version: int = 1,
+    ):
+        self.session = session or Session()
+        add_dict_to_cookiejar(self.session.cookies, {"session_id": session_id})
+        path = str(api_service_socket_path())
+        self.socket_path = f"http+unix://{quote_plus(path)}"
+        self.prefix = f"/api/v{version}/"
+
+    def request(self, verb: str, path: str, **kwargs) -> Response:
+        url = self.socket_path + self.prefix + path.lstrip("/")
+        return self.session.request(verb, url, **kwargs)
+
+    def get(self, path: str, **kwargs) -> Response:
+        return self.request("GET", path, **kwargs)
+
+    def post(self, path: str, **kwargs) -> Response:
+        return self.request("POST", path, **kwargs)
+
+    def put(self, path: str, **kwargs) -> Response:
+        return self.request("PUT", path, **kwargs)
+
+    def delete(self, path: str, **kwargs) -> Response:
+        return self.request("DELETE", path, **kwargs)
---
+++ src/tests/maasapiserver/test_client.py
@@ -0,0 +1,65 @@
+import pytest
+from requests.utils import dict_from_cookiejar
+
+from maasapiserver.client import APIServerClient
+
+SAMPLE_SESSION_ID = "abcdef"
+
+
+@pytest.fixture
+def client(mocker):
+    client = APIServerClient(SAMPLE_SESSION_ID)
+    mocker.patch.object(client.session, "request")
+    # override the socket path to a known value
+    client.socket_path = "http+unix://socket"
+    yield client
+
+
+class TestAPIServerClient:
+    def test_session_cookie(self, client):
+        assert dict_from_cookiejar(client.session.cookies) == {
+            "session_id": SAMPLE_SESSION_ID
+        }
+
+    @pytest.mark.parametrize(
+        "path,expected",
+        [
+            ("endpoint", "http+unix://socket/api/v1/endpoint"),
+            ("/endpoint", "http+unix://socket/api/v1/endpoint"),
+            ("/some/path", "http+unix://socket/api/v1/some/path"),
+        ],
+    )
+    def test_request(self, client, path, expected):
+        client.request("GET", path)
+        client.session.request.assert_called_with("GET", expected)
+
+    def test_get(self, client):
+        client.get("endpoint")
+        client.session.request.assert_called_with(
+            "GET", "http+unix://socket/api/v1/endpoint"
+        )
+
+    def test_post(self, client):
+        kwargs = {
+            "json": {"key": "value"},
+        }
+        client.post("endpoint", **kwargs)
+        client.session.request.assert_called_with(
+            "POST", "http+unix://socket/api/v1/endpoint", **kwargs
+        )
+
+    def test_put(self, client):
+        kwargs = {
+            "json": {"key": "value"},
+        }
+        client.put("endpoint", **kwargs)
+        client.session.request.assert_called_with(
+            "PUT", "http+unix://socket/api/v1/endpoint", **kwargs
+        )
+
+    def test_delete(self, client):
+        client.delete("endpoint")
+        client.session.request.assert_called_with(
+            "DELETE",
+            "http+unix://socket/api/v1/endpoint",
+        )
