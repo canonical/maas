35a41e9f89cb3b4ee1a706e7451c728ae138395c
Bjorn Tillenius <bjorn@canonical.com>
2023-07-05 09:48:34 +0000

    Add a method of getting a SQLAlchemy connection based on the Django one.
    This can be used in order to make use of SQLAlchemy core within
    the Django app, so that logic can be shared between maasapiserver
    and maasserver.

---
+++ src/maasserver/sqlalchemy.py
@@ -0,0 +1,104 @@
+"""Make SQLALchemy available from within the Django app.
+
+See the docstring for get_sqlalchemy_django_connection() for more info.
+"""
+
+from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
+from sqlalchemy.engine.base import Connection, Engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.pool import PoolProxiedConnection
+from sqlalchemy.pool.base import Pool
+
+
+def get_sqlalchemy_django_connection():
+    """Get a SQLAlchemy connection sharing the Django connection.
+
+    This returns the same kind of connection you would get if you called
+    Engine.connect().
+
+    The underlying dbapi connection is the same as the one that Django uses.
+
+    With this you can make mix SQLAlchemy core with Django. The transaction is
+    shared across both SQLAlchemy and Django.
+
+    Django is in charge of the transaction and connection handling. When using
+    SQLAlchemy core together with Django, SQLAlchemy should be used only to
+    construct and make SQL queries. Starting and committing transactions should
+    be done using the Django API.
+
+    For example:
+
+        from django.db import transaction
+
+        conn = get_sqlalchemy_django_connection()
+        with transaction.atomic():
+            conn.execute(...)
+    """
+    from django.conf import settings
+    from django.db import connection as django_connection
+
+    django_db = settings.DATABASES[django_connection.alias]
+    assert (
+        django_db["ENGINE"] == "django.db.backends.postgresql_psycopg2"
+    ), f"{django_db['ENGINE']} is not supported"
+
+    pool = SharedDjangoPool()
+    engine = Engine(
+        pool=pool,
+        dialect=PGDialect_psycopg2(dbapi=PGDialect_psycopg2.import_dbapi()),
+        url=URL.create("postgresql+psycopg2"),
+    )
+    conn = SharedConnection(
+        engine=engine,
+        connection=SharedDBAPIConnection(django_connection.connection),
+        _allow_autobegin=False,
+    )
+    return conn
+
+
+class SharedConnection(Connection):
+    """A connection that shares the dbapi connection with another framework.
+
+    The other framework, like Django, is expected to handle the transactions,
+    as well as the state of the connection.
+    """
+
+    def invalidate(self, exception=None):
+        raise NotImplementedError("Underlying connection is handled by Django")
+
+    def begin(self):
+        raise NotImplementedError("Transactions are handled by Django")
+
+    def begin_nested(self):
+        raise NotImplementedError("Transactions are handled by Django")
+
+    def begin_twophase(self, xid=None):
+        raise NotImplementedError("Transactions are handled by Django")
+
+    def commit(self):
+        raise NotImplementedError("Transactions are handled by Django")
+
+    def rollback(self):
+        raise NotImplementedError("Transactions are handled by Django")
+
+
+class SharedDBAPIConnection(PoolProxiedConnection):
+    def __init__(self, dbapi_connection):
+        self.dbapi_connection = dbapi_connection
+
+    @property
+    def driver_connection(self):
+        return self.dbapi_connection
+
+    def cursor(self):
+        return self.dbapi_connection.cursor()
+
+
+class SharedDjangoPool(Pool):
+    """A custom pool that ensures that no connection attempts are being made."""
+
+    def __init__(self):
+        pass
+
+    def connect(self) -> PoolProxiedConnection:
+        raise NotImplementedError("Underlying connection is handled by Django")
---
+++ src/tests/maasserver/test_sqlalchemy.py
@@ -0,0 +1,45 @@
+from django.db import connection as django_connection
+import pytest
+from sqlalchemy import select
+from sqlalchemy.sql.expression import func
+
+from maasserver.sqlalchemy import get_sqlalchemy_django_connection
+
+
+def test_same_transaction(maasdb):
+    conn = get_sqlalchemy_django_connection()
+    stmt = select(func.txid_current())
+    sqlalchemy_txid = conn.execute(stmt).scalar()
+    with django_connection.cursor() as cursor:
+        cursor.execute("SELECT txid_current()")
+        rows = cursor.fetchall()
+        django_txid = rows[0][0]
+    assert sqlalchemy_txid == django_txid
+
+
+def test_no_transaction_handling(maasdb):
+    conn = get_sqlalchemy_django_connection()
+    with pytest.raises(NotImplementedError):
+        conn.begin()
+    with pytest.raises(NotImplementedError):
+        conn.commit()
+    with pytest.raises(NotImplementedError):
+        conn.rollback()
+    with pytest.raises(NotImplementedError):
+        conn.begin_twophase()
+    with pytest.raises(NotImplementedError):
+        conn.begin_nested()
+
+
+def test_no_connection_handling(maasdb):
+    conn = get_sqlalchemy_django_connection()
+    with pytest.raises(NotImplementedError):
+        conn.close()
+    with pytest.raises(NotImplementedError):
+        conn.invalidate()
+
+
+def test_no_pool_connect(maasdb):
+    conn = get_sqlalchemy_django_connection()
+    with pytest.raises(NotImplementedError):
+        conn.engine.pool.connect()
