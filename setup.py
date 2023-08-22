76e9dd1e67b07392c060ffca90beb699defff7b2
Mauricio Faria de Oliveira <mfo@canonical.com>
2023-08-22 17:59:08 -0300

    Release 2.9.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name="maas",
-    version="2.9.3rc3",
+    version="2.9.3",
     url="https://maas.io/",
     license="AGPLv3",
     description="Metal As A Service",
