64f3902528bd43092af2ca09d0d70e6b7a22bc19
Alexsander Silva de Souza <alexsander.souza@canonical.com>
2023-08-28 12:40:10 +0000

    improve API performance tests

--- src/maasperf/tests/maasserver/api/test_machines.py
+++ src/maasperf/tests/maasserver/api/test_machines.py
@@ -2,20 +2,44 @@
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 from django.urls import reverse
+from piston3.emitters import Emitter
+from piston3.handler import typemapper
 
 from maasserver.api.machines import MachinesHandler
 from maastesting.http import make_HttpRequest
 from maastesting.perftest import perf_test
 
 
-@perf_test()
-def test_perf_list_machines_MachineHandler_api_endpoint(api_client):
-    api_client.get(reverse("machines_handler"))
+class DummyEmitter(Emitter):
+    def render(self, request):
+        self.construct()
 
 
 @perf_test(db_only=True)
-def test_perf_list_machines_MachinesHander_direct_call(maas_user):
+def test_perf_list_machines_MachineHandler_api_endpoint(admin_api_client):
+    admin_api_client.get(reverse("machines_handler"))
+
+
+@perf_test(db_only=True)
+def test_perf_list_machines_MachinesHander_direct_call(admin):
     handler = MachinesHandler()
     request = make_HttpRequest()
-    request.user = maas_user
-    handler.read(request)
+    request.user = admin
+
+    emitter = DummyEmitter(
+        handler.read(request),
+        typemapper,
+        handler,
+        handler.fields,
+        anonymous=False,
+    )
+    emitter.render(request)
+
+
+@perf_test(db_only=True)
+def test_perf_list_machines_MachinesHander_only_objects(admin):
+    handler = MachinesHandler()
+    request = make_HttpRequest()
+    request.user = admin
+
+    list(handler.read(request))
