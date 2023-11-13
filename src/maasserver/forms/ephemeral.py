77a57187b8558d3de0374e23d9889c70f08c473b
Alberto Donato <alberto.donato@canonical.com>
2023-11-13 08:56:57 +0000

    refactor forms that deal with scripts to extract common logic

--- src/maasserver/forms/ephemeral.py
+++ src/maasserver/forms/ephemeral.py
@@ -25,45 +25,108 @@ from maasserver.utils.forms import set_form_error
 from metadataserver.enum import SCRIPT_TYPE
 
 
-class TestForm(Form):
-    """Test form."""
+class ScriptForm(Form):
+    name = "script"
+    display = "Script"
+    # tuples of supported parameters names for scripts, with their script type
+    # and whether "none" can be passed as parameter
+    script_fields_details: tuple[tuple[str, int, bool]] = ()
 
-    enable_ssh = BooleanField(required=False, initial=False)
+    _scripts: list[Script]
 
-    def _get_storage_choices(self):
-        choices = [("all", "all")]
-        for bd in self.instance.physicalblockdevice_set.all():
-            choices.append((bd.id, bd.id))
-            choices.append((bd.name, bd.name))
-            choices.append((bd.model, bd.model))
-            choices.append((bd.serial, bd.serial))
-            model_serial = f"{bd.model}:{bd.serial}"
-            choices.append((model_serial, model_serial))
-            for tag in bd.tags:
-                choices.append((tag, tag))
-        return choices
+    def __init__(self, instance, user, data=None, **kwargs):
+        # ensure the data field is a dict, since QueryDict is immutable
+        data = data.copy() if data else {}
+        super().__init__(data=data, **kwargs)
+        self.instance = instance
+        self.user = user
+        self._scripts = list(
+            Script.objects.filter(
+                script_type__in=[
+                    entry[1] for entry in self.script_fields_details
+                ]
+            )
+        )
+        self._set_up_script_fields()
+        self._set_up_parameter_fields()
 
-    def _get_interface_choices(self):
-        choices = [("all", "all")]
-        for interface in self.instance.current_config.interface_set.filter(
-            children_relationships=None
-        ):
-            choices.append((interface.id, interface.id))
-            choices.append((interface.name, interface.name))
-            mac = str(interface.mac_address)
-            choices.append((mac, mac))
-            choices.append((interface.vendor, interface.vendor))
-            choices.append((interface.product, interface.product))
-            vendor_product = f"{interface.vendor}:{interface.product}"
-            choices.append((vendor_product, vendor_product))
-            for tag in interface.tags:
-                choices.append((tag, tag))
-        return choices
+    def clean(self):
+        action = get_node_action(self.instance, self.name, self.user)
+        if action is None:
+            raise ValidationError(
+                f"{self.display} is not available because "
+                "of the current state of the node."
+            )
+        return super().clean()
 
-    def _set_up_parameter_fields(self, scripts):
+    def _clean_fields(self):
+        # before validating fields, split values for the list of scripts
+        for param, script_type, _ in self.script_fields_details:
+            scripts = self.data.get(param)
+            if scripts is not None and isinstance(scripts, str):
+                if isinstance(self.data, QueryDict):
+                    # QueryDict must have its value updated using setlist otherwise
+                    # it puts the set value inside a list.
+                    self.data.setlist(param, scripts.split(","))
+                else:
+                    self.data[param] = scripts.split(",")
+        return super()._clean_fields()
+
+    def get_script_param_dict(self, names):
+        params = {}
+        scripts = list(
+            Script.objects.filter(Q(name__in=names) | Q(tags__overlap=names))
+        )
+        for name, value in self.cleaned_data.items():
+            if not value:
+                continue
+            # Check if the parameter is for a particular script
+            script_param = False
+            for script in scripts:
+                if script.name in name:
+                    if script.name not in params:
+                        params[script.name] = {}
+                    param = name.replace(script.name, "").strip("_-")
+                    if isinstance(value, timedelta):
+                        params[script.name][param] = int(value.total_seconds())
+                    else:
+                        params[script.name][param] = value
+                    script_param = True
+            # If its not for a particular script pass the parameter to all
+            # scripts that support it.
+            if not script_param:
+                for script in scripts:
+                    if name in script.parameters and name not in params.get(
+                        script.name, {}
+                    ):
+                        if script.name not in params:
+                            params[script.name] = {}
+                        if isinstance(value, timedelta):
+                            params[script.name][name] = int(
+                                value.total_seconds()
+                            )
+                        else:
+                            params[script.name][name] = value
+        return params
+
+    def _set_up_script_fields(self):
+        for param, script_type, allow_none in self.script_fields_details:
+            choices = []
+            if allow_none:
+                choices.append(("none", "none"))
+            for script in self._scripts:
+                if script.script_type == script_type:
+                    choices.append((script.name, script.name))
+                    choices.extend((tag, tag) for tag in script.tags)
+
+            self.fields[param] = MultipleChoiceField(
+                required=False, initial=None, choices=choices
+            )
+
+    def _set_up_parameter_fields(self):
         storage_choices = None
         interface_choices = None
-        for script in scripts:
+        for script in self._scripts:
             for pname, value in script.parameters.items():
                 ptype = value.get("type")
                 combined_name = f"{script.name}_{pname}"
@@ -119,9 +182,7 @@ class TestForm(Form):
                     # Support a Django choice list or a string list
                     choices = [
                         set(choice)
-                        if isinstance(choice, list)
-                        or isinstance(choice, set)
-                        or isinstance(choice, tuple)
+                        if isinstance(choice, (list, set, tuple))
                         else (choice, choice)
                         for choice in value.get("choices", [])
                     ]
@@ -133,48 +194,51 @@ class TestForm(Form):
                             required=False, initial=None, choices=choices
                         )
 
-    def _set_up_script_fields(self):
-        testing_scripts = self.data.get("testing_scripts")
-        if testing_scripts is not None and isinstance(testing_scripts, str):
-            if isinstance(self.data, QueryDict):
-                # QueryDict must have its value updated using setlist otherwise
-                # it puts the set value inside a list.
-                self.data.setlist(
-                    "testing_scripts", testing_scripts.split(",")
+    def _get_storage_choices(self):
+        choices = [("all", "all")]
+        for bd in self.instance.physicalblockdevice_set.all():
+            model_serial = f"{bd.model}:{bd.serial}"
+            choices.extend(
+                (
+                    (bd.id, bd.id),
+                    (bd.name, bd.name),
+                    (bd.model, bd.model),
+                    (bd.serial, bd.serial),
+                    (model_serial, model_serial),
                 )
-            else:
-                self.data["testing_scripts"] = testing_scripts.split(",")
-
-        choices = []
-        scripts = Script.objects.filter(script_type=SCRIPT_TYPE.TESTING)
-        for script in scripts:
-            if script.script_type == SCRIPT_TYPE.TESTING:
-                choices.append((script.name, script.name))
-                for tag in script.tags:
-                    choices.append((tag, tag))
-
-        self.fields["testing_scripts"] = MultipleChoiceField(
-            required=False, initial=None, choices=choices
-        )
-        self._set_up_parameter_fields(scripts)
-
-    def __init__(self, instance, user, data=None, **kwargs):
-        data = {} if data is None else data.copy()
-        super().__init__(data=data, **kwargs)
-        self.instance = instance
-        self.user = user
-        self._name = "test"
-        self._display = "Test"
-        self._set_up_script_fields()
-
-    def clean(self):
-        action = get_node_action(self.instance, self._name, self.user)
-        if action is None:
-            raise ValidationError(
-                "%s is not available because of the current state of the node."
-                % self._display
             )
-        return super().clean()
+            choices.extend((tag, tag) for tag in bd.tags)
+        return choices
+
+    def _get_interface_choices(self):
+        choices = [("all", "all")]
+        for interface in self.instance.current_config.interface_set.filter(
+            children_relationships=None
+        ):
+            mac = str(interface.mac_address)
+            vendor_product = f"{interface.vendor}:{interface.product}"
+            choices.extend(
+                (
+                    (interface.id, interface.id),
+                    (interface.name, interface.name),
+                    (mac, mac),
+                    (interface.vendor, interface.vendor),
+                    (interface.product, interface.product),
+                    (vendor_product, vendor_product),
+                )
+            )
+            choices.extend((tag, tag) for tag in interface.tags)
+        return choices
+
+
+class TestForm(ScriptForm):
+    """Test form."""
+
+    name = "test"
+    display = "Test"
+    script_fields_details = (("testing_scripts", SCRIPT_TYPE.TESTING, False),)
+
+    enable_ssh = BooleanField(required=False, initial=False)
 
     def is_valid(self):
         valid = super().is_valid()
@@ -198,67 +262,15 @@ class TestForm(Form):
                 valid = False
         return valid
 
-    def _get_script_param_dict(self, scripts):
-        params = {}
-        script_names = []
-        ids = []
-        for script in scripts:
-            if script.isdigit():
-                ids.append(int(script))
-            else:
-                script_names.append(script)
-        qs = Script.objects.filter(
-            Q(name__in=scripts) | Q(tags__overlap=scripts) | Q(id__in=ids)
-        )
-        for name, value in self.cleaned_data.items():
-            if name in [
-                "enable_ssh",
-                "testing_scripts",
-                "commissioning_scripts",
-                "skip_bmc_config",
-                "skip_networking",
-                "skip_storage",
-            ]:
-                continue
-            if not value:
-                continue
-            # Check if the parameter is for a particular script
-            script_param = False
-            for script in qs:
-                if script.name in name:
-                    if script.name not in params:
-                        params[script.name] = {}
-                    param = name.replace(script.name, "").strip("_-")
-                    if isinstance(value, timedelta):
-                        params[script.name][param] = int(value.total_seconds())
-                    else:
-                        params[script.name][param] = value
-                    script_param = True
-            # If its not for a particular script pass the parameter to all
-            # scripts that support it.
-            if not script_param:
-                for script in qs:
-                    if name in script.parameters and name not in params.get(
-                        script.name, {}
-                    ):
-                        if script.name not in params:
-                            params[script.name] = {}
-                        if isinstance(value, timedelta):
-                            params[script.name][name] = int(
-                                value.total_seconds()
-                            )
-                        else:
-                            params[script.name][name] = value
-        return params
-
     def save(self):
         enable_ssh = self.cleaned_data.get("enable_ssh", False)
         testing_scripts = self.cleaned_data.get("testing_scripts")
+        params = self.get_script_param_dict(testing_scripts)
         self.instance.start_testing(
             self.user,
             enable_ssh,
             testing_scripts,
-            self._get_script_param_dict(testing_scripts),
+            params,
         )
         return self.instance
 
@@ -266,48 +278,16 @@ class TestForm(Form):
 class CommissionForm(TestForm):
     """Commission form."""
 
+    name = "commission"
+    display = "Commission"
+    script_fields_details = (
+        ("commissioning_scripts", SCRIPT_TYPE.COMMISSIONING, False),
+        ("testing_scripts", SCRIPT_TYPE.TESTING, True),
+    )
+
     skip_bmc_config = BooleanField(required=False, initial=False)
     skip_networking = BooleanField(required=False, initial=False)
     skip_storage = BooleanField(required=False, initial=False)
-
-    def _set_up_script_fields(self):
-        # If a string was given convert into a list for validation and use.
-        for script_field in ["commissioning_scripts", "testing_scripts"]:
-            value = self.data.get(script_field)
-            if value is not None and isinstance(value, str):
-                if isinstance(self.data, QueryDict):
-                    # QueryDict must have its value updated using setlist
-                    # otherwise it puts the set value inside a list.
-                    self.data.setlist(script_field, value.split(","))
-                else:
-                    self.data[script_field] = value.split(",")
-
-        choices = {
-            SCRIPT_TYPE.COMMISSIONING: [("none", "none")],
-            SCRIPT_TYPE.TESTING: [("none", "none")],
-        }
-        scripts = Script.objects.all()
-        for script in scripts:
-            for script_type, script_choices in choices.items():
-                if script.script_type == script_type:
-                    script_choices.append((script.name, script.name))
-                    for tag in script.tags:
-                        script_choices.append((tag, tag))
-
-        self.fields["commissioning_scripts"] = MultipleChoiceField(
-            required=False,
-            initial=None,
-            choices=choices[SCRIPT_TYPE.COMMISSIONING],
-        )
-        self.fields["testing_scripts"] = MultipleChoiceField(
-            required=False, initial=None, choices=choices[SCRIPT_TYPE.TESTING]
-        )
-        self._set_up_parameter_fields(scripts)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._name = "commission"
-        self._display = "Commission"
 
     def save(self):
         enable_ssh = self.cleaned_data.get("enable_ssh", False)
@@ -316,6 +296,9 @@ class CommissionForm(TestForm):
         skip_storage = self.cleaned_data.get("skip_storage", False)
         commissioning_scripts = self.cleaned_data.get("commissioning_scripts")
         testing_scripts = self.cleaned_data.get("testing_scripts")
+        params = self.get_script_param_dict(
+            commissioning_scripts + testing_scripts
+        )
         self.instance.start_commissioning(
             self.user,
             enable_ssh,
@@ -324,8 +307,6 @@ class CommissionForm(TestForm):
             skip_storage,
             commissioning_scripts,
             testing_scripts,
-            self._get_script_param_dict(
-                commissioning_scripts + testing_scripts
-            ),
+            params,
         )
         return self.instance
