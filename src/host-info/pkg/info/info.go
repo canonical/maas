2da37db101be6b052dc2eb50b2a029aeec4a0e28
Anton Troyanov <anton.troyanov@canonical.com>
2024-04-05 21:08:57 +0000

    chore: lint copyright header in Go files

--- src/host-info/pkg/info/info.go
+++ src/host-info/pkg/info/info.go
@@ -1,8 +1,20 @@
+// Copyright (c) 2022-2024 Canonical Ltd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //go:build linux
 // +build linux
-
-// Copyright 2022 Canonical Ltd.  This software is licensed under the
-// GNU Affero General Public License version 3 (see the file LICENSE).
 
 //nolint:stylecheck // ignore ST1000
 package info
--- src/maasagent/internal/ethernet/arp_test.go
+++ src/maasagent/internal/ethernet/arp_test.go
@@ -1,9 +1,19 @@
-package ethernet
+// Copyright (c) 2023-2024 Canonical Ltd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/*
-	Copyright 2023 Canonical Ltd.  This software is licensed under the
-	GNU Affero General Public License version 3 (see the file LICENSE).
-*/
+package ethernet
 
 import (
 	"io"
