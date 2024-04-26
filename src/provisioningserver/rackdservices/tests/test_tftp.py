canonical/maas

commit a0b47aba48b2ad324c34b42aed9f47a7dfc85b28

fix: tftp paths are wrong
TFTP handler will make a proxy call over HTTP to fetch bootloader and
related files. We should not return anything if this is not HTTP 200 OK.

Resolves LP:2057767

(cherry picked from commit 61a53d4c8b98ee63c61900f4dc3cb278b29e3226)

src/provisioningserver/rackdservices/tftp.py
@@ -314,6 +314,8 @@ class TFTPBackend(FilesystemSynchronousBackend):
             [b"http://localhost:5248/images", file_name.strip(b"/")]
         )
         resp = yield self._cache_proxy.request(b"GET", url)
+        if resp.code != 200:
+            return BytesReader(bytes())
         body = yield readBody(resp)
         return BytesReader(body)
 

src/provisioningserver/rackdservices/tests/test_tftp.py
@@ -663,6 +663,27 @@ class TestTFTPBackend(MAASTestCase):
             f"http://localhost:5248/images/grub/{filename}".encode("utf-8"),
         )
 
+    @inlineCallbacks
+    def test_get_cache_reader_404(self):
+        params_okay = {
+            name.decode("ascii"): factory.make_name("value")
+            for name, _ in GetBootConfig.arguments
+        }
+        client = Mock()
+        client.localIdent = params_okay["system_id"]
+        client_service = Mock()
+        client_service.getClientNow.return_value = succeed(client)
+        backend = TFTPBackend(self.make_dir(), client_service)
+        backend._cache_proxy = Mock()
+        filename = factory.make_name()
+
+        mock_result = Mock()
+        mock_result.code = 404
+
+        backend._cache_proxy.request.return_value = mock_result
+        result = yield backend.get_cache_reader(f"/grub/{filename}")
+        self.assertEqual(result.read(0), b"")
+
 
 class TestTFTPService(MAASTestCase):
     def test_tftp_service(self):

commit 915882cfd7786a68d10a5cf461a29d7690912b80

fix(tftp): legacy BIOS mode fails with 0-size cfg
The fix for GRUB introduced in a0b47aba48b2ad324c34b42aed9f47a7dfc85b28
lead to an issue with legacy BIOS mode, which is expecting to receive
`Error code 1: File not found` for non existing pxelinux.cfg config
files instead for a 0-size result.

Resolves LP:2063844

src/provisioningserver/rackdservices/tftp.py
@@ -315,6 +315,16 @@ class TFTPBackend(FilesystemSynchronousBackend):
         )
         resp = yield self._cache_proxy.request(b"GET", url)
         if resp.code != 200:
+            # legacy BIOS mode is expecting to get `TFTP file not found error`
+            # for any `pxelinux.cfg/` that do not exist.
+            if str(file_name, encoding="utf-8").startswith("pxelinux.cfg"):
+                raise FileNotFound(file_name)
+            # However for anything else (at least for GRUB) things like
+            # - command.lst
+            # - fs.lst
+            # - crypto.lst
+            # - terminal.lst
+            # Are expected to be 0-sized with no error.
             return BytesReader(bytes())
         body = yield readBody(resp)
         return BytesReader(body)

src/provisioningserver/rackdservices/tests/test_tftp.py
@@ -684,6 +684,23 @@ class TestTFTPBackend(MAASTestCase):
         result = yield backend.get_cache_reader(f"/grub/{filename}")
         self.assertEqual(result.read(0), b"")
 
+    @inlineCallbacks
+    def test_get_cache_reader_file_not_found_for_pxelinuxcfg(self):
+        params_okay = {
+            name.decode("ascii"): factory.make_name("value")
+            for name, _ in GetBootConfig.arguments
+        }
+        client = Mock()
+        client.localIdent = params_okay["system_id"]
+        client_service = Mock()
+        client_service.getClientNow.return_value = succeed(client)
+        backend = TFTPBackend(self.make_dir(), client_service)
+        backend._cache_proxy = Mock()
+        filename = "pxelinux.cfg/0D0001BE"
+
+        with self.assertRaisesRegex(FileNotFound, rf"{filename}"):
+            yield backend.get_cache_reader(f"{filename}")
+
 
 class TestTFTPService(MAASTestCase):
     def test_tftp_service(self):