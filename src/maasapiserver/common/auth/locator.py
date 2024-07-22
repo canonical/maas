056d72c8669da776f577db642b24aec367342092
Jacopo Rota <jacopo.rota@canonical.com>
2024-07-22 08:22:13 +0000

    feat(maasapiserver): add v3 bakery macaroon third party locator.

---
+++ src/maasapiserver/common/auth/locator.py
@@ -0,0 +1,72 @@
+#  Copyright 2024 Canonical Ltd.  This software is licensed under the
+#  GNU Affero General Public License version 3 (see the file LICENSE).
+
+from urllib.parse import urlparse
+
+from aiohttp import ClientSession, CookieJar
+from macaroonbakery import bakery
+from macaroonbakery.httpbakery import BAKERY_PROTOCOL_HEADER, ThirdPartyLocator
+
+
+class AsyncThirdPartyLocator(ThirdPartyLocator):
+    """Implements macaroonbakery.ThirdPartyLocator by first looking in the
+    backing cache and, if that fails, making an HTTP request to find the
+    information associated with the given discharge location.
+
+    This is the async porting of ThirdPartyLocator with no extra logic.
+    """
+
+    BAKERY_HEADERS = {BAKERY_PROTOCOL_HEADER: str(bakery.LATEST_VERSION)}
+
+    def __init__(self, allow_insecure=False):
+        """
+        @param url: the url to retrieve public_key
+        @param allow_insecure: By default it refuses to use insecure URLs.
+        """
+        super().__init__(allow_insecure=allow_insecure)
+        self._session = ClientSession(
+            headers=self.BAKERY_HEADERS,
+            trust_env=True,
+            cookie_jar=CookieJar(unsafe=True),
+        )
+
+    async def third_party_info(self, loc):
+        u = urlparse(loc)
+        if u.scheme != "https" and not self._allow_insecure:
+            raise bakery.ThirdPartyInfoNotFound(
+                "untrusted discharge URL {}".format(loc)
+            )
+        loc = loc.rstrip("/")
+        info = self._cache.get(loc)
+        if info is not None:
+            return info
+        url_endpoint = "/discharge/info"
+        resp = await self._session.get(url=loc + url_endpoint)
+        status_code = resp.status
+        if status_code == 404:
+            url_endpoint = "/publickey"
+            resp = await self._session.get(url=loc + url_endpoint)
+            status_code = resp.status
+        if status_code != 200:
+            raise bakery.ThirdPartyInfoNotFound(
+                "unable to get info from {}".format(url_endpoint)
+            )
+        json_resp = await resp.json()
+        if json_resp is None:
+            raise bakery.ThirdPartyInfoNotFound(
+                "no response from /discharge/info"
+            )
+        pk = json_resp.get("PublicKey")
+        if pk is None:
+            raise bakery.ThirdPartyInfoNotFound(
+                "no public key found in /discharge/info"
+            )
+        idm_pk = bakery.PublicKey.deserialize(pk)
+        version = json_resp.get("Version", bakery.VERSION_1)
+        self._cache[loc] = bakery.ThirdPartyInfo(
+            version=version, public_key=idm_pk
+        )
+        return self._cache.get(loc)
+
+    async def close(self):
+        await self._session.close()
---
+++ src/tests/maasapiserver/common/auth/test_locator.py
@@ -0,0 +1,103 @@
+#  Copyright 2024 Canonical Ltd.  This software is licensed under the
+#  GNU Affero General Public License version 3 (see the file LICENSE).
+
+from macaroonbakery import bakery
+import pytest
+
+from maasapiserver.common.auth.locator import AsyncThirdPartyLocator
+
+
+@pytest.mark.asyncio
+class TestKeyRing:
+    """
+    Async porting of https://git.launchpad.net/ubuntu/+source/py-macaroon-bakery/tree/macaroonbakery/tests/test_keyring.py
+    """
+
+    async def test_cache_fetch(self, mock_aioresponse) -> None:
+        key = bakery.generate_key()
+
+        mock_aioresponse.get(
+            url="http://test/discharge/info",
+            status=200,
+            payload={
+                "Version": bakery.LATEST_VERSION,
+                "PublicKey": str(key.public_key),
+            },
+        )
+
+        expectInfo = bakery.ThirdPartyInfo(
+            public_key=key.public_key, version=bakery.LATEST_VERSION
+        )
+        kr = AsyncThirdPartyLocator(allow_insecure=True)
+        info = await kr.third_party_info("http://test/")
+        await kr.close()
+
+        assert info == expectInfo
+
+    async def test_cache_norefetch(self, mock_aioresponse):
+        key = bakery.generate_key()
+
+        mock_aioresponse.get(
+            url="http://test/discharge/info",
+            status=200,
+            payload={
+                "Version": bakery.LATEST_VERSION,
+                "PublicKey": str(key.public_key),
+            },
+        )
+
+        expectInfo = bakery.ThirdPartyInfo(
+            public_key=key.public_key, version=bakery.LATEST_VERSION
+        )
+        kr = AsyncThirdPartyLocator(allow_insecure=True)
+        info = await kr.third_party_info("http://test/")
+        assert info == expectInfo
+        info = await kr.third_party_info("http://test/")
+        await kr.close()
+        assert info == expectInfo
+
+    async def test_cache_fetch_no_version(self, mock_aioresponse):
+        key = bakery.generate_key()
+
+        mock_aioresponse.get(
+            url="http://test/discharge/info",
+            status=200,
+            payload={
+                "PublicKey": str(key.public_key),
+            },
+        )
+
+        expectInfo = bakery.ThirdPartyInfo(
+            public_key=key.public_key, version=bakery.VERSION_1
+        )
+        kr = AsyncThirdPartyLocator(allow_insecure=True)
+        info = await kr.third_party_info("http://test/")
+        await kr.close()
+        assert info == expectInfo
+
+    async def test_allow_insecure(self):
+        kr = AsyncThirdPartyLocator()
+        with pytest.raises(bakery.ThirdPartyInfoNotFound):
+            await kr.third_party_info("http://test/")
+        await kr.close()
+
+    async def test_fallback(self, mock_aioresponse):
+        key = bakery.generate_key()
+
+        mock_aioresponse.get(url="http://test/discharge/info", status=404)
+
+        mock_aioresponse.get(
+            url="http://test/publickey",
+            status=200,
+            payload={
+                "PublicKey": str(key.public_key),
+            },
+        )
+
+        expectInfo = bakery.ThirdPartyInfo(
+            public_key=key.public_key, version=bakery.VERSION_1
+        )
+        kr = AsyncThirdPartyLocator(allow_insecure=True)
+        info = await kr.third_party_info("http://test/")
+        await kr.close()
+        assert info == expectInfo
