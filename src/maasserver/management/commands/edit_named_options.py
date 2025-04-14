canonical/maas

commit f9e6fbe7a43c74babee6ee4e18272892725c4b52

fix: introduce a new BaseCommandWithConnection class to execute CLI commands that require a database connection. This new class ensures that the service layer is properly initialized and destroyed afterwards.

src/maasserver/management/commands/edit_named_options.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2014-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: Edit the named.conf.options file so that it includes
@@ -10,8 +10,9 @@ from collections import OrderedDict
 import sys
 from textwrap import dedent
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.models import Config
 from provisioningserver.dns.commands.edit_named_options import (
     add_arguments,
@@ -19,7 +20,7 @@ from provisioningserver.dns.commands.edit_named_options import (
 )
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = " ".join(
         dedent(
             """\

src/maasserver/management/commands/base.py
@@ -1,0 +1,23 @@
+#  Copyright 2025 Canonical Ltd.  This software is licensed under the
+#  GNU Affero General Public License version 3 (see the file LICENSE).
+from django.core.management import BaseCommand
+
+from maasserver.sqlalchemy import service_layer
+from maasserver.utils.orm import with_connection
+
+
+class BaseCommandWithConnection(BaseCommand):
+    """
+    A base class for executing commands that require a database connection.
+
+    This class ensures that the service layer is properly initialized before execution
+    and safely closed afterward.
+    """
+
+    @with_connection
+    def execute(self, *args, **options):
+        try:
+            service_layer.init()
+            return super().execute(*args, **options)
+        finally:
+            service_layer.close()

src/maasserver/management/commands/changepasswords.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2015-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: Batch update multiple passwords non-interactively."""
@@ -8,12 +8,13 @@ from fileinput import input as fileinput
 from textwrap import dedent
 
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.utils.orm import transactional
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = dedent(
         """\
         Update passwords in batch mode.

src/maasserver/management/commands/config_tls.py
@@ -1,17 +1,17 @@
-# Copyright 2012-2022 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: configure regiond listener."""
 
 import argparse
 
-from django.core.management.base import BaseCommand
 from django.db import DEFAULT_DB_ALIAS
 
 from maascli.init import prompt_yes_no
 from maasserver.audit import create_audit_event
 from maasserver.enum import ENDPOINT
 from maasserver.listener import notify
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.models import Config
 from maasserver.regiondservices.certificate_expiration_check import (
     clear_tls_notifications,
@@ -47,7 +47,7 @@ def _update_tls_config(
     notify("sys_reverse_proxy")
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = "Configure MAAS Region TLS."
 
     def add_arguments(self, parser):

src/maasserver/management/commands/config_vault.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Canonical Ltd.  This software is licensed under the
+# Copyright 2022-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: configure vault integration."""
@@ -8,13 +8,14 @@ from textwrap import dedent
 import time
 from typing import Optional
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 import yaml
 
 from maascli.init import prompt_yes_no
 from maasserver.enum import NODE_TYPE
 from maasserver.listener import notify
 from maasserver.locks import startup
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.models import (
     Config,
     ControllerInfo,
@@ -24,7 +25,7 @@ from maasserver.models import (
     VaultSecret,
 )
 from maasserver.utils import synchronised
-from maasserver.utils.orm import transactional, with_connection
+from maasserver.utils.orm import transactional
 from maasserver.vault import (
     configure_region_with_vault,
     get_region_vault_client,
@@ -34,7 +35,7 @@ from maasserver.vault import (
 from provisioningserver.utils.env import MAAS_ID
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = "Configure MAAS Region Vault integration."
     CONFIGURE_COMMAND = "configure"
     MIGRATE_COMMAND = "migrate"
@@ -167,7 +168,6 @@ class Command(BaseCommand):
             .order_by("node__hostname")
         )
 
-    @with_connection  # Needed by the following lock.
     @synchronised(startup)
     def _handle_migrate(self, options):
         if Config.objects.get_config("vault_enabled", False):
@@ -201,7 +201,6 @@ class Command(BaseCommand):
 
         return "Successfully migrated cluster secrets to Vault"
 
-    @with_connection
     def _handle_status(self, options):
         vault_enabled = Config.objects.get_config("vault_enabled", False)
         report = {"status": "enabled" if vault_enabled else "disabled"}

src/maasserver/management/commands/configauth.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2018 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: configure the authentication source."""
@@ -8,7 +8,7 @@ import json
 
 from django.contrib.sessions.models import Session
 from django.core.exceptions import ValidationError
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 from macaroonbakery.bakery import generate_key
 
 from maascli.init import (
@@ -18,6 +18,7 @@ from maascli.init import (
     read_input,
 )
 from maasserver.macaroon_auth import APIError
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.models.rbacsync import RBAC_ACTION, RBACLastSync, RBACSync
 from maasserver.rbac import RBACUserClient
 from maasserver.secrets import SecretManager
@@ -185,7 +186,7 @@ def clear_user_sessions():
     Session.objects.all().delete()
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = "Configure external authentication."
 
     def add_arguments(self, parser):

src/maasserver/management/commands/createadmin.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2016 Canonical Ltd.  This software is licensed under the
+# Copyright 2012-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Django command: create an administrator account."""
@@ -8,12 +8,13 @@ import re
 
 from aiohttp import ClientError
 from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 from django.db import DEFAULT_DB_ALIAS, IntegrityError, transaction
 
 from maascli.init import read_input
 from maasserver.enum import KEYS_PROTOCOL_TYPE
 from maasserver.macaroon_auth import external_auth_enabled
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.models import SSHKey
 from maasserver.models.sshkey import ImportSSHKeysError
 
@@ -118,7 +119,7 @@ def validate_ssh_import(ssh_import: str):
             )
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = "Create a MAAS administrator account."
 
     def add_arguments(self, parser):

src/maasserver/management/commands/msm.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Canonical Ltd.  This software is licensed under the
+# Copyright 2024-2025 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Commands for managing enrolment with a Site Manager instance."""
@@ -9,19 +9,19 @@ from datetime import datetime
 from ssl import CertificateError
 from urllib.parse import urlparse
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 from jose import ExpiredSignatureError, JOSEError, jwt
 from jsonschema import validate, ValidationError
 import yaml
 
 from maascli.init import prompt_yes_no
 from maasserver.enum import MSM_STATUS
+from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.msm import msm_enrol, msm_status, msm_withdraw
 from maasserver.utils.certificates import get_ssl_certificate
-from maasserver.utils.orm import with_connection
 
 
-class Command(BaseCommand):
+class Command(BaseCommandWithConnection):
     help = "Configure enrolment with a Site Manager instance"
     ENROL_COMMAND = "enrol"
     STATUS_COMMAND = "status"
@@ -78,7 +78,6 @@ class Command(BaseCommand):
                 # something is wrong
                 print("Could not determine the status of enrolment")
 
-    @with_connection
     def _enrol(self, options):
         # We don't know exactly what to expect from these claims, so don't verify them
         decode_opts = {

commit dbfdab51d658b0388eee12e7b44965014cadf5dc

fix: the CLI command edit_named_options should not require a db connection. Fixing the regression introduced with f9e6fbe7a43c74babee6ee4e18272892725c4b52.

src/maasserver/management/commands/edit_named_options.py
@@ -10,9 +10,8 @@ from collections import OrderedDict
 import sys
 from textwrap import dedent
 
-from django.core.management.base import CommandError
+from django.core.management.base import BaseCommand, CommandError
 
-from maasserver.management.commands.base import BaseCommandWithConnection
 from maasserver.models import Config
 from provisioningserver.dns.commands.edit_named_options import (
     add_arguments,
@@ -20,7 +19,7 @@ from provisioningserver.dns.commands.edit_named_options import (
 )
 
 
-class Command(BaseCommandWithConnection):
+class Command(BaseCommand):
     help = " ".join(
         dedent(
             """\