canonical/maas

commit 08204ac9bba3c3a1707815c1e5ad75d0724917e9

feat: patch uvicorn to include the TLS certificate info into the request scope and restructure the main module of maasapiserver.

src/maasapiserver/main.py
@@ -7,13 +7,17 @@ import logging
 import ssl
 
 from django.conf import settings as django_settings
-from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 import structlog
-import uvicorn
 
+from maasapiserver.app import (
+    App,
+    EventListener,
+    ExceptionHandler,
+    MiddlewareHandler,
+    ServerConfig,
+)
 from maasapiserver.common.api.handlers import APICommon
-from maasapiserver.common.constants import API_PREFIX
 from maasapiserver.common.middlewares.db import (
     DatabaseMetricsMiddleware,
     TransactionMiddleware,
@@ -29,6 +33,7 @@ from maasapiserver.settings import (
     internal_api_service_socket_path,
     read_config,
 )
+from maasapiserver.tls import TLSPatchedH11Protocol
 from maasapiserver.v3.api.internal.handlers import APIv3Internal
 from maasapiserver.v3.api.public.handlers import APIv3, APIv3UI
 from maasapiserver.v3.listeners.vault import VaultMigrationPostgresListener
@@ -63,119 +68,91 @@ def config_uvicorn_logging(level=logging.INFO) -> None:
     )
 
 
-async def prepare_app(
-    config: Config,
-    transaction_middleware_class: type = TransactionMiddleware,
-    # In the tests the database is created in the fixture, so we need to inject it here as a parameter.
-    db: Database = None,  # pyright: ignore [reportArgumentType]
-    add_authentication_middleware: bool = True,
-    app_title: str = "APIServer",
-    app_name: str = "apiserver",
-) -> FastAPI:
-    """Create the FastAPI application."""
-
-    if not django_settings.configured:
-        django_settings.configure()
-
-    if db is None:
-        db = Database(config.db, echo=config.debug_queries)
-
-    # In maasserver we have a startup lock. If it is set, we have to wait to start maasapiserver as well.
-    await wait_for_startup(db)
-
-    services_cache = CacheForServices()
-
-    app = FastAPI(
-        title=app_title,
-        name=app_name,
-        openapi_url=f"{API_PREFIX}/openapi.json",
-        # The SwaggerUI page is provided by the APICommon router.
-        docs_url=None,
-    )
-
-    # The order here is important: the exception middleware must be the first one being executed (i.e. it must be the last
-    # middleware added here)
-    app.add_middleware(PrometheusMiddleware)
-    app.add_middleware(DatabaseMetricsMiddleware, db=db)
-
-    if add_authentication_middleware:
-        app.add_middleware(
-            V3AuthenticationMiddleware,
-            providers_cache=AuthenticationProvidersCache(
-                jwt_authentication_providers=[LocalAuthenticationProvider()],
-                session_authentication_provider=DjangoSessionAuthenticationProvider(),
-                macaroon_authentication_provider=MacaroonAuthenticationProvider(),
-            ),
-        )
-
-    app.add_middleware(ServicesMiddleware, cache=services_cache)
-    app.add_middleware(transaction_middleware_class, db=db)
-    app.add_middleware(ExceptionMiddleware)
-    app.add_middleware(ContextMiddleware)
-
-    # Add exception handlers for exceptions that can be thrown outside the middlewares.
-    app.add_exception_handler(
-        RequestValidationError,
-        ExceptionHandlers.validation_exception_handler,  # pyright: ignore [reportArgumentType]
-    )
-
-    APICommon.register(app.router)
-
-    # Event handlers
-    app.add_event_handler(
-        "startup",
-        partial(
-            PostgresListenersTaskFactory.create,
-            db_engine=db.engine,
-            listeners=[VaultMigrationPostgresListener()],
-        ),
-    )
-    app.add_event_handler("shutdown", services_cache.close)
-
-    return app
-
-
-async def create_app(
-    config: Config,
-    transaction_middleware_class: type = TransactionMiddleware,
-    # In the tests the database is created in the fixture, so we need to inject it here as a parameter.
-    db: Database = None,  # pyright: ignore [reportArgumentType]
-) -> FastAPI:
-    app = await prepare_app(
-        config,
-        transaction_middleware_class,
-        db,
-        True,
+def craft_public_app(db: Database) -> App:
+    cache = CacheForServices()
+    return App(
         "MAASAPIServer",
         "maasapiserver",
+        [APICommon, APIv3, APIv3UI],
+        middlewares=[
+            MiddlewareHandler(PrometheusMiddleware),
+            MiddlewareHandler(DatabaseMetricsMiddleware, db=db),
+            MiddlewareHandler(
+                V3AuthenticationMiddleware,
+                providers_cache=AuthenticationProvidersCache(
+                    jwt_authentication_providers=[
+                        LocalAuthenticationProvider()
+                    ],
+                    session_authentication_provider=DjangoSessionAuthenticationProvider(),
+                    macaroon_authentication_provider=MacaroonAuthenticationProvider(),
+                ),
+            ),
+            MiddlewareHandler(ServicesMiddleware, cache=cache),
+            MiddlewareHandler(TransactionMiddleware, db=db),
+            MiddlewareHandler(ExceptionMiddleware),
+            MiddlewareHandler(ContextMiddleware),
+        ],
+        exception_handlers=[
+            ExceptionHandler(
+                RequestValidationError,
+                ExceptionHandlers.validation_exception_handler,
+            )
+        ],
+        event_listeners=[
+            EventListener(
+                "startup",
+                partial(
+                    PostgresListenersTaskFactory.create,
+                    db_engine=db.engine,
+                    listeners=[VaultMigrationPostgresListener()],
+                ),
+            ),
+            EventListener("shutdown", cache.close),
+        ],
+        server_config=ServerConfig(
+            socket_path=api_service_socket_path().as_posix()
+        ),
     )
-    APIv3.register(app.router)
-    APIv3UI.register(app.router)
-    return app
 
 
-async def create_internal_app(
-    config: Config,
-    transaction_middleware_class: type = TransactionMiddleware,
-    # In the tests the database is created in the fixture, so we need to inject it here as a parameter.
-    db: Database = None,  # pyright: ignore [reportArgumentType]
-) -> FastAPI:
-    # DO NOT add the authentication middleware. We enable MTLS at uvicorn level.
-    app = await prepare_app(
-        config,
-        transaction_middleware_class,
-        db,
-        False,
+def craft_internal_app(
+    db: Database, server_config: ServerConfig | None = None
+) -> App:
+    if server_config is None:
+        # Useful for the tests
+        cert, key, cacerts = get_maas_cluster_cert_paths()  # pyright: ignore [reportGeneralTypeIssues]
+        server_config = ServerConfig(
+            socket_path=internal_api_service_socket_path().as_posix(),
+            ssl_keyfile=key,
+            ssl_certfile=cert,
+            ssl_ca_certs=cacerts,
+            ssl_cert_reqs=ssl.CERT_OPTIONAL,
+            http=TLSPatchedH11Protocol,
+        )
+
+    internal_cache = CacheForServices()
+    return App(
         "MAASInternalAPIServer",
         "maasinternalapiserver",
+        [APICommon, APIv3Internal],
+        middlewares=[
+            MiddlewareHandler(PrometheusMiddleware),
+            MiddlewareHandler(DatabaseMetricsMiddleware, db=db),
+            MiddlewareHandler(ServicesMiddleware, cache=internal_cache),
+            MiddlewareHandler(TransactionMiddleware, db=db),
+            MiddlewareHandler(RequireClientCertMiddleware),
+            MiddlewareHandler(ExceptionMiddleware),
+            MiddlewareHandler(ContextMiddleware),
+        ],
+        exception_handlers=[
+            ExceptionHandler(
+                RequestValidationError,
+                ExceptionHandlers.validation_exception_handler,
+            )
+        ],
+        event_listeners=[EventListener("shutdown", internal_cache.close)],
+        server_config=server_config,
     )
-
-    app.add_middleware(ExceptionMiddleware)
-    app.add_middleware(RequireClientCertMiddleware)
-
-    APIv3Internal.register(app.router)
-
-    return app
 
 
 def run(app_config: Config | None = None):
@@ -185,12 +162,14 @@ def run(app_config: Config | None = None):
     because they rely on the fact that uvicorn is providing security on top. For this reason, DO NOT include the v3Internal
     router in the user app!
     """
+
+    if not django_settings.configured:
+        django_settings.configure()
+
     loop = asyncio.new_event_loop()
 
     if app_config is None:
         app_config = loop.run_until_complete(read_config())
-
-    servers_tasks = []
 
     configure_logging(
         level=logging.DEBUG if app_config.debug else logging.INFO,
@@ -202,42 +181,16 @@ def run(app_config: Config | None = None):
         logging.DEBUG if app_config.debug_http else logging.INFO
     )
 
-    # User app
-    user_app = loop.run_until_complete(create_app(config=app_config))
-    user_server_config = uvicorn.Config(
-        user_app,
-        loop="asyncio",
-        proxy_headers=True,
-        uds=str(api_service_socket_path()),
-        # We configure the logging OUTSIDE the library in order to use our custom json formatter.
-        log_config=None,
-    )
-    user_server = uvicorn.Server(user_server_config)
-    servers_tasks.append(user_server.serve())
+    db = Database(app_config.db, echo=app_config.debug_queries)
+    # In maasserver we have a startup lock. If it is set, we have to wait to start maasapiserver as well.
+    loop.run_until_complete(wait_for_startup(db))
 
-    # Internal app
-    internal_app = loop.run_until_complete(
-        create_internal_app(config=app_config)
-    )
-
-    cert, key, cacerts = get_maas_cluster_cert_paths()  # pyright: ignore [reportGeneralTypeIssues]
-    internal_server_config = uvicorn.Config(
-        internal_app,
-        loop="asyncio",
-        proxy_headers=True,
-        uds=internal_api_service_socket_path().as_posix(),
-        ssl_keyfile=key,
-        ssl_certfile=cert,
-        ssl_ca_certs=cacerts,
-        ssl_cert_reqs=ssl.CERT_REQUIRED,
-        # We configure the logging OUTSIDE the library in order to use our custom json formatter.
-        log_config=None,
-    )
-    internal_server = uvicorn.Server(internal_server_config)
+    public_app = craft_public_app(db)
+    internal_app = craft_internal_app(db)
 
     async def run_servers():
         return await asyncio.gather(
-            user_server.serve(), internal_server.serve()
+            public_app.server.serve(), internal_app.server.serve()
         )
 
     loop.run_until_complete(run_servers())

src/tests/maasapiserver/fixtures/app.py
@@ -1,3 +1,6 @@
+# Copyright 2024-2025 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
 from datetime import timedelta
 from typing import AsyncIterator, Awaitable, Callable, Iterator
 from unittest.mock import Mock
@@ -16,7 +19,7 @@ from maasapiserver.common.middlewares.exceptions import (
     ExceptionHandlers,
     ExceptionMiddleware,
 )
-from maasapiserver.main import create_app
+from maasapiserver.main import craft_public_app
 from maasapiserver.settings import Config
 from maasapiserver.v3.api.internal.handlers import APIv3Internal
 from maasapiserver.v3.api.public.handlers import APIv3, APIv3UI
@@ -251,12 +254,8 @@ async def api_app(
     db: Database,
 ) -> Iterator[FastAPI]:
     """The API application."""
-
-    yield await create_app(
-        config=test_config,
-        transaction_middleware_class=transaction_middleware_class,
-        db=db,
-    )
+    app = craft_public_app(db)
+    yield app.fastapi_app
 
 
 @pytest.fixture

src/maasapiserver/app.py
@@ -1,0 +1,126 @@
+# Copyright 2025 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+import asyncio
+from dataclasses import dataclass
+import ssl
+from typing import Callable, Type
+
+from fastapi import FastAPI
+import uvicorn
+from uvicorn.config import HTTPProtocolType
+
+from maasapiserver.common.api.base import API
+from maasapiserver.common.constants import API_PREFIX
+
+
+class MiddlewareHandler:
+    def __init__(self, middleware_class, **kwargs):
+        self.middleware_class = middleware_class
+        self.kwargs = kwargs
+
+    def get_middleware(self):
+        return self.middleware_class
+
+    def get_kwargs(self):
+        return self.kwargs
+
+
+@dataclass
+class ExceptionHandler:
+    exception_type: Type[Exception]
+    handler: Callable
+
+
+@dataclass
+class EventListener:
+    event: str
+    handler: Callable
+
+
+@dataclass
+class ServerConfig:
+    host: str = "127.0.0.1"
+    port: int = 8000
+    socket_path: str | None = None
+    ssl_keyfile: str | None = None
+    ssl_certfile: str | None = None
+    ssl_ca_certs: str | None = None
+    ssl_cert_reqs: int = ssl.CERT_NONE
+    http: type[asyncio.Protocol] | HTTPProtocolType = "auto"
+
+
+class App:
+    def __init__(
+        self,
+        app_title: str,
+        app_name: str,
+        api: list[API],
+        # Order is important: the last in the list is the first processing the request.
+        middlewares: list[MiddlewareHandler],
+        exception_handlers: list[ExceptionHandler],
+        event_listeners: list[EventListener],
+        server_config: ServerConfig,
+    ):
+        self._app_title = app_title
+        self._name = app_name
+        self._api = api
+        self._middlewares = middlewares
+        self._exception_handlers = exception_handlers
+        self._event_listeners = event_listeners
+        self._server_config = server_config
+        self._app = self._prepare_app()
+        self._server = self._prepare_server()
+
+    def _prepare_app(self):
+        app = FastAPI(
+            title=self._app_title,
+            name=self._name,
+            openapi_url=f"{API_PREFIX}/openapi.json",
+            # The SwaggerUI page is provided by the APICommon router.
+            docs_url=None,
+        )
+
+        for api in self._api:
+            api.register(app.router)
+
+        for middleware in self._middlewares:
+            app.add_middleware(
+                middleware.get_middleware(), **middleware.get_kwargs()
+            )
+
+        for exception_handler in self._exception_handlers:
+            app.add_exception_handler(
+                exception_handler.exception_type, exception_handler.handler
+            )
+
+        for event_listener in self._event_listeners:
+            app.add_event_handler(event_listener.event, event_listener.handler)
+
+        return app
+
+    def _prepare_server(self) -> uvicorn.Server:
+        server_config = uvicorn.Config(
+            self._app,
+            loop="asyncio",
+            proxy_headers=True,
+            host=self._server_config.host,
+            port=self._server_config.port,
+            uds=self._server_config.socket_path,
+            ssl_keyfile=self._server_config.ssl_keyfile,
+            ssl_certfile=self._server_config.ssl_certfile,
+            ssl_ca_certs=self._server_config.ssl_ca_certs,
+            ssl_cert_reqs=self._server_config.ssl_cert_reqs,
+            # We configure the logging OUTSIDE the library in order to use our custom json formatter.
+            log_config=None,
+            http=self._server_config.http,
+        )
+        return uvicorn.Server(server_config)
+
+    @property
+    def fastapi_app(self) -> FastAPI:
+        return self._app
+
+    @property
+    def server(self) -> uvicorn.Server:
+        return self._server

src/maasapiserver/settings.py
@@ -18,7 +18,7 @@ logger = structlog.getLogger()
 
 @dataclass
 class Config:
-    db: DatabaseConfig | None
+    db: DatabaseConfig
     debug: bool = False
     debug_queries: bool = False
     debug_http: bool = False
@@ -88,23 +88,12 @@ async def _get_default_db_config(
 
 
 async def read_config() -> Config:
-    try:
-        with RegionConfiguration.open() as config:
-            database_config = await _get_default_db_config(config)
-            debug = config.debug
-            debug_queries = debug or config.debug_queries
-            debug_http = debug or config.debug_http
-            num_workers = config.num_workers
-    except (FileNotFoundError, KeyError, ValueError):
-        # The regiond.conf will attempt to be loaded when the 'maas' command
-        # is read by a standard user. We allow this to fail and miss configure the
-        # database information. Django will still complain since no 'default'
-        # connection is defined.
-        database_config = None
-        debug = False
-        debug_queries = False
-        debug_http = False
-        num_workers = 4
+    with RegionConfiguration.open() as config:
+        database_config = await _get_default_db_config(config)
+        debug = config.debug
+        debug_queries = debug or config.debug_queries
+        debug_http = debug or config.debug_http
+        num_workers = config.num_workers
 
     return Config(
         db=database_config,

src/maasapiserver/tls.py
@@ -1,0 +1,170 @@
+# Copyright 2025 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+# ruff: noqa
+# pyright: basic, reportArgumentType=false
+
+import asyncio
+import ssl
+from urllib.parse import unquote
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+import h11
+from uvicorn.protocols.http.flow_control import (
+    HIGH_WATER_LIMIT,
+    service_unavailable,
+)
+from uvicorn.protocols.http.h11_impl import H11Protocol, RequestResponseCycle
+
+
+class TLSPatchedH11Protocol(H11Protocol):
+    """
+    Patched version of H11Protocol to include the information about TSL. We can get rid of this when Uvicorn will officially
+    add support for it https://github.com/Kludex/uvicorn/issues/1118 .
+    """
+
+    def _include_tls(self, transport, scope):
+        scope["extensions"] = {}
+
+        ssl_object = transport.get_extra_info("ssl_object")
+
+        if ssl_object:
+            tlsext = scope["extensions"]["tls"] = {
+                "tls_used": True,
+                "client_cert_chain": [],
+                "tls_version": {
+                    "TLSv1": 0x0301,
+                    "TLSv1.1": 0x0302,
+                    "TLSv1.2": 0x0303,
+                    "TLSv1.3": 0x0304,
+                }.get(ssl_object.version(), None),
+            }
+
+            client_cert = ssl_object.getpeercert(binary_form=True)
+            if client_cert:
+                pem_cert = ssl.DER_cert_to_PEM_cert(client_cert)
+                tlsext["client_cert_chain"].append(pem_cert)
+
+                # Extract CN using cryptography
+                cert_obj = x509.load_pem_x509_certificate(
+                    pem_cert.encode("utf-8"), default_backend()
+                )
+                cn_attr = cert_obj.subject.get_attributes_for_oid(
+                    x509.NameOID.COMMON_NAME
+                )
+                if cn_attr:
+                    tlsext["client_cn"] = cn_attr[0].value
+
+    # Copyright © 2017-present, Encode OSS Ltd. All rights reserved.
+    # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+    #   - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    #   - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    #   - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+    # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    def handle_events(self) -> None:
+        while True:
+            try:
+                event = self.conn.next_event()
+            except h11.RemoteProtocolError:
+                msg = "Invalid HTTP request received."
+                self.logger.warning(msg)
+                self.send_400_response(msg)
+                return
+
+            if event is h11.NEED_DATA:
+                break
+
+            elif event is h11.PAUSED:
+                # This case can occur in HTTP pipelining, so we need to
+                # stop reading any more data, and ensure that at the end
+                # of the active request/response cycle we handle any
+                # events that have been buffered up.
+                self.flow.pause_reading()
+                break
+
+            elif isinstance(event, h11.Request):
+                self.headers = [
+                    (key.lower(), value) for key, value in event.headers
+                ]
+                raw_path, _, query_string = event.target.partition(b"?")
+                path = unquote(raw_path.decode("ascii"))
+                full_path = self.root_path + path
+                full_raw_path = self.root_path.encode("ascii") + raw_path
+                self.scope = {
+                    "type": "http",
+                    "asgi": {
+                        "version": self.config.asgi_version,
+                        "spec_version": "2.3",
+                    },
+                    "http_version": event.http_version.decode("ascii"),
+                    "server": self.server,
+                    "client": self.client,
+                    "scheme": self.scheme,  # type: ignore[typeddict-item]
+                    "method": event.method.decode("ascii"),
+                    "root_path": self.root_path,
+                    "path": full_path,
+                    "raw_path": full_raw_path,
+                    "query_string": query_string,
+                    "headers": self.headers,
+                    "state": self.app_state.copy(),
+                }
+                ### BEGIN PATCH
+                self._include_tls(self.transport, self.scope)
+                ### END PATCH
+
+                upgrade = self._get_upgrade()
+                if upgrade == b"websocket" and self._should_upgrade_to_ws():
+                    self.handle_websocket_upgrade(event)
+                    return
+
+                # Handle 503 responses when 'limit_concurrency' is exceeded.
+                if self.limit_concurrency is not None and (
+                    len(self.connections) >= self.limit_concurrency
+                    or len(self.tasks) >= self.limit_concurrency
+                ):
+                    app = service_unavailable
+                    message = "Exceeded concurrency limit."
+                    self.logger.warning(message)
+                else:
+                    app = self.app
+
+                # When starting to process a request, disable the keep-alive
+                # timeout. Normally we disable this when receiving data from
+                # client and set back when finishing processing its request.
+                # However, for pipelined requests processing finishes after
+                # already receiving the next request and thus the timer may
+                # be set here, which we don't want.
+                self._unset_keepalive_if_required()
+
+                self.cycle = RequestResponseCycle(
+                    scope=self.scope,
+                    conn=self.conn,
+                    transport=self.transport,
+                    flow=self.flow,
+                    logger=self.logger,
+                    access_logger=self.access_logger,
+                    access_log=self.access_log,
+                    default_headers=self.server_state.default_headers,
+                    message_event=asyncio.Event(),
+                    on_response=self.on_response_complete,
+                )
+                task = self.loop.create_task(self.cycle.run_asgi(app))
+                task.add_done_callback(self.tasks.discard)
+                self.tasks.add(task)
+
+            elif isinstance(event, h11.Data):
+                if self.conn.our_state is h11.DONE:
+                    continue
+                self.cycle.body += event.data
+                if len(self.cycle.body) > HIGH_WATER_LIMIT:
+                    self.flow.pause_reading()
+                self.cycle.message_event.set()
+
+            elif isinstance(event, h11.EndOfMessage):
+                if self.conn.our_state is h11.DONE:
+                    self.transport.resume_reading()
+                    self.conn.start_next_cycle()
+                    continue
+                self.cycle.more_body = False
+                self.cycle.message_event.set()

src/maasapiserver/v3/middlewares/client_certificate.py
@@ -33,13 +33,14 @@ class RequireClientCertMiddleware(BaseHTTPMiddleware):
         ):
             return await call_next(request)
 
-        if (
-            "extensions" in request.scope
-            and request.scope["extensions"].get("tls", {}).get("CN") is None
-        ):
+        # The internal API server is using a special version of uvicorn to always include the tls info inside the context,
+        # so we can assume it's there.
+        cn = request.scope["extensions"].get("tls", {}).get("client_cn", None)
+        if cn is None:
             return JSONResponse(
                 {"detail": "Client certificate required."},
                 status_code=403,
             )
 
+        request.scope["client_cn"] = cn
         return await call_next(request)

src/tests/maasapiserver/common/api/test_db.py
@@ -1,9 +1,11 @@
+# Copyright 2024-2025 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
 from typing import AsyncIterator
 
 from fastapi import FastAPI
 from httpx import AsyncClient
 import pytest
-from pytest_mock import MockerFixture
 from sqlalchemy import (
     Column,
     insert,
@@ -18,8 +20,7 @@ from sqlalchemy.ext.asyncio import AsyncConnection
 from starlette.requests import Request
 
 from maasapiserver.common.api.base import API, Handler, handler
-from maasapiserver.main import create_app
-from maasapiserver.settings import Config
+from maasapiserver.main import craft_public_app
 from maasservicelayer.db import Database
 
 METADATA = MetaData()
@@ -39,10 +40,8 @@ class MyException(Exception):
 
 @pytest.fixture
 async def insert_app(
-    test_config: Config,
     db: Database,
     db_connection: AsyncConnection,
-    mocker: MockerFixture,
 ) -> FastAPI:
     class InsertHandler(Handler):
         @handler(path="/success", methods=["GET"])
@@ -58,7 +57,7 @@ async def insert_app(
             )
             raise MyException("boom")
 
-    api_app = await create_app(config=test_config, db=db)
+    api_app = craft_public_app(db=db).fastapi_app
     api = API(prefix="/insert", handlers=[InsertHandler()])
     api.register(api_app.router)
 

commit d600c352e9ef49a5eb4f96e02e21b10c5c9c0317

fix: add transaction_middleware_class back to craft_public_app
This fixes performance tests that were were broken in 08204ac9.

src/maasapiserver/main.py
@@ -68,7 +68,9 @@ def config_uvicorn_logging(level=logging.INFO) -> None:
     )
 
 
-def craft_public_app(db: Database) -> App:
+def craft_public_app(
+    db: Database, transaction_middleware_class: type = TransactionMiddleware
+) -> App:
     cache = CacheForServices()
     return App(
         "MAASAPIServer",
@@ -88,7 +90,7 @@ def craft_public_app(db: Database) -> App:
                 ),
             ),
             MiddlewareHandler(ServicesMiddleware, cache=cache),
-            MiddlewareHandler(TransactionMiddleware, db=db),
+            MiddlewareHandler(transaction_middleware_class, db=db),
             MiddlewareHandler(ExceptionMiddleware),
             MiddlewareHandler(ContextMiddleware),
         ],

src/tests/maasapiserver/fixtures/app.py
@@ -20,7 +20,6 @@ from maasapiserver.common.middlewares.exceptions import (
     ExceptionMiddleware,
 )
 from maasapiserver.main import craft_public_app
-from maasapiserver.settings import Config
 from maasapiserver.v3.api.internal.handlers import APIv3Internal
 from maasapiserver.v3.api.public.handlers import APIv3, APIv3UI
 from maasapiserver.v3.api.public.models.responses.oauth2 import (
@@ -249,12 +248,13 @@ async def mocked_api_client_admin_rbac(
 
 @pytest.fixture
 async def api_app(
-    test_config: Config,
     transaction_middleware_class: type,
     db: Database,
 ) -> Iterator[FastAPI]:
     """The API application."""
-    app = craft_public_app(db)
+    app = craft_public_app(
+        db=db, transaction_middleware_class=transaction_middleware_class
+    )
     yield app.fastapi_app
 
 