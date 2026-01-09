89b08d93e43e964d7d5b64fea4ef944a2ca40f58
Aloizio Macedo <aloizio.macedo@canonical.com>
2026-01-09 15:52:23 -0300

    chore: prepare for fetching installer from service

--- src/maasapiserver/v3/api/public/handlers/nos.py
+++ src/maasapiserver/v3/api/public/handlers/nos.py
@@ -4,7 +4,7 @@
 from enum import Enum
 import os
 from pathlib import Path
-from typing import Iterator, assert_never
+from typing import assert_never, Iterator, Protocol
 
 from fastapi import Depends, Request
 from fastapi.responses import StreamingResponse
@@ -27,6 +27,8 @@ _SNAP_COMMON = Path(os.environ.get("SNAP_COMMON", ""))
 
 
 class OnieHeaders(BaseModel):
+    """Headers that come out of a ONIE request for installer."""
+
     serial_number: str = Field(alias="onie-serial-number")
     eth_address: str = Field(alias="onie-eth-addr")
     vendor_id: str = Field(alias="onie-vendor-id")
@@ -50,60 +52,68 @@ class OnieHeaders(BaseModel):
             return None
 
 
-class Installer(Enum):
+class Installer(Protocol):
+    """Provides the bytes of the binary associated to an installer."""
+
+    def bytes_stream(self) -> Iterator[bytes]: ...
+
+
+class InstallerEnum(Enum):
+    """Installers related to a fixed set of available choices."""
+
     DELL = "DELL"
     SONIC = "SONIC"
 
     # TODO: Implement based on where the installers are.
-    def storage(self) -> FileStorage:
-        match self:
-            case Installer.DELL:
-                return FileStorage(
-                    id=1000,
-                    filename="dell",
-                    content=bytes('echo "Hello, world!"', encoding="utf8"),
-                    key="1",
-                    owner_id=None,
-                )
-            case Installer.SONIC:
-                raise NotImplementedError
-            case _:
-                assert_never(self)
-
-    # TODO: Implement based on where the installers are.
     def bytes_stream(self) -> Iterator[bytes]:
         match self:
-            case Installer.DELL:
+            case InstallerEnum.DELL:
                 with open(
                     _SNAP_COMMON.joinpath("dell.bin"),
                     "rb",
                 ) as file:
                     while chunk := file.read(_FIVE_MB):
                         yield chunk
-            case Installer.SONIC:
+            case InstallerEnum.SONIC:
                 raise NotImplementedError
             case _:
                 assert_never(self)
 
 
-# TODO: Implement based on where the installers are.
-def find_user_specified_installer(
-    onie_headers: OnieHeaders,
-) -> Installer | None:
-    return None
+class InstallerFetcher(Protocol):
+    """Fetches installer from some service or file storage."""
+
+    def get_installer(self, onie_headers: OnieHeaders) -> Installer | None: ...
+
+
+class EnumFetcher:
+    """Fetches installer from existing, provided choices."""
+
+    def get_installer(self, onie_headers: OnieHeaders) -> Installer | None:
+        return InstallerEnum.DELL
 
 
 # TODO: Add logic of how to determine the NOS
 # image to use.
-def get_installer(request: Request) -> Installer:
+def choose_installer(request: Request, fetcher: InstallerFetcher) -> Installer:
+    """Chooses installer based on the request and on a way to fetch them.
+
+    The happy path is for this to just fetch the installer using the fetcher,
+    which should use the request and cross-reference it with a preconfigured
+    switch entry in the database to know which one to pull.
+
+    Logic is also implemented to deal with the case of no reference to an
+    existing configuration and/or inability to fetch a installer based
+    on the headers.
+    """
     onie_headers = OnieHeaders.from_request(request)
     if onie_headers is None:
-        return Installer.DELL
+        return InstallerEnum.DELL
 
-    installer = find_user_specified_installer(onie_headers)
+    installer = fetcher.get_installer(onie_headers)
     if installer is None:
         # TODO: Logic when the user did not specify an installer.
-        return Installer.DELL
+        return InstallerEnum.DELL
 
     return installer
 
@@ -137,14 +147,14 @@ class NOSInstallerHandler(Handler):
         self,
         request: Request,
     ):
-        installer = get_installer(request)
+        fetcher = EnumFetcher()
 
-        accept_header = request.headers.get("Accept")
+        installer = choose_installer(request, fetcher)
 
-        if accept_header == "application/hal+json":
-            return HalFileResponse.from_model(
-                installer.storage(),
-                self_base_hyperlink=f"{V3_API_PREFIX}/custom_images",
-            )
-        else:
-            return StreamingResponse(content=installer.bytes_stream())
+        # accept_header = request.headers.get("Accept")
+        # if accept_header == "application/hal+json":
+        # return HalFileResponse.from_model(
+        # installer.storage(),
+        # self_base_hyperlink=f"{V3_API_PREFIX}/custom_images",
+        # )
+        return StreamingResponse(content=installer.bytes_stream())
