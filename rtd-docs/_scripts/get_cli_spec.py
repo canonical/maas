d58a615af4f9453f0f1b286bed309e66ced212f7
Aloizio Macedo <aloizio.macedo@canonical.com>
2026-04-15 14:28:27 -0300

    docs: update copyright notices on scripts

--- rtd-docs/_scripts/get_cli_spec.py
+++ rtd-docs/_scripts/get_cli_spec.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2025 Canonical Ltd.  This software is licensed under the
+# Copyright 2026 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Discover MAAS CLI commands by constructing argparse tree from source."""
