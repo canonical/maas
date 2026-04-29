724713157a7230fc9a33e38b942d8dfb8d3c2604
Jacopo Rota <jacopo.rota@canonical.com>
2026-04-29 18:09:26 +0200

    fix: support sk-ssh-ed25519 and sk-ecdsa-sha2-nistp256 ssh keys

--- src/maascommon/enums/sshkeys.py
+++ src/maascommon/enums/sshkeys.py
@@ -17,6 +17,8 @@ OPENSSH_PROTOCOL2_KEY_TYPES = frozenset(
         "ecdsa-sha2-nistp256",
         "ecdsa-sha2-nistp384",
         "ecdsa-sha2-nistp521",
+        "sk-ecdsa-sha2-nistp256@openssh.com",
+        "sk-ssh-ed25519@openssh.com",
         "ssh-ed25519",
         "ssh-rsa",
     )
