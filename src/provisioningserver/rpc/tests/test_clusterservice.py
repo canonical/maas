0926961cfd2d9b9c0fdfd3a00d54448da1a51734
Jacopo Rota <jacopo.rota@canonical.com>
2026-05-21 12:50:56 +0200

    fix: prevent concurrent scale-up from over-allocating RPC connections (#274)
    The previous implementatino of `scale_up_connections` was not taking
    into account that new connections have to go through a long-complex
    asynchronous process before becoming available. This lead concurrent
    calls to scale up connections without limit, because all the calls were
    evaluating `len(conns) < max_connections` as true, without taking into
    consideration the pending connections.
    
    With this change, `scale_up_connections` tracks in-flight connections
    with a pending counter and wait for these connections to become ready
    (with a 30s timeout) before releasing the slot.
    
    In addition to that, `_reap_extra_connection` is now called with a
    timeout `_keepalive / 1000` because `keepalive` is in milliseconds, but
    `callLater` expects seconds.
    
    Resolves: LP:2074122
    
    (cherry-picked from 6fc05cb47935ea20a1bdec90d1825a13af2389e8)

--- src/provisioningserver/rpc/tests/test_clusterservice.py
+++ src/provisioningserver/rpc/tests/test_clusterservice.py
@@ -78,6 +78,7 @@ from provisioningserver.rpc.clusterservice import (
     get_scan_all_networks_args,
     spawnProcessAndNullifyStdout,
 )
+from provisioningserver.rpc.common import Client
 from provisioningserver.rpc.interfaces import IConnection
 from provisioningserver.rpc.testing import (
     call_responder,
@@ -1059,7 +1060,7 @@ class TestClusterClientService(MAASTestCase):
                 if len(conns) < service.connections._max_connections:
                     conn = yield service.connections.connect()
                     service.connections[ev].append(conn)
-                    break
+                    return Client(conn)
 
         scale_up = self.patch(service.connections, "scale_up_connections")
         scale_up.side_effect = mock_scale_up_connections
