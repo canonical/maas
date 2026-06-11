2323acc4a2ba1dabb56b07fef7bece8915d3ba46
Aloizio Macedo <aloizio.macedo@canonical.com>
2026-06-11 18:10:47 -0300

    docs: patch temporal and paramiko

--- docs/_scripts/get_api_spec.py
+++ docs/_scripts/get_api_spec.py
@@ -4,6 +4,7 @@
 
 """Discover MAAS CLI commands by constructing argparse tree from source."""
 
+from importlib.abc import Loader
 import os
 from typing import Any
 
@@ -100,6 +101,65 @@ def _patch_tftp():
     sys.modules["tftp.backend"] = tftp.backend
 
 
+def _patch_temporalio():
+    """Patch temporalio module so imports succeed without the package installed."""
+    from importlib.machinery import ModuleSpec
+    import sys
+    from unittest.mock import MagicMock
+
+    class MockClientInterceptor:
+        pass
+
+    class MockWorkerInterceptor:
+        pass
+
+    from types import ModuleType
+
+    temporalio_root_mock = MagicMock()
+    temporalio_root_mock.client.ClientInterceptor = MockClientInterceptor
+    temporalio_root_mock.worker.WorkerInterceptor = MockWorkerInterceptor
+
+    sys.modules["maascommon.workflows.interceptors"] = MagicMock()
+
+    # The trickery that follows is intended to prevent two things:
+    # 1 - A bunch of assignments to sys.modules
+    # 2 - Maintenance here in case we simply import a different submodule
+    #     of temporalio. Differently from the other cases, temporal
+    #     is something that is pervasive in the code and also has several
+    #     imports, so this is much more likely here.
+    class TemporalioFinder:
+        def find_spec(self, fullname, path, target=None):
+            if fullname == "temporalio" or fullname.startswith("temporalio."):
+                return ModuleSpec(fullname, TemporalioLoader())
+            return None
+
+    class TemporalioLoader(Loader):
+        def create_module(self, spec):
+            module = ModuleType(spec.name)
+
+            parts = spec.name.split(".")[1:]
+            mock_obj = temporalio_root_mock
+            for part in parts:
+                mock_obj = getattr(mock_obj, part)
+
+            module.__path__ = []  # Triggers Python to treat it as a package
+            module.__getattr__ = lambda name: getattr(mock_obj, name)
+
+            return module
+
+        def exec_module(self, module):
+            pass
+
+    sys.meta_path.insert(0, TemporalioFinder())
+
+
+def _patch_paramiko():
+    """Patch paramiko module so imports succeed without the package installed."""
+    paramiko = MagicMock()
+
+    sys.modules["paramiko"] = paramiko
+
+
 def generate_api_description_from_source():
     if "DJANGO_SETTINGS_MODULE" not in os.environ:
         os.environ.setdefault(
@@ -127,7 +187,9 @@ def get_openapi_spec() -> dict[str, str | Any]:
     _patch_distro_info()
     _patch_seamicroclient()
     _patch_curtin()
+    _patch_temporalio()
     _patch_tftp()
+    _patch_paramiko()
     return generate_api_description_from_source()
 
 
