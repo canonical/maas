d8e8bb953bcd2098d482fc360ccc8b8a59eb2072
Aloizio Macedo <aloizio.macedo@canonical.com>
2026-07-15 15:36:11 -0300

    chore: get min of vlans by key instead of sorted[0]

--- src/maasserver/models/fabric.py
+++ src/maasserver/models/fabric.py
@@ -157,9 +157,7 @@ class Fabric(CleanSave, TimestampedModel):
         return self.id == 0
 
     def get_default_vlan(self):
-        # This logic is replicated in the dehydrate() function of the
-        # websockets handler.
-        return sorted(self.vlan_set.all(), key=attrgetter("id"))[0]
+        return min(self.vlan_set.all(), key=attrgetter("id"))
 
     def get_name(self):
         """Return the name of the fabric."""
