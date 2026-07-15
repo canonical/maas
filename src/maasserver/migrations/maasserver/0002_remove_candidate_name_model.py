49f1bade6227e09ac1e0d4476bf668f2682553e2
Jacopo Rota <jacopo.rota@canonical.com>
2026-07-15 17:46:15 +0200

    refactor: remove django migrations and db->disk image migrations

--- src/maasserver/migrations/maasserver/0002_remove_candidate_name_model.py
+++
@@ -1,7 +0,0 @@
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-    dependencies = [("maasserver", "0001_initial")]
-
-    operations = [migrations.DeleteModel(name="CandidateName")]
--- src/maasserver/migrations/maasserver/0017_remove_node_power_type.py
+++
@@ -1,7 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [("maasserver", "0016_migrate_power_data_node_to_bmc")]
-
-    operations = [migrations.RemoveField(model_name="node", name="power_type")]
