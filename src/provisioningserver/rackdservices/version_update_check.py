80fea87caf90dbd53b7e4a5c0027ab9e0522ad7f
Jacopo Rota <jacopo.rota@canonical.com>
2026-07-23 18:20:22 +0200

    lint

--- src/provisioningserver/rackdservices/version_update_check.py
+++ src/provisioningserver/rackdservices/version_update_check.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Canonical Ltd.  This software is licensed under the
+# Copyright 2021-2026 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Service to periodically check for version updates."""
