292d5bd20ed3551a230fd317a010567438a323fa
Alessandro Marcolini <alessandro.marcolini@canonical.com>
2026-08-07 10:46:53 +0200

    chore: update copyright headers

--- src/tests/maasservicelayer/services/test_scriptresult.py
+++ src/tests/maasservicelayer/services/test_scriptresult.py
@@ -1,5 +1,5 @@
-#  Copyright 2025 Canonical Ltd.  This software is licensed under the
-#  GNU Affero General Public License version 3 (see the file LICENSE).
+# Copyright 2025-2026 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
 
 from unittest.mock import Mock
 
