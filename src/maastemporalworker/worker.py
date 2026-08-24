857aea1664094d06ed358b24d8d9d147e62408ba
Alexsander de Souza <61709370+alexsander-souza@users.noreply.github.com>
2026-08-24 14:12:35 -0300

    fix: Update temporal service configuration and binding (#569)

--- src/maastemporalworker/worker.py
+++ src/maastemporalworker/worker.py
@@ -17,13 +17,14 @@ import temporalio.converter
 from temporalio.service import RPCError, RPCStatusCode
 from temporalio.worker import Worker as TemporalWorker
 
+from maasserver.config import get_temporal_connect_address
 from maastemporalworker.encryptor import EncryptionCodec
 from maastemporalworker.workflow.utils import async_retry
 from provisioningserver.certificates import get_maas_cluster_cert_paths
 from provisioningserver.utils.env import MAAS_ID, MAAS_SHARED_SECRET
+from provisioningserver.utils.network import convert_host_to_uri_str
 
 REGION_TASK_QUEUE = "region"
-TEMPORAL_HOST = "localhost"
 TEMPORAL_PORT = 5271
 TEMPORAL_WORKFLOW_RETENTION = "259200s"  # tctl's default retention in seconds
 TEMPORAL_NAMESPACE = "default"
@@ -42,8 +43,9 @@ async def get_client_async() -> Client:
     with open(cacert_file, "rb") as f:
         cacert = f.read()
 
+    connect_address = convert_host_to_uri_str(get_temporal_connect_address())
     return await Client.connect(
-        f"{TEMPORAL_HOST}:{TEMPORAL_PORT}",
+        f"{connect_address}:{TEMPORAL_PORT}",
         identity=f"{maas_id}@region:{pid}",
         data_converter=dataclasses.replace(
             temporalio.converter.default(),
