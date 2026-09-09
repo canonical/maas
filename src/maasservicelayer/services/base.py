6b04f2c3766b3bae9818b40edf6de4b933f88412
Tanzeel Ur Rehman <tanzeel.urrehman@canonical.com>
2026-09-09 09:12:39 -0600

    refactor: add pre_update_hook for builder-only update validation
    (cherry picked from commit dabf34d7c5b2fa807005efd8304707c38cfb21ee)

--- src/maasservicelayer/services/base.py
+++ src/maasservicelayer/services/base.py
@@ -277,6 +277,12 @@ class BaseService(ReadOnlyService[M, BR], ABC, Generic[M, BR, B]):
         """
         return None
 
+    async def pre_update_hook(self, builder: B) -> None:
+        """
+        Override this function in your Service to perform pre-hooks for the object to update
+        """
+        return None
+
     async def pre_update_instance(
         self, existing_resource: M, builder: B
     ) -> None:
@@ -323,6 +329,7 @@ class BaseService(ReadOnlyService[M, BR], ABC, Generic[M, BR, B]):
                 ]
             )
 
+        await self.pre_update_hook(builder)
         await self.pre_update_instance(existing_resource, builder)
         self.etag_check(existing_resource, etag_if_match)
         updated_resource = await self.repository.update_by_id(
